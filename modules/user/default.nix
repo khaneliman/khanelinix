9a4a93d35f4fbd315fc51e7b49238b039eaa60af
Austin Horstman <khaneliman12@gmail.com>
2023-05-30 10:01:07 -0500

    change login shell to zsh

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -185,7 +185,7 @@ in
         home = "/home/${cfg.name}";
         group = "users";
 
-        shell = pkgs.fish;
+        shell = pkgs.zsh;
 
         # Arbitrary user ID to use for the user. Since I only
         # have a single user on my machines this won't ever collide.
