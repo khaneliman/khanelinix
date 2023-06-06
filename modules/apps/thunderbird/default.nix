5921799db07690768e4c857ac96fd4e43d307aab
Austin Horstman <khaneliman12@gmail.com>
2023-06-06 12:34:25 -0500

    thunderbird updates

--- modules/apps/thunderbird/default.nix
+++ modules/apps/thunderbird/default.nix
@@ -14,5 +14,45 @@ in
   };
 
   config =
-    mkIf cfg.enable { environment.systemPackages = with pkgs; [ thunderbird davmail ]; };
+    mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [
+        birdtray
+        davmail
+      ];
+
+      khanelinix.home.extraOptions = {
+        programs.thunderbird = {
+          enable = true;
+          package = pkgs.thunderbird;
+
+          profiles.${config.khanelinix.user.name} = {
+            isDefault = true;
+
+            settings = {
+              "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+              "layers.acceleration.force-enabled" = true;
+              "gfx.webrender.all" = true;
+              "gfx.webrender.enabled" = true;
+              "svg.context-properties.content.enabled" = true;
+              "browser.display.use_system_colors" = true;
+              "browser.theme.dark-toolbar-theme" = true;
+            };
+
+            userChrome = ''
+              #spacesToolbar,
+              #agenda-container,
+              #agenda,
+              #agenda-toolbar,
+              #mini-day-box
+              {
+                background-color: #24273a !important;
+              }
+            '';
+
+            # TODO: Bundle extensions
+            # TODO: set up accounts
+          };
+        };
+      };
+    };
 }
