1746cc995e71f63ac8d38ac8ac4b0d1ac93991e3
Austin Horstman <khaneliman12@gmail.com>
2023-06-27 09:31:07 -0500

    refine xdg-desktop-portal implementation

--- modules/desktop/addons/xdg-portal/default.nix
+++ modules/desktop/addons/xdg-portal/default.nix
@@ -1,14 +1,14 @@
-{ options
-, config
-, lib
-, pkgs
-, ...
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 with lib;
 with lib.internal; let
   cfg = config.khanelinix.desktop.addons.xdg-portal;
-in
-{
+in {
   options.khanelinix.desktop.addons.xdg-portal = with types; {
     enable = mkBoolOpt false "Whether or not to add support for xdg portal.";
   };
@@ -17,10 +17,12 @@ in
     xdg = {
       portal = {
         enable = true;
-        extraPortals = with pkgs; [
-          xdg-desktop-portal-wlr
-          xdg-desktop-portal-gtk
-        ];
+        extraPortals = with pkgs;
+          [
+            xdg-desktop-portal-gtk
+          ]
+          ++ (lib.optional config.khanelinix.desktop.hyprland.enable xdg-desktop-portal-hyprland)
+          ++ (lib.optional config.khanelinix.desktop.sway.enable xdg-desktop-portal-wlr);
       };
     };
   };
