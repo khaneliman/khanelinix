95eada4de9545c3d27af6b550529b951322a041c
Austin Horstman <khaneliman12@gmail.com>
2023-07-01 23:16:03 -0500

    screenlayout disable resetting secondary monitor

--- systems/x86_64-linux/khanelinix/.screenlayout/primary.sh
+++ systems/x86_64-linux/khanelinix/.screenlayout/primary.sh
@@ -3,6 +3,6 @@ xrandr \
 	--output XWAYLAND0 --primary --mode 1920x1080 --pos 1420x0 --rotate normal \
 	--output XWAYLAND1 --mode 5120x1440 --pos 0x1080 --rotate normal
 
-wlr-randr \
-	--output DP-3 --off && sleep 1 &&
-	wlr-randr --output DP-3 --on --pos 1420,0 --mode 3840x2160@59.997 --scale 2
+# wlr-randr \
+# 	--output DP-3 --off && sleep 1 &&
+# 	wlr-randr --output DP-3 --on --pos 1420,0 --mode 3840x2160@59.997 --scale 2
