3cef623959dcf5df8f4bf461a77711e1e0d904ca
Austin Horstman <khaneliman12@gmail.com>
2023-07-06 10:27:09 -0500

    shared blender config

--- modules/nixos/apps/blender/default.nix
+++ modules/nixos/apps/blender/default.nix
@@ -9,13 +9,5 @@ with lib;
 with lib.internal; let
   cfg = config.khanelinix.apps.blender;
 in {
-  options.khanelinix.apps.blender = with types; {
-    enable = mkBoolOpt false "Whether or not to enable Blender.";
-  };
-
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      blender
-    ];
-  };
+  imports = [../../../shared/blender/default.nix];
 }
