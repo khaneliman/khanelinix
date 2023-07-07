a9c427a02e1780cce652ff88d5be6b208aaf4648
Austin Horstman <khaneliman12@gmail.com>
2023-07-07 09:24:58 -0500

    khanelinix enable sysrq

--- systems/x86_64-linux/khanelinix/hardware.nix
+++ systems/x86_64-linux/khanelinix/hardware.nix
@@ -19,6 +19,7 @@ in {
   ##
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
+    kernel.sysctl."kernel.sysrq" = 1;
     kernelParams = [
       "video=DP-1:5120x1440@120"
       "video=DP-3:3840x2160@60"
