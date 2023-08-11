8035fcad4e4bca48b9f7cfc34a5f77daf71ed366
Austin Horstman <khaneliman12@gmail.com>
2023-08-11 09:57:04 -0500

    nixos remove bundled op-ssh-sign

--- modules/nixos/apps/_1password/default.nix
+++ modules/nixos/apps/_1password/default.nix
@@ -27,11 +27,5 @@ in
           IdentityAgent ~/.1password/agent.sock
       '';
     };
-
-    system.activationScripts.postInstall1Password = stringAfter [ "users" ] ''
-      echo "Running command after 1Password installation"
-      mkdir -p /opt/1Password/
-      ln -sf ${./op-ssh-sign} /opt/1Password/op-ssh-sign
-    '';
   };
 }
