fe6f125b8e318efaeacac2a8f022a426ccee6b8f
Austin Horstman <khaneliman12@gmail.com>
2023-08-12 09:17:37 -0500

    micro home-manager consolidation

---
+++ modules/home/cli-apps/micro/default.nix
@@ -0,0 +1,48 @@
+{ options
+, config
+, lib
+, ...
+}:
+with lib;
+with lib.internal; let
+  cfg = config.khanelinix.cli-apps.micro;
+in
+{
+  options.khanelinix.cli-apps.micro = with types; {
+    enable = mkBoolOpt false "Whether or not to enable micro.";
+    default = mkBoolOpt false "Whether to set micro as the session EDITOR";
+  };
+
+  config = mkIf cfg.enable {
+    programs.micro = {
+      enable = true;
+
+      settings = {
+        colorscheme = "catppuccin-macchiato";
+      };
+    };
+
+    programs.zsh.shellAliases.vimdiff = mkIf cfg.default "micro -d";
+    programs.bash.shellAliases.vimdiff = mkIf cfg.default "micro -d";
+    programs.fish.shellAliases.vimdiff = mkIf cfg.default "micro -d";
+
+
+    home.sessionVariables = {
+      EDITOR = mkIf cfg.default "micro";
+    };
+
+    xdg.configFile."micro/colorschemes" = {
+      source = lib.cleanSourceWith {
+        filter = name: _type:
+          let
+            baseName = baseNameOf (toString name);
+          in
+          (lib.hasSuffix ".micro" baseName);
+        src = lib.cleanSource ./.;
+      };
+
+      # links each file individually, which lets us insert the colors file separately
+      recursive = true;
+    };
+  };
+}
