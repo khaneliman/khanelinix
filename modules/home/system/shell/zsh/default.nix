4e39afd357f3693c409289352e5ceec8dc7ce2a0
Austin Horstman <khaneliman12@gmail.com>
2023-08-20 14:22:48 -0500

    zsh plugins

--- modules/home/system/shell/zsh/default.nix
+++ modules/home/system/shell/zsh/default.nix
@@ -58,12 +58,43 @@ in
           {
             name = "zsh-nix-shell";
             file = "nix-shell.plugin.zsh";
-            src = pkgs.fetchFromGitHub {
-              owner = "chisui";
-              repo = "zsh-nix-shell";
-              rev = "v0.4.0";
-              sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
-            };
+            src = pkgs.zsh-nix-shell;
+          }
+          {
+            name = "zsh-autocomplete";
+            src = pkgs.zsh-autocomplete;
+          }
+          {
+            name = "zsh-autosuggestions";
+            src = pkgs.zsh-autosuggestions;
+          }
+          {
+            name = "zsh-syntax-highlighting";
+            src = pkgs.zsh-syntax-highlighting;
+          }
+          {
+            name = "zsh-you-should-use";
+            src = pkgs.zsh-you-should-use;
+          }
+          {
+            name = "zsh-history";
+            src = pkgs.zsh-history;
+          }
+          {
+            name = "zsh-history";
+            src = pkgs.zsh-history;
+          }
+          {
+            name = "zsh-command-time";
+            src = pkgs.zsh-command-time;
+          }
+          {
+            name = "zsh-history-to-fish";
+            src = pkgs.zsh-history-to-fish;
+          }
+          {
+            name = "zsh-navigation-tools";
+            src = pkgs.zsh-navigation-tools;
           }
         ];
       };
