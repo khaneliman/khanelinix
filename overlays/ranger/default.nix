19dc795e4248fb3986ee9159b163b89e9ac427bf
Austin Horstman <khaneliman12@gmail.com>
2023-08-28 15:41:55 -0500

    ranger overlay updated to latest commit

--- overlays/ranger/default.nix
+++ overlays/ranger/default.nix
@@ -1,12 +1,12 @@
 { ... }: (_self: super: {
   # Use ranger PR, fixes freeze after opening image in kitty: https://github.com/ranger/ranger/pull/2856
   ranger = super.ranger.overrideAttrs (old: {
-    version = "1.9.3";
+    version = "136416c7e2ecc27315fe2354ecadfe09202df7dd";
     src = super.fetchFromGitHub {
-      owner = "Ethsan";
+      owner = "ranger";
       repo = "ranger";
-      rev = "71a06f28551611d192d3e644d95ad04023e10801";
-      sha256 = "sha256-Yjdn1oE5VtJMGnmQ2VC764UXKm1PrkIPXXQ8MzQ8u1U=";
+      rev = "136416c7e2ecc27315fe2354ecadfe09202df7dd";
+      sha256 = "sha256-nW4KlatugmPRPXl+XvV0/mo+DE5o8FLRrsJuiKbFGyY=";
     };
     propagatedBuildInputs = old.propagatedBuildInputs ++ (with super.python3Packages; [ astroid pylint ]);
   });
