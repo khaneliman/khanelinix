44e9b86f37cca9f4073368882b8d2687b50a6c0d
Austin Horstman <khaneliman12@gmail.com>
2023-08-28 14:36:07 -0500

    remove unused sketchybar version overlay

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,25 +1,5 @@
 { ... }: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
-    version = "2.16.1";
-
-    src = prev.fetchFromGitHub {
-      owner = "FelixKratz";
-      repo = "SketchyBar";
-      rev = "v2.16.1";
-      hash = "sha256-H+bR5ZhUTrN2KAEdY/hnq6c3TEb1NQvPQ9uPo09gSM8=";
-    };
-
-    buildInputs = [
-      prev.darwin.apple_sdk_11_0.frameworks.AppKit
-      prev.darwin.apple_sdk_11_0.frameworks.CoreAudio
-      prev.darwin.apple_sdk_11_0.frameworks.CoreWLAN
-      prev.darwin.apple_sdk_11_0.frameworks.CoreVideo
-      prev.darwin.apple_sdk_11_0.frameworks.DisplayServices
-      prev.darwin.apple_sdk_11_0.frameworks.IOKit
-      prev.darwin.apple_sdk_11_0.frameworks.MediaRemote
-      prev.darwin.apple_sdk_11_0.frameworks.SkyLight
-    ];
-
     # Create secondary sketchybar executable for dynamic island
     installPhase = ''
       ${oldAttrs.installPhase}
