1d3b8c52246065af0c6c746f0395c1c2f8e792c2
Austin Horstman <khaneliman12@gmail.com>
2023-09-01 22:55:32 -0500

    remove upstreamed overlay fix

--- overlays/ncdu/default.nix
+++
@@ -1,7 +0,0 @@
-{ ...
-}: _final: prev: {
-  # FIX: resolve nixpkgs broken build
-  ncdu = prev.ncdu.overrideAttrs (_oldAttrs: {
-    zigBuildFlags = prev.lib.optional prev.stdenv.isDarwin "-Dpie=true";
-  });
-}
