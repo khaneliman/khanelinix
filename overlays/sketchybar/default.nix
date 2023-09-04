a4b97d117fbdd92bf85beb69158b69aa010c5bec
Austin Horstman <khaneliman12@gmail.com>
2023-09-04 09:18:52 -0500

    chore: sketchybar version overlay until nixpkgs updated

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,5 +1,14 @@
 { ... }: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
+    version = "2.16.3";
+
+    src = prev.fetchFromGitHub {
+      owner = "FelixKratz";
+      repo = "SketchyBar";
+      rev = "v2.16.3";
+      hash = "sha256-PCAGIcO7lvIAEFXlJn/e9zG5kxvDABshxFbu/bXWX7o=";
+    };
+
     # Create secondary sketchybar executable for dynamic island
     installPhase = ''
       ${oldAttrs.installPhase}
