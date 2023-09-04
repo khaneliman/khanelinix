34281e87c6b0aa69c9b43fdae9eb7ba19a39592e
Austin Horstman <khaneliman12@gmail.com>
2023-09-04 00:47:36 -0500

    feat: add lightdm

---
+++ modules/nixos/display-managers/lightdm/default.nix
@@ -0,0 +1,52 @@
+{ options
+, config
+, lib
+, pkgs
+, ...
+}:
+with lib;
+with lib.internal; let
+  cfg = config.khanelinix.display-managers.lightdm;
+in
+{
+  options.khanelinix.display-managers.lightdm = with types; {
+    enable = mkBoolOpt false "Whether or not to enable lightdm.";
+  };
+
+  config =
+    mkIf cfg.enable
+      {
+        services.xserver = {
+          enable = true;
+
+          displayManager.lightdm = {
+            enable = true;
+            background = pkgs.khanelinix.wallpapers.flatppuccin_macchiato;
+
+            greeters = {
+              gtk = {
+                enable = true;
+
+                cursorTheme = {
+                  name = config.khanelinix.desktop.addons.gtk.cursor.name;
+                  package = config.khanelinix.desktop.addons.gtk.cursor.pkg;
+                };
+
+                iconTheme = {
+                  name = config.khanelinix.desktop.addons.gtk.icon.name;
+                  package = config.khanelinix.desktop.addons.gtk.icon.pkg;
+                };
+
+                theme = {
+                  name = "${config.khanelinix.desktop.addons.gtk.theme.name}";
+                  package = config.khanelinix.desktop.addons.gtk.theme.pkg;
+                };
+              };
+            };
+          };
+        };
+
+        security.pam.services.lightdm.gnupg.enable = true;
+        security.pam.services.lightdm.enableGnomeKeyring = true;
+      };
+}
