dfe062c7a06718d6f13740063126fc5cbb5cd7ae
Austin Horstman <khaneliman12@gmail.com>
2023-09-11 21:26:00 -0500

    refactor: move import_env to packages

--- modules/home/desktop/hyprland/apps.nix
+++ modules/home/desktop/hyprland/apps.nix
@@ -7,64 +7,7 @@ let
   inherit (lib) mkIf getExe;
 
   cfg = config.khanelinix.desktop.hyprland;
-
-  import_env = pkgs.writeShellScriptBin "import_env" ''
-    #!/usr/bin/env bash
-    set -e
-
-    [[ -n $HYPRLAND_DEBUG_CONF ]] && exit 0
-    USAGE="\
-    Import environment variables
-    Usgae: $0 <command>
-    Commands:
-       tmux         import to tmux server
-       system       import to systemd and dbus user session
-       help         print this help
-    "
-
-    _envs=(
-     # display
-     WAYLAND_DISPLAY
-     DISPLAY
-     # xdg
-     USERNAME
-     XDG_BACKEND
-     XDG_CURRENT_DESKTOP
-     XDG_SESSION_TYPE
-     XDG_SESSION_ID
-     XDG_SESSION_CLASS
-     XDG_SESSION_DESKTOP
-     XDG_SEAT
-     XDG_VTNR
-     # hyprland
-     HYPRLAND_INSTANCE_SIGNATURE
-     # ssh
-     SSH_AUTH_SOCK
-    )
-
-    case "$1" in
-    system)
-     dbus-update-activation-environment --systemd "$${_envs[@]}"
-     ;;
-    tmux)
-     for v in "$${_envs[@]}"; do
-      if [[ -n $${!v} ]]; then
-       tmux setenv -g "$v" "$${!v}"
-      fi
-     done
-     ;;
-    help)
-     echo -n "$USAGE"
-     exit 0
-     ;;
-    *)
-     echo "operation reuqired"
-     echo "use \"$0 help\" to see usage help"
-     exit 1
-     ;;
-    esac
-  '';
-in
+ in
 {
   config =
     mkIf cfg.enable
@@ -77,8 +20,8 @@ in
               # ░▀░▀░▀░░░▀░░░░░▀▀▀░░▀░░▀░▀░▀░▀░░▀░░▀▀▀░▀░░
 
               # import env
-              "${import_env}/bin/import_env system"
-              "${import_env}/bin/import_env tmux"
+              "${getExe pkgs.khanelinix.import_env} system"
+              "${getExe pkgs.khanelinix.import_env} tmux"
 
               # Startup background apps
               "${pkgs.libsForQt5.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1 &"
