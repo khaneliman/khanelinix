4d3f6d43a3e63c909d0a6ac8e8343a15cb3a8a5a
Austin Horstman <khaneliman12@gmail.com>
2023-09-13 20:52:21 -0500

    feat: dooit 2.0.0 overlay

---
+++ overlays/dooit/default.nix
@@ -0,0 +1,17 @@
+{ ... }: (_final: prev: {
+  dooit = prev.dooit.overrideAttrs (old: {
+    pname = "dooit";
+    version = "2.0.0";
+    format = "pyproject";
+
+    src = prev.fetchFromGitHub {
+      owner = "kraanzu";
+      repo = "dooit";
+      rev = "v2.0.0";
+      hash = "sha256-Ipj3ltuewbMIUYRffxxPcJgIPxP5dJAkHpo14ZZKq+k=";
+    };
+
+    propagatedBuildInputs = old.propagatedBuildInputs ++
+      [ prev.python3.pkgs.parsedatetime ];
+  });
+})
