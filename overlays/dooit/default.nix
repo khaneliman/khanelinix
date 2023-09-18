030e7c996082b54cb6c94a77a7eda40e8ecedc7b
Austin Horstman <khaneliman12@gmail.com>
2023-09-17 19:09:48 -0500

    chore: bump dooit overlay until new nixos-unstable

--- overlays/dooit/default.nix
+++ overlays/dooit/default.nix
@@ -1,17 +1,16 @@
 _: (_final: prev: {
   dooit = prev.dooit.overrideAttrs (old: {
     pname = "dooit";
-    version = "2.0.0";
-    format = "pyproject";
+    version = "2.0.1";
 
     src = prev.fetchFromGitHub {
       owner = "kraanzu";
       repo = "dooit";
-      rev = "v2.0.0";
-      hash = "sha256-Ipj3ltuewbMIUYRffxxPcJgIPxP5dJAkHpo14ZZKq+k=";
+      rev = "v2.0.1";
+      hash = "sha256-iQAGD6zrBBd4fJONaB7to1OJpAJUO0zeA1xhVQZBkMc=";
     };
 
     propagatedBuildInputs = old.propagatedBuildInputs ++
-      [ prev.python3.pkgs.parsedatetime ];
+      [ prev.python3.pkgs.python-dateutil ];
   });
 })
