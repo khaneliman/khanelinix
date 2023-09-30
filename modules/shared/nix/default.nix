5f521e64ee1396f4a11d2951d040f75d8f440355
Austin Horstman <khaneliman12@gmail.com>
2023-09-29 22:24:29 -0500

    feat: add khanelinix cachix

--- modules/shared/nix/default.nix
+++ modules/shared/nix/default.nix
@@ -50,10 +50,12 @@ in
           substituters = [
             "https://cache.nixos.org"
             "https://nix-community.cachix.org"
+            "https://khanelinix.cachix.org"
           ];
           trusted-public-keys = [
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "khanelinix.cachix.org-1:FTmbv7OqlMsmJEOFvAlz7PVkoGtstbwLC2OldAiJZ10="
           ];
         };
 
