a83934faea03446a0cf2c9b7ebaa41425ee9d9f8
Austin Horstman <khaneliman12@gmail.com>
2023-10-07 22:40:01 -0500

    feat: add k9s

--- homes/x86_64-linux/nixos@CORE-PW00LM92/default.nix
+++ homes/x86_64-linux/nixos@CORE-PW00LM92/default.nix
@@ -19,6 +19,7 @@ in
 
     cli-apps = {
       home-manager = enabled;
+      k9s = enabled;
     };
 
     security = {
---
+++ modules/home/cli-apps/k9s/default.nix
@@ -0,0 +1,38 @@
+{ config
+, lib
+, options
+, pkgs
+, ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.internal) mkBoolOpt;
+
+  cfg = config.khanelinix.cli-apps.k9s;
+
+  fromYAML = f:
+    let
+      jsonFile =
+        pkgs.runCommand "in.json"
+          {
+            nativeBuildInputs = [ pkgs.jc ];
+          } ''
+          jc --yaml < "${f}" > "$out"
+        '';
+    in
+    builtins.elemAt (builtins.fromJSON (builtins.readFile jsonFile)) 0;
+in
+{
+  options.khanelinix.cli-apps.k9s = {
+    enable = mkBoolOpt false "Whether or not to enable k9s.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.k9s = {
+      enable = true;
+      package = pkgs.k9s;
+
+      skin = fromYAML ./themes/macchiato.yml;
+    };
+  };
+}
