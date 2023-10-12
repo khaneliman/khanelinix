a78d9bc9d8eea45a4d4dfe35be685cc7fe84c22b
Austin Horstman <khaneliman12@gmail.com>
2023-10-12 10:43:33 -0500

    fix: khanelinix static ip match router config

--- systems/x86_64-linux/khanelinix/default.nix
+++ systems/x86_64-linux/khanelinix/default.nix
@@ -193,6 +193,14 @@ in
     };
   };
 
+  networking = {
+    defaultGateway = "192.168.1.1";
+    interfaces.enp6s0.ipv4.addresses = [{
+      address = "192.168.1.3";
+      prefixLength = 24;
+    }];
+  };
+
   services.xserver.displayManager.defaultSession = "hyprland";
   services.mpd = {
     musicDirectory = "nfs://austinserver.local/mnt/user/data/media/music";
