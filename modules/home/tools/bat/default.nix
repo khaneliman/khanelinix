e6621af03372d1c009abd7b1357735b006a4220d
Austin Horstman <khaneliman12@gmail.com>
2023-10-13 08:39:52 -0500

    refactor: bat theme attribute set new package

--- modules/home/tools/bat/default.nix
+++ modules/home/tools/bat/default.nix
@@ -34,14 +34,10 @@ in
       ];
 
       themes = {
-        catppuccin-macchiato = builtins.readFile (pkgs.fetchFromGitHub
-          {
-            owner = "catppuccin";
-            repo = "bat";
-            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-            hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-          }
-        + "/Catppuccin-macchiato.tmTheme");
+        catppuccin-macchiato = {
+          src = pkgs.catppuccin;
+          file = "/bat/Catppuccin-macchiato.tmTheme";
+        };
       };
     };
 
