da9db286f9643037efed44c55851790852f03290
Austin Horstman <khaneliman12@gmail.com>
2023-10-23 23:14:43 -0500

    feat: kitty add shell integration

--- modules/home/desktop/addons/kitty/default.nix
+++ modules/home/desktop/addons/kitty/default.nix
@@ -20,7 +20,60 @@ in
   config = mkIf cfg.enable {
     programs.kitty = {
       enable = true;
-      inherit (cfg) theme;
+
+      keybindings =
+        {
+          "ctrl+shift+v" = "paste_from_clipboard";
+          "ctrl+shift+s" = "paste_from_selection";
+          "ctrl+shift+c" = "copy_to_clipboard";
+          "shift+insert" = "paste_from_selection";
+
+          "ctrl+shift+up" = "scroll_line_up";
+          "ctrl+shift+down" = "scroll_line_down";
+          "ctrl+shift+k" = "scroll_line_up";
+          "ctrl+shift+j" = "scroll_line_down";
+          "ctrl+shift+page_up" = "scroll_page_up";
+          "ctrl+shift+page_down" = "scroll_page_down";
+          "ctrl+shift+home" = "scroll_home";
+          "ctrl+shift+end" = "scroll_end";
+          "ctrl+shift+h" = "show_scrollback";
+
+          "ctrl+shift+enter" = "new_window";
+          "ctrl+shift+n" = "new_os_window";
+          "ctrl+shift+w" = "close_window";
+          "ctrl+shift+]" = "next_window";
+          "ctrl+shift+[" = "previous_window";
+          "ctrl+shift+f" = "move_window_forward";
+          "ctrl+shift+b" = "move_window_backward";
+          "ctrl+shift+`" = "move_window_to_top";
+          "ctrl+shift+1" = "first_window";
+          "ctrl+shift+2" = "second_window";
+          "ctrl+shift+3" = "third_window";
+          "ctrl+shift+4" = "fourth_window";
+          "ctrl+shift+5" = "fifth_window";
+          "ctrl+shift+6" = "sixth_window";
+          "ctrl+shift+7" = "seventh_window";
+          "ctrl+shift+8" = "eighth_window";
+          "ctrl+shift+9" = "ninth_window";
+          "ctrl+shift+0" = "tenth_window";
+
+          "ctrl+shift+right" = "next_tab";
+          "ctrl+shift+left" = "previous_tab";
+          "ctrl+shift+t" = "new_tab";
+          "ctrl+shift+q" = "close_tab";
+          "ctrl+shift+l" = "next_layout";
+          "ctrl+shift+." = "move_tab_forward";
+          "ctrl+shift+," = "move_tab_backward";
+          "ctrl+shift+alt+t" = "set_tab_title";
+
+          "ctrl+shift+equal" = "increase_font_size";
+          "ctrl+shift+minus" = "decrease_font_size";
+          "ctrl+shift+backspace" = "restore_font_size";
+          "ctrl+shift+f6" = "set_font_size 16.0";
+        }
+        // lib.optionalAttrs pkgs.stdenv.isDarwin {
+          "cmd+opt+s" = "noop";
+        };
 
       settings =
         {
@@ -112,59 +165,13 @@ in
           macos_colorspace = "displayp3";
         };
 
-      keybindings =
-        {
-          "ctrl+shift+v" = "paste_from_clipboard";
-          "ctrl+shift+s" = "paste_from_selection";
-          "ctrl+shift+c" = "copy_to_clipboard";
-          "shift+insert" = "paste_from_selection";
+      shellIntegration = {
+        enableBashIntegration = true;
+        enableFishIntegration = true;
+        enableZshIntegration = true;
+      };
 
-          "ctrl+shift+up" = "scroll_line_up";
-          "ctrl+shift+down" = "scroll_line_down";
-          "ctrl+shift+k" = "scroll_line_up";
-          "ctrl+shift+j" = "scroll_line_down";
-          "ctrl+shift+page_up" = "scroll_page_up";
-          "ctrl+shift+page_down" = "scroll_page_down";
-          "ctrl+shift+home" = "scroll_home";
-          "ctrl+shift+end" = "scroll_end";
-          "ctrl+shift+h" = "show_scrollback";
-
-          "ctrl+shift+enter" = "new_window";
-          "ctrl+shift+n" = "new_os_window";
-          "ctrl+shift+w" = "close_window";
-          "ctrl+shift+]" = "next_window";
-          "ctrl+shift+[" = "previous_window";
-          "ctrl+shift+f" = "move_window_forward";
-          "ctrl+shift+b" = "move_window_backward";
-          "ctrl+shift+`" = "move_window_to_top";
-          "ctrl+shift+1" = "first_window";
-          "ctrl+shift+2" = "second_window";
-          "ctrl+shift+3" = "third_window";
-          "ctrl+shift+4" = "fourth_window";
-          "ctrl+shift+5" = "fifth_window";
-          "ctrl+shift+6" = "sixth_window";
-          "ctrl+shift+7" = "seventh_window";
-          "ctrl+shift+8" = "eighth_window";
-          "ctrl+shift+9" = "ninth_window";
-          "ctrl+shift+0" = "tenth_window";
-
-          "ctrl+shift+right" = "next_tab";
-          "ctrl+shift+left" = "previous_tab";
-          "ctrl+shift+t" = "new_tab";
-          "ctrl+shift+q" = "close_tab";
-          "ctrl+shift+l" = "next_layout";
-          "ctrl+shift+." = "move_tab_forward";
-          "ctrl+shift+," = "move_tab_backward";
-          "ctrl+shift+alt+t" = "set_tab_title";
-
-          "ctrl+shift+equal" = "increase_font_size";
-          "ctrl+shift+minus" = "decrease_font_size";
-          "ctrl+shift+backspace" = "restore_font_size";
-          "ctrl+shift+f6" = "set_font_size 16.0";
-        }
-        // lib.optionalAttrs pkgs.stdenv.isDarwin {
-          "cmd+opt+s" = "noop";
-        };
+      inherit (cfg) theme;
     };
   };
 }
