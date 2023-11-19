883ed7b5034a50bfc9d0408be0de84c49608e867
Austin Horstman <khaneliman12@gmail.com>
2023-11-19 14:27:07 -0600

    fix: zsh sketchybar aliases

--- modules/home/desktop/addons/sketchybar/default.nix
+++ modules/home/desktop/addons/sketchybar/default.nix
@@ -1,13 +1,18 @@
 { config
 , lib
 , options
+, pkgs
 , ...
 }:
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf getExe;
   inherit (lib.internal) mkBoolOpt;
 
   cfg = config.khanelinix.desktop.addons.sketchybar;
+
+  shellAliases = with pkgs; {
+    push = ''command git push && ${getExe sketchybar} --trigger git_push'';
+  };
 in
 {
   options.khanelinix.desktop.addons.sketchybar = {
@@ -16,6 +21,18 @@ in
   };
 
   config = mkIf cfg.enable {
+    home.shellAliases = shellAliases;
+
+    programs.zsh.initExtra = ''
+      brew() {
+        command brew "$@" && ${getExe pkgs.sketchybar} --trigger brew_update
+      }
+
+      mas() {
+        command mas "$@" && ${getExe pkgs.sketchybar} --trigger brew_update
+      }
+    '';
+
     xdg.configFile = {
       "sketchybar" = {
         source = lib.cleanSourceWith {
