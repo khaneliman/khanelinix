4e617da4bc890833debabdc86276ce90d03534d0
Austin Horstman <khaneliman12@gmail.com>
2023-12-10 22:55:47 -0600

    feat: add cloudflared

---
+++ modules/nixos/services/cloudflared/default.nix
@@ -0,0 +1,50 @@
+{ lib
+, pkgs
+, config
+, ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.internal) mkBoolOpt;
+
+  cfg = config.khanelinix.services.cloudflared;
+in
+{
+  options.khanelinix.services.cloudflared = {
+    enable = mkBoolOpt false "Whether or not to configure cloudflared";
+  };
+
+  config = mkIf cfg.enable {
+    # NOTE: future reference for adding assertions
+    # assertions = [
+    #   {
+    #     assertion = cfg.autoconnect.enable -> cfg.autoconnect.key != "";
+    #     message = "khanelinix.services.cloudflared.autoconnect.key must be set";
+    #   }
+    # ];
+
+    services.cloudflared = {
+      enable = true;
+      package = pkgs.cloudflared;
+
+      tunnels = {
+        "KHANELIMANCOM" = {
+          # TODO: replace with sops secret
+          credentialsFile = "REPLACEME";
+          default = "http_status:404";
+          ingress = {
+            "khaneliman.com" = {
+              # TODO: replace with sops secret
+              service = "https://ip:port";
+              originRequest = {
+                noTLSVerify = true;
+                originServerName = "khaneliman.com";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
