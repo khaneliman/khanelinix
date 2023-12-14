096013bd591de2ce1eb3265e676ba1305b7833d0
Austin Horstman <khaneliman12@gmail.com>
2023-12-13 22:26:46 -0600

    refactor: use catppuccin pkg qt theme file

--- modules/home/desktop/addons/qt/default.nix
+++ modules/home/desktop/addons/qt/default.nix
@@ -10,15 +10,6 @@ let
 
   cfg = config.khanelinix.desktop.addons.qt;
 
-  # TODO: move to catppuccin nixpkg
-  colorScheme = {
-    ColorScheme = {
-      active_colors = "#ffcad3f5, #ff1e2030, #ffa5adcb, #ff939ab7, #ff494d64, #ff6e738d, #ffcad3f5, #ffcad3f5, #ffcad3f5, #ff24273a, #ff1e2030, #ff8087a2, #ff8aadf4, #ff24273a, #ff8aadf4, #ffed8796, #ff24273a, #ffcad3f5, #ff181926, #ffcad3f5, #808087a2";
-      disabled_colors = "#ffa5adcb, #ff1e2030, #ffa5adcb, #ff939ab7, #ff494d64, #ff6e738d, #ffa5adcb, #ffa5adcb, #ffa5adcb, #ff24273a, #ff1e2030, #ff8087a2, #ff8aadf4, #ff494d64, #ff8aadf4, #ffed8796, #ff24273a, #ffcad3f5, #ff181926, #ffcad3f5, #808087a2";
-      inactive_colors = "#ffcdd6f4, #ff1e2030, #ffa5adcb, #ff939ab7, #ff494d64, #ff6e738d, #ffcad3f5, #ffcad3f5, #ffcad3f5, #ff24273a, #ff1e2030, #ff8087a2, #ff8aadf4, #ffa5adcb, #ff8aadf4, #ffed8796, #ff24273a, #ffcad3f5, #ff181926, #ffcad3f5, #808087a2";
-    };
-  };
-
   settings = {
     Appearance = {
       color_scheme_path = "";
@@ -54,6 +45,8 @@ let
       ignored_applications = null; # You might need to adjust this depending on Nix version
     };
   };
+
+  colorSchemePath = "${pkgs.catppuccin}/qt5ct/Catppuccin-Macchiato.conf";
 in
 {
   options.khanelinix.desktop.addons.qt = with types; {
@@ -69,15 +62,13 @@ in
   config = mkIf cfg.enable {
     xdg.configFile = {
       "Kvantum".source = ./Kvantum;
-      "qt5ct/colors/Catppuccin-Macchiato.conf".text = lib.generators.toINI { } colorScheme;
       "qt5ct/qt5ct.conf".text = lib.generators.toINI { }
         (settings // {
-          Appearance = mergeAttrs settings.Appearance { color_scheme_path = "/home/${config.khanelinix.user.name}/.config/qt5ct/colors/Catppuccin-Macchiato.conf"; };
+          Appearance = mergeAttrs settings.Appearance { color_scheme_path = colorSchemePath; };
         });
-      "qt6ct/colors/Catppuccin-Macchiato.conf".text = lib.generators.toINI { } colorScheme;
       "qt6ct/qt6ct.conf".text = lib.generators.toINI { }
         (settings // {
-          Appearance = mergeAttrs settings.Appearance { color_scheme_path = "/home/${config.khanelinix.user.name}/.config/qt6ct/colors/Catppuccin-Macchiato.conf"; };
+          Appearance = mergeAttrs settings.Appearance { color_scheme_path = colorSchemePath; };
         });
     };
 
