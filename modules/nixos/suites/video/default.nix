102a2e8a8bc55bc9ed5e7ae89d045aff642ff4d8
Austin Horstman <khaneliman12@gmail.com>
2023-12-13 19:18:33 -0600

    fix: hacks and workarounds for k3b

--- modules/nixos/suites/video/default.nix
+++ modules/nixos/suites/video/default.nix
@@ -19,10 +19,21 @@ in
     environment.systemPackages = with pkgs; [
       devede
       handbrake
-      k3b
       mediainfo-gui
       pitivi
       vlc
     ];
+
+    # NOTE: dvd burning software requires cdrom group and k3b dependencies in nix store do not have those permissions
+    # https://github.com/NixOS/nixpkgs/issues/19154#issuecomment-647005545 
+    # Additionally to installing `k3b` enabling this will
+    # add `setuid` wrappers in `/run/wrappers/bin`
+    # for both `cdrdao` and `cdrecord`. On first
+    # run you must manually configure the path of `cdrdae` and
+    # `cdrecord` to correspond to the appropriate paths under
+    # `/run/wrappers/bin` in the "Setup External Programs" menu.
+    programs.k3b.enable = true;
+
+    users.groups.cdrom.members = [ config.khanelinix.user.name ];
   };
 }
