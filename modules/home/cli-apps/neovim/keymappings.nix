3869b81009f9798d25b1226d7c8fbfcc8e2b4ddd
Austin Horstman <khaneliman12@gmail.com>
2023-12-29 00:04:46 -0600

    feat: nixvim keymappings

--- modules/home/cli-apps/neovim/keymappings.nix
+++ modules/home/cli-apps/neovim/keymappings.nix
@@ -12,62 +12,79 @@
       let
         normal =
           lib.mapAttrsToList
-            (key: action: {
+            (key: { action, options }: {
               mode = "n";
-              inherit action key;
+              inherit action key options;
             })
             {
-              "<Space>" = "<NOP>";
+              "<Space>" = { action = "<NOP>"; options = { }; };
 
               # Esc to clear search results
-              "<esc>" = ":noh<CR>";
+              "<esc>" = { action = ":noh<CR>"; options = { }; };
 
               # fix Y behaviour
-              "Y" = "y$";
+              "Y" = { action = "y$"; options = { }; };
 
               # back and fourth between the two most recent files
-              "<C-c>" = ":b#<CR>";
+              "<C-c>" = { action = ":b#<CR>"; options = { }; };
 
               # close by Ctrl+x
-              "<C-x>" = ":close<CR>";
+              "<C-x>" = { action = ":close<CR>"; options = { }; };
 
               # save by Space+s or Ctrl+s
-              "<leader>s" = ":w<CR>";
-              "<C-s>" = ":w<CR>";
+              "<leader>s" = { action = ":w<CR>"; options = { }; };
+              # "<C-s>" = ":w<CR>";
 
               # navigate to left/right window
-              "<leader>h" = "<C-w>h";
-              "<leader>l" = "<C-w>l";
+              "<leader>h" = { action = "<C-w>h"; options = { }; };
+              "<leader>l" = { action = "<C-w>l"; options = { }; };
 
               # resize with arrows
-              "<C-Up>" = ":resize -2<CR>";
-              "<C-Down>" = ":resize +2<CR>";
-              "<C-Left>" = ":vertical resize +2<CR>";
-              "<C-Right>" = ":vertical resize -2<CR>";
+              "<C-Up>" = { action = ":resize -2<CR>"; options = { }; };
+              "<C-Down>" = { action = ":resize +2<CR>"; options = { }; };
+              "<C-Left>" = { action = ":vertical resize +2<CR>"; options = { }; };
+              "<C-Right>" = { action = ":vertical resize -2<CR>"; options = { }; };
 
               # move current line up/down
               # M = Alt key
-              "<M-k>" = ":move-2<CR>";
-              "<M-j>" = ":move+<CR>";
+              "<M-k>" = { action = ":move-2<CR>"; options = { }; };
+              "<M-j>" = { action = ":move+<CR>"; options = { }; };
 
-              "<leader>rp" = ":!remi push<CR>";
+              "<leader>rp" = { action = ":!remi push<CR>"; options = { }; };
+
+              "<Leader>w" = {
+                action = "<Cmd>w<CR>"; # Action to perform (save the file in this case)
+                options = {
+                  desc = "Save";
+                };
+              };
+
+              "j" = { action = "v:count == 0 ? 'gj' : 'j'"; options = { desc = "Move cursor down"; expr = true; }; };
+              "k" = { action = "v:count == 0 ? 'gk' : 'k'"; options = { desc = "Move cursor up"; expr = true; }; };
+              "<Leader>q" = { action = "<Cmd>confirm q<CR>"; options = { desc = "Quit"; }; };
+              "<Leader>Q" = { action = "<Cmd>confirm qall<CR>"; options = { desc = "Quit all"; }; };
+              "<Leader>n" = { action = "<Cmd>enew<CR>"; options = { desc = "New File"; }; };
+              "<C-s>" = { action = "<Cmd>w!<CR>"; options = { desc = "Force write"; }; };
+              "<C-q>" = { action = "<Cmd>q!<CR>"; options = { desc = "Force quit"; }; };
+              "|" = { action = "<Cmd>vsplit<CR>"; options = { desc = "Vertical Split"; }; };
+              "\\" = { action = "<Cmd>split<CR>"; options = { desc = "Horizontal Split"; }; };
             };
         visual =
           lib.mapAttrsToList
-            (key: action: {
+            (key: { action, options }: {
               mode = "v";
-              inherit action key;
+              inherit action key options;
             })
             {
-              # better indenting
-              ">" = ">gv";
-              "<" = "<gv";
-              "<TAB>" = ">gv";
-              "<S-TAB>" = "<gv";
+              # Better indenting
+              "<S-Tab>" = { action = "<gv"; options = { desc = "Unindent line"; }; };
+              "<" = { action = "<gv"; options = { desc = "Unindent line"; }; };
+              "<Tab>" = { action = ">gv"; options = { desc = "Indent line"; }; };
+              ">" = { action = ">gv"; options = { desc = "Indent line"; }; };
 
-              # move selected line / block of text in visual mode
-              "K" = ":m '<-2<CR>gv=gv";
-              "J" = ":m '>+1<CR>gv=gv";
+              # Move selected line/block in visual mode
+              "K" = { action = ":m '<-2<CR>gv=gv"; options = { }; };
+              "J" = { action = ":m '>+1<CR>gv=gv"; options = { }; };
             };
       in
       config.nixvim.helpers.keymaps.mkKeymaps
--- modules/home/cli-apps/neovim/plugins/telescope.nix
+++ modules/home/cli-apps/neovim/plugins/telescope.nix
@@ -4,17 +4,31 @@
       enable = true;
 
       keymaps = {
-        # Find files using Telescope command-line sugar.
-        "<leader>ff" = "find_files";
-        "<leader>fg" = "live_grep";
-        "<leader>b" = "buffers";
-        "<leader>fh" = "help_tags";
-        "<leader>fd" = "diagnostics";
-
-        # FZF like bindings
-        "<C-p>" = "git_files";
-        "<leader>p" = "oldfiles";
-        "<C-f>" = "live_grep";
+        "<leader>gb" = { action = "git_branches"; desc = ""; };
+        "<leader>gc" = { action = "git_commits"; desc = ""; };
+        "<leader>gC" = { action = "git_bcommits"; desc = ""; };
+        "<leader>gt" = { action = "git_status"; desc = ""; };
+        "<leader>f<CR>" = { action = "resume"; desc = ""; };
+        "<leader>f'" = { action = "marks"; desc = ""; };
+        "<leader>f/" = { action = "current_buffer_fuzzy_find"; desc = ""; };
+        # "<leader>fa" = { action = "find_files prompt_title=Config Files cwd=${vim.fn.stdpath('config')} follow=true"; desc = ""; };
+        "<leader>fb" = { action = "buffers"; desc = ""; };
+        "<leader>fc" = { action = "grep_string"; desc = ""; };
+        "<leader>fC" = { action = "commands"; desc = ""; };
+        "<leader>fd" = { action = "diagnostics"; desc = ""; };
+        "<leader>ff" = { action = "find_files"; desc = ""; };
+        # "<leader>fF" = { action = "find_files hidden=true no_ignore=true"; desc = ""; };
+        # "<leader>fg" = { action = "live_grep"; desc = ""; };
+        "<leader>fh" = { action = "help_tags"; desc = ""; };
+        "<leader>fk" = { action = "keymaps"; desc = ""; };
+        "<leader>fm" = { action = "man_pages"; desc = ""; };
+        # "<leader>fn" = { action = "notify.notify"; desc = ""; };
+        "<leader>fo" = { action = "oldfiles"; desc = ""; };
+        "<leader>fr" = { action = "registers"; desc = ""; };
+        # "<leader>ft" = { action = "colorscheme enable_preview=true"; desc = ""; };
+        "<leader>fw" = { action = "live_grep"; desc = ""; };
+        # "<leader>fW" = { action = "live_grep { additional_args = function(args) return vim.list_extend(args, { \"--hidden\", \"--no-ignore\" }) end,}"; desc = ""; };
+        # "<leader>ls" = { action = "aerial.aerial"; desc = ""; };
       };
 
       keymapsSilent = true;
