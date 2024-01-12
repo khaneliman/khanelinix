4520129c1471b3c5ebdfab6ad91ba7fc5d82fe6d
Austin Horstman <khaneliman12@gmail.com>
2024-01-12 08:32:47 -0600

    chore: waybar overlay changed to upstream

--- overlays/waybar/default.nix
+++ overlays/waybar/default.nix
@@ -2,9 +2,9 @@ _: _final: prev: {
   waybar = prev.waybar.overrideAttrs (old: {
     version = "unstable-waybar";
     src = prev.fetchFromGitHub {
-      owner = "Syndelis";
+      owner = "Alexays";
       repo = "waybar";
-      rev = "9e08512927d2de34a55281ee7fc3f13c36c6c9c5";
+      rev = "f744d906becbf6d06a3d95fee37af5c32061d1aa";
       sha256 = "sha256-7hyOmMTkvQ1a7yQ4YA5Kheg9FLUAPV3otvDj7kGKgko=";
     };
 
