6b45d68a6b058bbd7076863821a1027decd1d674
Austin Horstman <khaneliman12@gmail.com>
2024-01-27 17:47:27 -0600

    feat: sketchybar 2.20.0

--- modules/home/cli-apps/neovim/plugins/treesitter.nix
+++ modules/home/cli-apps/neovim/plugins/treesitter.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{ config, ... }: {
   programs.nixvim.plugins = {
     treesitter = {
       enable = true;
--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,5 +1,14 @@
 _: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
+    version = "2.20.0";
+
+    src = prev.fetchFromGitHub {
+      owner = "FelixKratz";
+      repo = "SketchyBar";
+      rev = "v2.20.0";
+      hash = "sha256-Nyje2F2OXtojbAGNgGPGdX8SEH75SWWpBKLFULb96n4=";
+    };
+
     # Create secondary sketchybar executable for dynamic island
     installPhase = /* bash */ ''
       ${oldAttrs.installPhase}
