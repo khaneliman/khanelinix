70925b4cd806c6211560dd4ef8fd2ca2aaacba10
Austin Horstman <khaneliman12@gmail.com>
2024-01-27 22:22:24 -0600

    chore: update telescope keymappings

--- modules/home/cli-apps/neovim/plugins/telescope.nix
+++ modules/home/cli-apps/neovim/plugins/telescope.nix
@@ -4,29 +4,30 @@
       enable = true;
 
       keymaps = {
-        "<leader>gb" = { action = "git_branches"; desc = ""; };
-        "<leader>gc" = { action = "git_commits"; desc = ""; };
-        "<leader>gC" = { action = "git_bcommits"; desc = ""; };
-        "<leader>gt" = { action = "git_status"; desc = ""; };
-        "<leader>f<CR>" = { action = "resume"; desc = ""; };
-        "<leader>f'" = { action = "marks"; desc = ""; };
-        "<leader>f/" = { action = "current_buffer_fuzzy_find"; desc = ""; };
+        "<leader>f'" = { action = "marks"; desc = "View marks"; };
+        "<leader>f/" = { action = "current_buffer_fuzzy_find"; desc = "Fuzzy find in current buffer"; };
+        "<leader>fC" = { action = "commands"; desc = "View commands"; };
+        "<leader>fb" = { action = "buffers"; desc = "View buffers"; };
+        "<leader>fc" = { action = "grep_string"; desc = "Grep string"; };
+        "<leader>fd" = { action = "diagnostics"; desc = "View diagnostics"; };
+        "<leader>ff" = { action = "find_files"; desc = "Find files"; };
+        "<leader>gC" = { action = "git_bcommits"; desc = "View git bcommits"; };
+        "<leader>gb" = { action = "git_branches"; desc = "View git branches"; };
+        "<leader>gc" = { action = "git_commits"; desc = "View git commits"; };
+        "<leader>gt" = { action = "git_status"; desc = "View git status"; };
+        "<leader>fh" = { action = "help_tags"; desc = "View help tags"; };
+        "<leader>fk" = { action = "keymaps"; desc = "View keymaps"; };
+        "<leader>fm" = { action = "man_pages"; desc = "View man pages"; };
+        "<leader>fo" = { action = "oldfiles"; desc = "View old files"; };
+        "<leader>fr" = { action = "registers"; desc = "View registers"; };
+        "<leader>f<CR>" = { action = "resume"; desc = "Resume action"; };
+        "<leader>fw" = { action = "live_grep"; desc = "Live grep"; };
+
+        # TODO: map these
         # "<leader>fa" = { action = "find_files prompt_title=Config Files cwd=${vim.fn.stdpath('config')} follow=true"; desc = ""; };
-        "<leader>fb" = { action = "buffers"; desc = ""; };
-        "<leader>fc" = { action = "grep_string"; desc = ""; };
-        "<leader>fC" = { action = "commands"; desc = ""; };
-        "<leader>fd" = { action = "diagnostics"; desc = ""; };
-        "<leader>ff" = { action = "find_files"; desc = ""; };
         # "<leader>fF" = { action = "find_files hidden=true no_ignore=true"; desc = ""; };
-        # "<leader>fg" = { action = "live_grep"; desc = ""; };
-        "<leader>fh" = { action = "help_tags"; desc = ""; };
-        "<leader>fk" = { action = "keymaps"; desc = ""; };
-        "<leader>fm" = { action = "man_pages"; desc = ""; };
         # "<leader>fn" = { action = "notify.notify"; desc = ""; };
-        "<leader>fo" = { action = "oldfiles"; desc = ""; };
-        "<leader>fr" = { action = "registers"; desc = ""; };
         # "<leader>ft" = { action = "colorscheme enable_preview=true"; desc = ""; };
-        "<leader>fw" = { action = "live_grep"; desc = ""; };
         # "<leader>fW" = { action = "live_grep { additional_args = function(args) return vim.list_extend(args, { \"--hidden\", \"--no-ignore\" }) end,}"; desc = ""; };
         # "<leader>ls" = { action = "aerial.aerial"; desc = ""; };
       };
