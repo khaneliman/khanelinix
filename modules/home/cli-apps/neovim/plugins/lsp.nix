66d8e633489df4509267a7479b4e8a38d40a29cc
Austin Horstman <khaneliman12@gmail.com>
2024-01-27 18:43:02 -0600

    feat: nixvim add lsp servers

--- modules/home/cli-apps/neovim/plugins/lsp.nix
+++ modules/home/cli-apps/neovim/plugins/lsp.nix
@@ -23,9 +23,68 @@
         };
 
         servers = {
-          clangd.enable = true;
-          lua-ls.enable = true;
-          texlab.enable = true;
+          bashls = {
+            enable = true;
+            filetypes = [ "sh" "bash" ];
+          };
+          ccls = {
+            enable = true;
+            filetypes = [ "c" "cpp" "objc" "objcpp" ];
+          };
+          clangd = {
+            enable = true;
+            filetypes = [ "c" "cpp" "objc" "objcpp" ];
+          };
+          csharp-ls = {
+            enable = true;
+            filetypes = [ "cs" ];
+          };
+          dockerls = {
+            enable = true;
+            filetypes = [ "dockerfile" ];
+          };
+          eslint = {
+            enable = true;
+            filetypes = [ "javascript" "javascriptreact" "typescript" "typescriptreact" ];
+          };
+          html = {
+            enable = true;
+            filetypes = [ "html" ];
+          };
+          java-language-server = {
+            enable = true;
+            filetypes = [ "java" ];
+          };
+          jsonls = {
+            enable = true;
+            filetypes = [ "json" ];
+          };
+          lua-ls = {
+            enable = true;
+            filetypes = [ "lua" ];
+          };
+          nil_ls = {
+            enable = true;
+            filetypes = [ "nix" ];
+          };
+          pyright = {
+            enable = true;
+            filetypes = [ "python" ];
+          };
+          rust-analyzer = {
+            enable = true;
+            filetypes = [ "rust" ];
+            installCargo = true;
+            installRustc = true;
+          };
+          tsserver = {
+            enable = true;
+            filetypes = [ "javascript" "javascriptreact" "typescript" "typescriptreact" ];
+          };
+          yamlls = {
+            enable = true;
+            filetypes = [ "yaml" ];
+          };
         };
       };
     };
