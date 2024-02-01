f2b43bffd2b93872f9a69fa4fe9fcbb99ad88c79
Austin Horstman <khaneliman12@gmail.com>
2024-01-31 19:12:44 -0600

    nixvim: add indent-blankline

---
+++ modules/home/cli-apps/neovim/plugins/indent-blankline.nix
@@ -0,0 +1,18 @@
+{ ... }: {
+  programs.nixvim.plugins = {
+    indent-blankline = {
+      enable = true;
+      scope.enabled = false;
+
+      #TODO: figure out more transparent highlights
+      indent.highlight = [
+        # "rainbow1"
+        # "rainbow2"
+        # "rainbow3"
+        # "rainbow4"
+        # "rainbow5"
+        # "rainbow6"
+      ];
+    };
+  };
+}
