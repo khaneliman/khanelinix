c7696d6c6591fcfec29bb74491516f7e294d7209
Austin Horstman <khaneliman12@gmail.com>
2024-02-06 18:45:10 -0600

    chore: sketchybar bump

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,12 +1,12 @@
 _: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
-    version = "2.20.0";
+    version = "2.20.1";
 
     src = prev.fetchFromGitHub {
       owner = "FelixKratz";
       repo = "SketchyBar";
-      rev = "v2.20.0";
-      hash = "sha256-Nyje2F2OXtojbAGNgGPGdX8SEH75SWWpBKLFULb96n4=";
+      rev = "v2.20.1";
+      hash = "sha256-dyo4J3Qszq7SSXdAXmgm7xFhWE5Eqtr7yZpNGmtuphY=";
     };
 
     # Create secondary sketchybar executable for dynamic island
