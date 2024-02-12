42597cf64e3dc4d9a79544aa218a941f03738982
Austin Horstman <khaneliman12@gmail.com>
2024-02-11 20:55:18 -0600

    sketchybar: add logFile output

--- modules/darwin/desktop/addons/sketchybar/default.nix
+++ modules/darwin/desktop/addons/sketchybar/default.nix
@@ -5,21 +5,26 @@
 , ...
 }:
 let
-  inherit (lib) mkIf;
-  inherit (lib.internal) mkBoolOpt;
+  inherit (lib) mkIf types;
+  inherit (lib.internal) mkBoolOpt mkOpt;
 
   cfg = config.khanelinix.desktop.addons.sketchybar;
-
 in
 {
   options.khanelinix.desktop.addons.sketchybar = {
     enable = mkBoolOpt false "Whether or not to enable sketchybar.";
+    logFile = mkOpt types.str "/var/tmp/sketchybar.log" "Filepath of log output";
   };
 
   config = mkIf cfg.enable {
     homebrew = {
       brews = [ "cava" "jq" ];
       casks = [ "background-music" ];
+    };
+
+    launchd.user.agents.sketchybar = {
+      serviceConfig.StandardErrorPath = cfg.logFile;
+      serviceConfig.StandardOutPath = cfg.logFile;
     };
 
     services.sketchybar = {
