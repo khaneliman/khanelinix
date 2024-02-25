343499e104d7724196df4eca073fbc409d8cb6d8
Austin Horstman <khaneliman12@gmail.com>
2024-02-24 22:09:33 -0600

    sketchybar: 2.20.1 -> master

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,12 +1,12 @@
 _: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
-    version = "2.20.1";
+    version = "2.20.1-unstable";
 
     src = prev.fetchFromGitHub {
       owner = "FelixKratz";
       repo = "SketchyBar";
-      rev = "v2.20.1";
-      hash = "sha256-dyo4J3Qszq7SSXdAXmgm7xFhWE5Eqtr7yZpNGmtuphY=";
+      rev = "5e7b93221cad6c9704d7f34711ec013cddf58881";
+      hash = "sha256-MlNnkff4sD/GkM1m+FH8ZtbNymgvxNOCoLWolwFEXTE=";
     };
 
     # Create secondary sketchybar executable for dynamic island
