dcc4d40af8ec26c1bfbde76a8548073eb633fb87
Austin Horstman <khaneliman12@gmail.com>
2024-02-27 13:06:00 -0600

    business: add teams

--- modules/darwin/suites/business/default.nix
+++ modules/darwin/suites/business/default.nix
@@ -21,6 +21,7 @@ in
       dooit
       jrnl
       nb
+      teams
     ];
 
     homebrew = {
--- modules/nixos/suites/business/default.nix
+++ modules/nixos/suites/business/default.nix
@@ -19,6 +19,7 @@ in
     environment.systemPackages = with pkgs; [
       dooit
       libreoffice
+      teams-for-linux
     ];
 
     khanelinix = {
