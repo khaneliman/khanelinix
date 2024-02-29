4bef106d39ac67a64c70e582de49611941d55535
Austin Horstman <khaneliman12@gmail.com>
2024-02-28 19:28:03 -0600

    sketchybar: commit bump

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -5,8 +5,8 @@ _: _final: prev: {
     src = prev.fetchFromGitHub {
       owner = "FelixKratz";
       repo = "SketchyBar";
-      rev = "4c457077c92c8616dd2525388df47bf42560fdcf";
-      hash = "sha256-xIwmlQtLBfH8ljHZWOF/Wvw15s6FOhhYgV96VK4qL4U=";
+      rev = "a9775b4fed95bc1c7026435a3df1d7b17fa67b42";
+      hash = "sha256-Kfch7Etp7ntuQjmmCHVSlOcnrILbp3BNffaskqPcmYQ=";
     };
 
     # Create secondary sketchybar executable for dynamic island
