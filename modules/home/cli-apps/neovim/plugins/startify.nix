d01d13cbff763bde79f079d9722ec393c1f6eeb0
Austin Horstman <khaneliman12@gmail.com>
2024-03-09 00:04:39 -0600

    nixvim: startify tweaks

--- modules/home/cli-apps/neovim/plugins/startify.nix
+++ modules/home/cli-apps/neovim/plugins/startify.nix
@@ -2,6 +2,10 @@ _: {
   programs.nixvim.plugins.startify = {
     enable = true;
 
+    bookmarks = [
+      "~/khanelinix/flake.nix"
+    ];
+
     customHeader = [
       "   ██╗  ██╗██╗  ██╗ █████╗ ███╗   ██╗███████╗██╗     ██╗███╗   ██╗██╗██╗  ██╗"
       "   ██║ ██╔╝██║  ██║██╔══██╗████╗  ██║██╔════╝██║     ██║████╗  ██║██║╚██╗██╔╝"
@@ -12,7 +16,12 @@ _: {
     ];
 
     # When opening a file or bookmark, change to its directory.
-    changeToDir = false;
+    changeToDir = true;
+
+    # When opening a file or bookmark, change to the root directory of the VCS
+    changeToVcsRoot = true;
+
+    relativePath = true;
 
     # By default, the fortune header uses ASCII characters, because they work for everyone.
     # If you set this option to 1 and your 'encoding' is "utf-8", Unicode box-drawing characters will
