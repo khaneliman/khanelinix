2e20fd5a2028c53c51486df25ae07c967ae8346f
Austin Horstman <khaneliman12@gmail.com>
2024-03-09 23:38:02 -0600

    nixvim: barbar tweak

--- modules/home/cli-apps/neovim/plugins/barbar.nix
+++ modules/home/cli-apps/neovim/plugins/barbar.nix
@@ -1,12 +1,16 @@
 _: {
   programs.nixvim.plugins.barbar = {
     enable = true;
+
+    insertAtEnd = true;
+
     keymaps = {
       silent = true;
 
       next = "<TAB>";
       previous = "<S-TAB>";
       close = "<C-w>";
+      pin = "<C-p>";
     };
   };
 }
