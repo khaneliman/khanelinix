59c0dd6e3d7fe92814310b975aa003cb67e4d69e
Austin Horstman <khaneliman12@gmail.com>
2024-03-10 23:48:18 -0500

    nixvim: telescope keybinding updates

--- modules/home/cli-apps/neovim/plugins/telescope.nix
+++ modules/home/cli-apps/neovim/plugins/telescope.nix
@@ -4,6 +4,86 @@
   ];
 
   programs.nixvim = {
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>fc";
+        lua = true;
+        action = /*lua*/ ''
+          function()
+            require("telescope.builtin").find_files {
+              prompt_title = "Config Files",
+              cwd = vim.fn.stdpath "config",
+              follow = true,
+            }
+          end
+        '';
+        options = {
+          desc = "Find config files";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fF";
+        lua = true;
+        action = /*lua*/ ''
+          function()
+            require("telescope.builtin").find_files({ hidden = true, no_ignore = true})
+          end
+        '';
+        options = {
+          desc = "Find all files";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fn";
+        lua = true;
+        action = /*lua*/ ''
+          function()
+            require("telescope").extensions.notify.notify()
+          end
+        '';
+        options = {
+          desc = "Find notifications";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>ft";
+        lua = true;
+        action = /*lua*/ ''
+          function()
+            require("telescope.builtin").colorscheme({ enable_preview = true })
+          end
+        '';
+        options = {
+          desc = "Find theme";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fW";
+        lua = true;
+        action = /*lua*/ ''
+          function()
+            require("telescope.builtin").live_grep {
+              additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+            }
+          end
+        '';
+        options = {
+          desc = "Find words in all files";
+          silent = true;
+        };
+      }
+    ];
+
     plugins.project-nvim.enable = true;
 
     plugins.telescope = {
@@ -31,30 +111,23 @@
       keymaps = {
         "<leader>f'" = { action = "marks"; desc = "View marks"; };
         "<leader>f/" = { action = "current_buffer_fuzzy_find"; desc = "Fuzzy find in current buffer"; };
+        "<leader>f<CR>" = { action = "resume"; desc = "Resume action"; };
         "<leader>fC" = { action = "commands"; desc = "View commands"; };
         "<leader>fb" = { action = "buffers"; desc = "View buffers"; };
         "<leader>fc" = { action = "grep_string"; desc = "Grep string"; };
         "<leader>fd" = { action = "diagnostics"; desc = "View diagnostics"; };
         "<leader>ff" = { action = "find_files"; desc = "Find files"; };
-        "<leader>gC" = { action = "git_bcommits"; desc = "View git bcommits"; };
-        "<leader>gb" = { action = "git_branches"; desc = "View git branches"; };
-        "<leader>gc" = { action = "git_commits"; desc = "View git commits"; };
-        "<leader>gt" = { action = "git_status"; desc = "View git status"; };
         "<leader>fh" = { action = "help_tags"; desc = "View help tags"; };
         "<leader>fk" = { action = "keymaps"; desc = "View keymaps"; };
         "<leader>fm" = { action = "man_pages"; desc = "View man pages"; };
         "<leader>fo" = { action = "oldfiles"; desc = "View old files"; };
         "<leader>fr" = { action = "registers"; desc = "View registers"; };
-        "<leader>f<CR>" = { action = "resume"; desc = "Resume action"; };
+        "<leader>fs" = { action = "lsp_document_symbols"; desc = "Search symbols"; };
         "<leader>fw" = { action = "live_grep"; desc = "Live grep"; };
-
-        # TODO: map these
-        # "<leader>fa" = { action = "find_files prompt_title=Config Files cwd=${vim.fn.stdpath('config')} follow=true"; desc = ""; };
-        # "<leader>fF" = { action = "find_files hidden=true no_ignore=true"; desc = ""; };
-        # "<leader>fn" = { action = "notify.notify"; desc = ""; };
-        # "<leader>ft" = { action = "colorscheme enable_preview=true"; desc = ""; };
-        # "<leader>fW" = { action = "live_grep { additional_args = function(args) return vim.list_extend(args, { \"--hidden\", \"--no-ignore\" }) end,}"; desc = ""; };
-        # "<leader>ls" = { action = "aerial.aerial"; desc = ""; };
+        "<leader>gC" = { action = "git_bcommits"; desc = "View git bcommits"; };
+        "<leader>gb" = { action = "git_branches"; desc = "View git branches"; };
+        "<leader>gc" = { action = "git_commits"; desc = "View git commits"; };
+        "<leader>gt" = { action = "git_status"; desc = "View git status"; };
       };
 
       keymapsSilent = true;
