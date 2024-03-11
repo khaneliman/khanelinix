056806d78c7de35bea426b67dc929d8a7e3166de
Austin Horstman <khaneliman12@gmail.com>
2024-03-11 13:27:13 -0500

    security: add sudo-rs

---
+++ modules/nixos/security/sudo-rs/default.nix
@@ -0,0 +1,33 @@
+{ config
+, lib
+, pkgs
+, options
+, ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.internal) mkBoolOpt;
+
+  cfg = config.khanelinix.security.sudo-rs;
+in
+{
+  options.khanelinix.security.sudo-rs = {
+    enable = mkBoolOpt false "Whether or not to replace sudo with sudo-rs.";
+  };
+
+  config = mkIf cfg.enable {
+    security.sudo-rs = {
+      enable = true;
+      package = pkgs.sudo-rs;
+
+      wheelNeedsPassword = false;
+
+      # extraRules = [
+      #   {
+      #     noPass = true;
+      #     users = [ config.khanelinix.user.name ];
+      #   }
+      # ];
+    };
+  };
+}
