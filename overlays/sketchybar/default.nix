4faf0182fab5ac9dedc2eb944b6dd5c21778e070
Austin Horstman <khaneliman12@gmail.com>
2024-03-14 08:01:46 -0500

    sketchybar: 2.20.1-unstable -> 2.21.0

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,11 +1,11 @@
 _: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
-    version = "2.20.1-unstable";
+    version = "2.21.0";
 
     src = prev.fetchFromGitHub {
       owner = "FelixKratz";
       repo = "SketchyBar";
-      rev = "0cc53dc1d05c98ede829178fff1f329005485011";
+      rev = "v2.21.0";
       hash = "sha256-AJKYW7XRZCgK4pafXs97/iu6yUz7lunmQWygTwah7/4=";
     };
 
