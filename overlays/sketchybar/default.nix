cc9d300b6b561b32282bb0ca3edc2e3391da7a8d
Austin Horstman <khaneliman12@gmail.com>
2024-03-15 22:10:52 -0500

    chore: remove sketchybar version overlay

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,14 +1,5 @@
 _: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
-    version = "2.21.0";
-
-    src = prev.fetchFromGitHub {
-      owner = "FelixKratz";
-      repo = "SketchyBar";
-      rev = "v2.21.0";
-      hash = "sha256-AJKYW7XRZCgK4pafXs97/iu6yUz7lunmQWygTwah7/4=";
-    };
-
     # Create secondary sketchybar executable for dynamic island
     installPhase = /* bash */ ''
       ${oldAttrs.installPhase}
