913d17698e25c9c5c9ab90dfd65382c30f9cb87c
Austin Horstman <khaneliman12@gmail.com>
2024-03-24 16:45:57 -0500

    lualine: add aerial

--- modules/home/cli-apps/neovim/plugins/lualine.nix
+++ modules/home/cli-apps/neovim/plugins/lualine.nix
@@ -41,6 +41,32 @@ _: {
         "fileformat"
         "filetype"
       ];
+
+      lualine_y = [
+        {
+          name = "aerial";
+          extraConfig = {
+            # -- The separator to be used to separate symbols in status line.
+            sep = " ) ";
+
+            # -- The number of symbols to render top-down. In order to render only 'N' last
+            # -- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
+            # -- be used in order to render only current symbol.
+            depth.__raw = "nil";
+
+            # -- When 'dense' mode is on, icons are not rendered near their symbols. Only
+            # -- a single icon that represents the kind of current symbol is rendered at
+            # -- the beginning of status line.
+            dense = false;
+
+            # -- The separator to be used to separate symbols in dense mode.
+            dense_sep = ".";
+
+            # -- Color the symbol icons.
+            colored = true;
+          };
+        }
+      ];
     };
   };
 }
