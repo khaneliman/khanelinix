008a3f54b4a8e4f5c7f7f931804ced6acea4088b
Austin Horstman <khaneliman12@gmail.com>
2024-03-25 20:54:29 -0500

    flake.lock: update

--- modules/home/cli-apps/neovim/plugins/indent-blankline.nix
+++ modules/home/cli-apps/neovim/plugins/indent-blankline.nix
@@ -2,17 +2,20 @@ _: {
   programs.nixvim.plugins = {
     indent-blankline = {
       enable = true;
-      scope.enabled = false;
 
-      #TODO: figure out more transparent highlights
-      indent.highlight = [
-        # "rainbow1"
-        # "rainbow2"
-        # "rainbow3"
-        # "rainbow4"
-        # "rainbow5"
-        # "rainbow6"
-      ];
+      settings = {
+        scope.enabled = false;
+
+        #TODO: figure out more transparent highlights
+        indent.highlight = [
+          # "rainbow1"
+          # "rainbow2"
+          # "rainbow3"
+          # "rainbow4"
+          # "rainbow5"
+          # "rainbow6"
+        ];
+      };
     };
   };
 }
