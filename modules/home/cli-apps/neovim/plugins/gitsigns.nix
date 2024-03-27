efa4958a9a0fb382c2c418c5d9e105d04a3dd6f3
Austin Horstman <khaneliman12@gmail.com>
2024-03-26 22:15:34 -0500

    gitsigns: add line blame

--- modules/home/cli-apps/neovim/plugins/gitsigns.nix
+++ modules/home/cli-apps/neovim/plugins/gitsigns.nix
@@ -5,6 +5,11 @@ _: {
         enable = true;
 
         settings = {
+          current_line_blame = true;
+          current_line_blame_opts = {
+            virt_text = true;
+            virt_text_pos = "eol";
+          };
           signs = {
             add.text = "+";
             change.text = "~";
