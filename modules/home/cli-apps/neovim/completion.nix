3df7ab1783c7b152726f0f684bb34a29582350c3
Austin Horstman <khaneliman12@gmail.com>
2024-03-29 22:50:56 -0500

    cmp: refine completion selection and insertion

--- modules/home/cli-apps/neovim/completion.nix
+++ modules/home/cli-apps/neovim/completion.nix
@@ -13,10 +13,12 @@ _: {
             "<C-f>" = "cmp.mapping.scroll_docs(4)";
             "<C-Space>" = "cmp.mapping.complete()";
             "<C-e>" = "cmp.mapping.close()";
-            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), {'i', 's'})";
+            "<CR>" = "cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace })";
           };
+
+          preselect = "cmp.PreselectMode.None";
 
           snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
 
