77c8552b949c7e3293b6d45934d2a06357a33f35
Austin Horstman <khaneliman12@gmail.com>
2024-04-03 22:02:08 -0500

    efm: add linters

--- modules/home/cli-apps/neovim/plugins/efm.nix
+++ modules/home/cli-apps/neovim/plugins/efm.nix
@@ -1,10 +1,11 @@
 _: {
   programs.nixvim.plugins = {
+
     lsp.servers.efm = {
       enable = true;
       extraOptions.init_options = {
-        documentFormatting = true;
-        documentRangeFormatting = true;
+        documentFormatting = false;
+        documentRangeFormatting = false;
         hover = true;
         documentSymbol = true;
         codeAction = true;
@@ -12,6 +13,29 @@ _: {
       };
     };
 
-    efmls-configs.enable = true;
+    efmls-configs = {
+      enable = true;
+
+      setup = {
+        all.linter = [ "codespell" ];
+        bash.linter = [ "shellcheck" ];
+        c.linter = [ "clang_tidy" ];
+        "c++".linter = [ "clang_tidy" "cppcheck" "cpplint" ];
+        "c#".linter = [ "mcs" ];
+        css.linter = [ "stylelint" ];
+        gitcommit.linter = [ "gitlint" ];
+        javascript.linter = [ "eslint_d" ];
+        json.linter = [ "jq" ];
+        lua.linter = [ "luacheck" ];
+        markdown.linter = [ "markdownlint" ];
+        nix.linter = [ "statix" ];
+        python.linter = [ "pylint" ];
+        scss.linter = [ "stylelint" ];
+        sh.linter = [ "shellcheck" ];
+        sql.linter = [ "sqlfluff" ];
+        typecript.linter = [ "eslint_d" ];
+        yaml.linter = [ "yamllint" ];
+      };
+    };
   };
 }
