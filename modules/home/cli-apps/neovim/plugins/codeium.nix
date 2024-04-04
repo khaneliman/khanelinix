66b00da05927cbdc41ed5fb3a9d0d6c8fe6dfd66
Austin Horstman <khaneliman12@gmail.com>
2024-04-04 17:37:26 -0500

    codeium: add plugin settings

---
+++ modules/home/cli-apps/neovim/plugins/codeium.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+{
+  programs.nixvim = {
+    plugins = {
+      codeium-nvim = {
+        enable = true;
+
+        extraOptions = {
+          enable_chat = true;
+        };
+
+        tools = {
+          curl = lib.getExe pkgs.curl;
+          gzip = lib.getExe pkgs.gzip;
+          uname = lib.getExe' pkgs.coreutils "uname";
+          uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
+        };
+      };
+    };
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>uc";
+        action = ":Codeium Chat<CR>";
+        options = {
+          desc = "Toggle Codeium Chat";
+          silent = true;
+        };
+      }
+    ];
+  };
+}
