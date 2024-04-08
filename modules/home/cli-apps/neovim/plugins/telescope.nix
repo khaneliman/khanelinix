2ad6a162ac78ffc45e5ab6d2575c08514923092a
Austin Horstman <khaneliman12@gmail.com>
2024-04-07 19:55:06 -0500

    flake.lock: update

--- modules/home/cli-apps/neovim/plugins/telescope.nix
+++ modules/home/cli-apps/neovim/plugins/telescope.nix
@@ -92,9 +92,11 @@
       enable = true;
 
       extensions = {
-        file_browser = {
+        file-browser = {
           enable = true;
-          hidden = true;
+          settings = {
+            hidden = true;
+          };
         };
 
         # FIX: annoying frecency validation on startup about removed files
@@ -108,41 +110,43 @@
       };
 
       keymaps = {
-        "<leader>f'" = { action = "marks"; desc = "View marks"; };
-        "<leader>f/" = { action = "current_buffer_fuzzy_find"; desc = "Fuzzy find in current buffer"; };
-        "<leader>f<CR>" = { action = "resume"; desc = "Resume action"; };
-        "<leader>fa" = { action = "autocommands"; desc = "View autocommands"; };
-        "<leader>fC" = { action = "commands"; desc = "View commands"; };
-        "<leader>fb" = { action = "buffers"; desc = "View buffers"; };
-        "<leader>fc" = { action = "grep_string"; desc = "Grep string"; };
-        "<leader>fd" = { action = "diagnostics"; desc = "View diagnostics"; };
-        "<leader>ff" = { action = "find_files"; desc = "Find files"; };
-        "<leader>fh" = { action = "help_tags"; desc = "View help tags"; };
-        "<leader>fk" = { action = "keymaps"; desc = "View keymaps"; };
-        "<leader>fm" = { action = "man_pages"; desc = "View man pages"; };
-        "<leader>fo" = { action = "oldfiles"; desc = "View old files"; };
-        "<leader>fr" = { action = "registers"; desc = "View registers"; };
-        "<leader>fs" = { action = "lsp_document_symbols"; desc = "Search symbols"; };
-        "<leader>fw" = { action = "live_grep"; desc = "Live grep"; };
-        "<leader>gC" = { action = "git_bcommits"; desc = "View git bcommits"; };
-        "<leader>gb" = { action = "git_branches"; desc = "View git branches"; };
-        "<leader>gc" = { action = "git_commits"; desc = "View git commits"; };
-        "<leader>gs" = { action = "git_status"; desc = "View git status"; };
-        "<leader>gS" = { action = "git_stash"; desc = "View git stashes"; };
+        "<leader>f'" = { action = "marks"; options.desc = "View marks"; };
+        "<leader>f/" = { action = "current_buffer_fuzzy_find"; options.desc = "Fuzzy find in current buffer"; };
+        "<leader>f<CR>" = { action = "resume"; options.desc = "Resume action"; };
+        "<leader>fa" = { action = "autocommands"; options.desc = "View autocommands"; };
+        "<leader>fC" = { action = "commands"; options.desc = "View commands"; };
+        "<leader>fb" = { action = "buffers"; options.desc = "View buffers"; };
+        "<leader>fc" = { action = "grep_string"; options.desc = "Grep string"; };
+        "<leader>fd" = { action = "diagnostics"; options.desc = "View diagnostics"; };
+        "<leader>ff" = { action = "find_files"; options.desc = "Find files"; };
+        "<leader>fh" = { action = "help_tags"; options.desc = "View help tags"; };
+        "<leader>fk" = { action = "keymaps"; options.desc = "View keymaps"; };
+        "<leader>fm" = { action = "man_pages"; options.desc = "View man pages"; };
+        "<leader>fo" = { action = "oldfiles"; options.desc = "View old files"; };
+        "<leader>fr" = { action = "registers"; options.desc = "View registers"; };
+        "<leader>fs" = { action = "lsp_document_symbols"; options.desc = "Search symbols"; };
+        "<leader>fw" = { action = "live_grep"; options.desc = "Live grep"; };
+        "<leader>gC" = { action = "git_bcommits"; options.desc = "View git bcommits"; };
+        "<leader>gb" = { action = "git_branches"; options.desc = "View git branches"; };
+        "<leader>gc" = { action = "git_commits"; options.desc = "View git commits"; };
+        "<leader>gs" = { action = "git_status"; options.desc = "View git status"; };
+        "<leader>gS" = { action = "git_stash"; options.desc = "View git stashes"; };
       };
 
       keymapsSilent = true;
 
-      defaults = {
-        file_ignore_patterns = [
-          "^.git/"
-          "^.mypy_cache/"
-          "^__pycache__/"
-          "^output/"
-          "^data/"
-          "%.ipynb"
-        ];
-        set_env.COLORTERM = "truecolor";
+      settings = {
+        defaults = {
+          file_ignore_patterns = [
+            "^.git/"
+            "^.mypy_cache/"
+            "^__pycache__/"
+            "^output/"
+            "^data/"
+            "%.ipynb"
+          ];
+          set_env.COLORTERM = "truecolor";
+        };
       };
     };
   };
