e2199110b1493d31faef9e4adfb015895f4daeaf
Austin Horstman <khaneliman12@gmail.com>
2024-04-07 21:33:03 -0500

    mini: replace startify with mini.starter

--- modules/home/cli-apps/neovim/plugins/mini.nix
+++ modules/home/cli-apps/neovim/plugins/mini.nix
@@ -8,6 +8,7 @@ _: {
         bracketed = { };
         bufremove = { };
         indentscope = { };
+
         map = {
           # __raw = lua code
           # __unkeyed.* = no key, just the value
@@ -24,6 +25,33 @@ _: {
             winblend = 0;
           };
         };
+
+        starter = {
+          header = ''
+            ██╗  ██╗██╗  ██╗ █████╗ ███╗   ██╗███████╗██╗     ██╗███╗   ██╗██╗██╗  ██╗
+            ██║ ██╔╝██║  ██║██╔══██╗████╗  ██║██╔════╝██║     ██║████╗  ██║██║╚██╗██╔╝
+            █████╔╝ ███████║███████║██╔██╗ ██║█████╗  ██║     ██║██╔██╗ ██║██║ ╚███╔╝
+            ██╔═██╗ ██╔══██║██╔══██║██║╚██╗██║██╔══╝  ██║     ██║██║╚██╗██║██║ ██╔██╗
+            ██║  ██╗██║  ██║██║  ██║██║ ╚████║███████╗███████╗██║██║ ╚████║██║██╔╝ ██╗
+            ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝
+          '';
+
+          evaluate_single = true;
+
+          items = {
+            "__unkeyed.buildtin_actions".__raw = "require('mini.starter').sections.builtin_actions()";
+            "__unkeyed.recent_files_current_directory".__raw = "require('mini.starter').sections.recent_files(10, true)";
+            "__unkeyed.recent_files".__raw = "require('mini.starter').sections.recent_files(10, false)";
+            "__unkeyed.sessions".__raw = "require('mini.starter').sections.sessions(5, true)";
+          };
+
+          content_hooks = {
+            "__unkeyed.adding_bullet".__raw = "require('mini.starter').gen_hook.adding_bullet()";
+            "__unkeyed.indexing".__raw = "require('mini.starter').gen_hook.indexing('all', { 'Builtin actions' })";
+            "__unkeyed.padding".__raw = "require('mini.starter').gen_hook.aligning('center', 'center')";
+          };
+        };
+
         surround = { };
       };
     };
