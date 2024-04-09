8a8bebdf673b6f295307aba3f0c65e06d300e150
Austin Horstman <khaneliman12@gmail.com>
2024-04-09 09:34:14 -0500

    zsh: tweak completions

--- modules/home/system/shell/zsh/default.nix
+++ modules/home/system/shell/zsh/default.nix
@@ -26,9 +26,22 @@ in
         enable = true;
         package = pkgs.zsh;
 
+        autocd = true;
+
         completionInit = # bash
           ''
-            zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+            # case insensitive tab completion
+            zstyle ':completion:*' completer _complete _ignored _approximate
+            zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+            zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+            zstyle ':completion:*' menu select
+            zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+            zstyle ':completion:*' verbose true
+
+            # use cache for completions
+            zstyle ':completion:*' use-cache on
+            zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+            _comp_options+=(globdots)
           '';
 
         autosuggestion = {
@@ -54,14 +67,34 @@ in
             # Improved vim bindings.
             source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 
+            # C-right / C-left for word skips
+            bindkey "^[[1;5C" forward-word
+            bindkey "^[[1;5D" backward-word
+
+            # C-Backspace / C-Delete for word deletions
+            # bindkey "^[[3;5~" forward-kill-word
+            bindkey "^H" backward-kill-word
+
+            # Home/End
+            bindkey "^[[OH" beginning-of-line
+            bindkey "^[[OF" end-of-line
+
             fastfetch
           '';
 
         plugins = [
-          # {
-          #   name = "zsh-autocomplete";
-          #   src = pkgs.zsh-autocomplete;
-          # }
+          {
+            name = "zsh-autocomplete";
+            src = pkgs.zsh-autocomplete;
+          }
+          {
+            name = "zsh-better-npm-completion";
+            src = pkgs.zsh-better-npm-completion;
+          }
+          {
+            name = "zsh-completions";
+            src = pkgs.zsh-completions;
+          }
           {
             name = "zsh-command-time";
             src = pkgs.zsh-command-time;
