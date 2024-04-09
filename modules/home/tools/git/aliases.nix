d0b9600a1f4431f7a8c5aef33fdf1aec3f090490
Austin Horstman <khaneliman12@gmail.com>
2024-04-09 13:35:32 -0500

    git: remove git alias that blocks real command

--- modules/home/tools/git/aliases.nix
+++ modules/home/tools/git/aliases.nix
@@ -235,7 +235,6 @@
     add = "git add";
     commit = "git commit";
     pull = "git pull";
-    stat = "git status";
     gdiff = "git diff HEAD";
     vdiff = "git difftool HEAD";
     log = "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
