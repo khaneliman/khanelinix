1543ad5401e2a2cdddbf1470e9f1df853a5f394f
Austin Horstman <khaneliman12@gmail.com>
2024-04-29 13:00:00 -0500

    boot: add silentBoot config

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -16,6 +16,7 @@ in
     enable = mkBoolOpt false "Whether or not to enable booting.";
     plymouth = mkBoolOpt false "Whether or not to enable plymouth boot splash.";
     secureBoot = mkBoolOpt false "Whether or not to enable secure boot.";
+    silentBoot = mkBoolOpt false "Whether or not to enable silent boot.";
   };
 
   config = mkIf cfg.enable {
@@ -30,7 +31,29 @@ in
       ++ lib.optionals cfg.secureBoot [ sbctl ];
 
     boot = {
-      kernelParams = lib.optionals cfg.plymouth [ "quiet" ];
+      kernelParams =
+        lib.optionals cfg.plymouth [ "quiet" ]
+        ++ lib.optionals cfg.silentBoot [
+          # tell the kernel to not be verbose
+          "quiet"
+
+          # kernel log message level
+          "loglevel=3" # 1: system is unusable | 3: error condition | 7: very verbose
+
+          # udev log message level
+          "udev.log_level=3"
+
+          # lower the udev log level to show only errors or worse
+          "rd.udev.log_level=3"
+
+          # disable systemd status messages
+          # rd prefix means systemd-udev will be used instead of initrd
+          "systemd.show_status=auto"
+          "rd.systemd.show_status=auto"
+
+          # disable the cursor in vt to get a black screen during intermissions
+          "vt.global_cursor_default=0"
+        ];
 
       lanzaboote = mkIf cfg.secureBoot {
         enable = true;
@@ -42,6 +65,8 @@ in
           canTouchEfiVariables = true;
           efiSysMountPoint = "/boot";
         };
+
+        generationsDir.copyKernels = true;
 
         systemd-boot = {
           enable = !cfg.secureBoot;
