f7bfa90d9593deb5347cff1562f5390b15cf32a8
Austin Horstman <khaneliman12@gmail.com>
2024-04-29 08:52:38 -0500

    bluetooth: tweaks

--- modules/nixos/hardware/bluetooth/default.nix
+++ modules/nixos/hardware/bluetooth/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkIf;
   inherit (lib.internal) mkBoolOpt;
@@ -14,12 +19,19 @@ in
     hardware.bluetooth = {
       enable = true;
 
+      package = pkgs.bluez-experimental;
+      powerOnBoot = true;
+
       settings = {
         General = {
           Experimental = true;
+          JustWorksRepairing = "always";
+          MultiProfile = "multiple";
         };
       };
     };
+
+    boot.kernelParams = [ "btusb" ];
 
     services.blueman = {
       enable = true;
