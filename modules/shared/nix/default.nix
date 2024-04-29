f03d297fe68035e3e01633d4e43b1da45fce4e85
Austin Horstman <khaneliman12@gmail.com>
2024-04-29 10:58:07 -0500

    nix: add extra substituters

--- modules/shared/nix/default.nix
+++ modules/shared/nix/default.nix
@@ -49,15 +49,21 @@ in
           sandbox = "relaxed";
           trusted-users = users;
           warn-dirty = false;
+
           substituters = [
             "https://cache.nixos.org"
-            "https://nix-community.cachix.org"
             "https://khanelinix.cachix.org"
+            "https://nix-community.cachix.org"
+            "https://nixpkgs-unfree.cachix.org"
+            "https://numtide.cachix.org"
           ];
+
           trusted-public-keys = [
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "khanelinix.cachix.org-1:FTmbv7OqlMsmJEOFvAlz7PVkoGtstbwLC2OldAiJZ10="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+            "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
           ];
         };
 
