e8fbf5c93f5dbda818ecda441883d5fefa0fec7f
Austin Horstman <khaneliman12@gmail.com>
2024-04-29 08:52:14 -0500

    pipewire: audio enable

--- modules/nixos/hardware/audio/default.nix
+++ modules/nixos/hardware/audio/default.nix
@@ -60,6 +60,7 @@ in
     services.pipewire = {
       enable = true;
       alsa.enable = true;
+      audio.enable = true;
       jack.enable = true;
       pulse.enable = true;
       wireplumber.enable = true;
