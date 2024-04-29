039c2d7940c1c7bb389e2758c4b8d69d533f4f67
Austin Horstman <khaneliman12@gmail.com>
2024-04-29 09:05:31 -0500

    khanelinix/specialization: migrate nixos-hardware nvidia removal to flake

--- systems/x86_64-linux/khanelinix/specializations.nix
+++ systems/x86_64-linux/khanelinix/specializations.nix
@@ -1,12 +1,4 @@
-{
-  inputs,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  inherit (inputs) nixos-hardware;
-in
+{ lib, pkgs, ... }:
 {
   specialisation = {
     # vfio = {
@@ -46,7 +38,30 @@ in
       configuration = {
         system.nixos.tags = [ "no-nvidia" ];
 
-        imports = with nixos-hardware.nixosModules; [ common-gpu-nvidia-disable ];
+        boot.extraModprobeConfig = ''
+          blacklist nouveau
+          options nouveau modeset=0
+        '';
+
+        services.udev.extraRules = ''
+          # Remove NVIDIA USB xHCI Host Controller devices, if present
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+
+          # Remove NVIDIA USB Type-C UCSI devices, if present
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+
+          # Remove NVIDIA Audio devices, if present
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+
+          # Remove NVIDIA VGA/3D controller devices
+          ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+        '';
+        boot.blacklistedKernelModules = [
+          "nouveau"
+          "nvidia"
+          "nvidia_drm"
+          "nvidia_modeset"
+        ];
       };
     };
   };
