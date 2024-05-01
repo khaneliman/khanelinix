3e6fe5b74b093ed502c2d5e1905ba3a27a94f033
Austin Horstman <khaneliman12@gmail.com>
2024-05-01 10:47:35 -0500

    nixos/noisetorch: init module

---
+++ modules/nixos/apps/noisetorch/default.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    mkOption
+    literalExpression
+    types
+    ;
+
+  cfg = config.khanelinix.apps.noisetorch;
+in
+{
+  options.khanelinix.apps.noisetorch = {
+    enable = mkEnableOption "noisetorch service";
+    package = mkOption {
+      type = types.package;
+      default = pkgs.noisetorch;
+      defaultText = literalExpression "pkgs.noisetorch";
+      description = "Which package to use for noisetorch";
+    };
+    threshold = mkOption {
+      type = types.int;
+      default = -1;
+      description = "Voice activation threshold (default -1)";
+    };
+    device = mkOption {
+      type = types.str;
+      description = "Use the specified source/sink device ID";
+    };
+    deviceUnit = mkOption {
+      type = types.str;
+      description = "Systemd device unit which is providing the audio device";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.noisetorch = {
+      enable = true;
+
+      inherit (cfg) package;
+    };
+  };
+}
