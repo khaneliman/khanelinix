e51385047fcc0f05f4acd32b3b337e1085980c55
Austin Horstman <khaneliman12@gmail.com>
2024-05-03 22:44:21 -0500

    nix/hardware: add tpm module

---
+++ modules/nixos/hardware/tpm/default.nix
@@ -0,0 +1,30 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkDefault mkIf;
+  inherit (lib.internal) mkBoolOpt;
+
+  cfg = config.khanelinix.hardware.tpm;
+in
+{
+  options.khanelinix.hardware.tpm = {
+    enable = mkBoolOpt false "Whether or not to enable Trusted Platform Module 2 (TPM2).";
+  };
+
+  config = mkIf cfg.enable {
+    security.tpm2 = {
+      enable = true;
+
+      # enable Trusted Platform 2 userspace resource manager daemon
+      abrmd.enable = mkDefault false;
+
+      # The TCTI is the "Transmission Interface" that is used to communicate with a
+      # TPM. this option sets TCTI environment variables to the specified values if enabled
+      #  - TPM2TOOLS_TCTI
+      #  - TPM2_PKCS11_TCTI
+      tctiEnvironment.enable = mkDefault true;
+
+      # enable TPM2 PKCS#11 tool and shared library in system path
+      pkcs11.enable = mkDefault false;
+    };
+  };
+}
