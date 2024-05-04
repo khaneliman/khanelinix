226487612a42e2d1ae5b26a4664de74d18403839
Austin Horstman <khaneliman12@gmail.com>
2024-05-04 16:10:43 -0500

    system/time: add ntp

--- modules/nixos/system/time/default.nix
+++ modules/nixos/system/time/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkIf;
   inherit (lib.internal) mkBoolOpt;
@@ -7,8 +12,27 @@ let
 in
 {
   options.khanelinix.system.time = {
-    enable = mkBoolOpt false "Whether or not to configure timezone information.";
+    enable = mkBoolOpt false "Whether or not to configure time related settings.";
   };
 
-  config = mkIf cfg.enable { time.timeZone = "America/Chicago"; };
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.openntpd ];
+
+    networking.timeServers = [
+      "0.nixos.pool.ntp.org"
+      "1.nixos.pool.ntp.org"
+      "2.nixos.pool.ntp.org"
+      "3.nixos.pool.ntp.org"
+    ];
+
+    services.openntpd = {
+      enable = true;
+      extraConfig = ''
+        listen on 127.0.0.1
+        listen on ::1
+      '';
+    };
+
+    time.timeZone = "America/Chicago";
+  };
 }
