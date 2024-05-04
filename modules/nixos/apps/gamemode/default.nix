db1434c15364a5612e92b050b6899a5cd2601f2c
Austin Horstman <khaneliman12@gmail.com>
2024-05-03 22:53:01 -0500

    gamemode: renice tweaks

--- modules/nixos/apps/gamemode/default.nix
+++ modules/nixos/apps/gamemode/default.nix
@@ -42,16 +42,34 @@ in
     mkIf cfg.enable {
       programs.gamemode = {
         enable = true;
+        enableRenice = true;
+
         settings = {
           general = {
             softrealtime = "auto";
             renice = 15;
           };
+
           custom = {
             start = startScript.outPath;
             end = endScript.outPath;
           };
         };
+      };
+
+      security.wrappers.gamemode = {
+        owner = "root";
+        group = "root";
+        source = "${getExe' pkgs.gamemode "gamemoderun"}";
+        capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
+      };
+
+      # <https://www.phoronix.com/news/Fedora-39-VM-Max-Map-Count>
+      # <https://github.com/pop-os/default-settings/blob/master_jammy/etc/sysctl.d/10-pop-default-settings.conf>
+      boot.kernel.sysctl = {
+        # default on some gaming (SteamOS) and desktop (Fedora) distributions
+        # might help with gaming performance
+        "vm.max_map_count" = 2147483642;
       };
     };
 }
