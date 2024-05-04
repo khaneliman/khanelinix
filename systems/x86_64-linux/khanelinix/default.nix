b0b82d1254abf0b9f059b79ce02e3e34853f3de6
Austin Horstman <khaneliman12@gmail.com>
2024-05-04 09:09:47 -0500

    khanelinix: add networking address
    Apparently configured in wrong system...

--- systems/x86_64-linux/khanelinix/default.nix
+++ systems/x86_64-linux/khanelinix/default.nix
@@ -209,6 +209,20 @@ in
     max-jobs = 24;
   };
 
+  networking = {
+    defaultGateway = {
+      address = "192.168.1.1";
+      interface = "enp6s0";
+    };
+
+    interfaces.enp6s0.ipv4.addresses = [
+      {
+        address = "192.168.1.3";
+        prefixLength = 24;
+      }
+    ];
+  };
+
   services = {
     displayManager.defaultSession = "hyprland";
     mpd = {
