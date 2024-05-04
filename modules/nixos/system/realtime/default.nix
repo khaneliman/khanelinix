e6adb3c27cf34680f0692b8317ed9f195f3dadba
Austin Horstman <khaneliman12@gmail.com>
2024-05-04 15:55:07 -0500

    nixos/system: add realtime module

---
+++ modules/nixos/system/realtime/default.nix
@@ -0,0 +1,48 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf;
+  inherit (lib.internal) mkBoolOpt;
+
+  cfg = config.khanelinix.system.realtime;
+in
+{
+  options.khanelinix.system.realtime = {
+    enable = mkBoolOpt false "Whether or not to configure xkb.";
+  };
+
+  config = mkIf cfg.enable {
+    # port of https://gitlab.archlinux.org/archlinux/packaging/packages/realtime-privileges
+    # see https://wiki.archlinux.org/title/Realtime_process_management
+    # tldr: realtime processes have higher priority than normal processes
+    # and that's a good thing
+    users = {
+      users."${config.khanelinix.user.name}".extraGroups = [ "realtime" ];
+      groups.realtime = { };
+    };
+
+    services.udev.extraRules = ''
+      KERNEL=="cpu_dma_latency", GROUP="realtime"
+    '';
+
+    security.pam.loginLimits = [
+      {
+        domain = "@realtime";
+        type = "-";
+        item = "rtprio";
+        value = 98;
+      }
+      {
+        domain = "@realtime";
+        type = "-";
+        item = "memlock";
+        value = "unlimited";
+      }
+      {
+        domain = "@realtime";
+        type = "-";
+        item = "nice";
+        value = -11;
+      }
+    ];
+  };
+}
