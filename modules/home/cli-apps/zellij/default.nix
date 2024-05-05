c9acbaf24fd9adfa19a2b3e1f77deae63b0e34f1
Austin Horstman <khaneliman12@gmail.com>
2024-05-04 20:11:13 -0500

    zellij: add custom layout

--- modules/home/cli-apps/zellij/default.nix
+++ modules/home/cli-apps/zellij/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkIf;
   inherit (lib.internal) mkBoolOpt;
@@ -12,13 +17,48 @@ in
 
   config = mkIf cfg.enable {
     programs.zellij = {
-      # enable = true;
+      enable = true;
 
-      enableBashIntegration = true;
-      enableFishIntegration = true;
-      enableZshIntegration = true;
+      # These enable auto starting
+      # enableBashIntegration = true;
+      # enableFishIntegration = true;
+      # enableZshIntegration = true;
 
       settings = {
+        # custom defined layouts
+        layout_dir = "${./layouts}";
+
+        # clipboard provider
+        copy_command =
+          if pkgs.stdenv.isLinux then
+            "wl-copy"
+          else if pkgs.stdenv.isDarwin then
+            "pbcopy"
+          else
+            "";
+
+        auto_layouts = true;
+
+        default_layout = "system"; # or compact
+        default_mode = "locked";
+
+        on_force_close = "quit";
+        pane_frames = true;
+        session_serialization = false;
+
+        ui.pane_frames = {
+          rounded_corners = true;
+          hide_session_name = true;
+        };
+
+        # load internal plugins from built-in paths
+        plugins = {
+          tab-bar.path = "tab-bar";
+          status-bar.path = "status-bar";
+          strider.path = "strider";
+          compact-bar.path = "compact-bar";
+        };
+
         theme = "catppuccin-macchiato";
       };
     };
