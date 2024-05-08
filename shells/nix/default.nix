420850a4d4c3df501c9d536fa2b7f9815941c9af
Austin Horstman <khaneliman12@gmail.com>
2024-05-08 13:43:28 -0500

    shells/nix: init

---
+++ shells/nix/default.nix
@@ -0,0 +1,37 @@
+{
+  inputs,
+  mkShell,
+  pkgs,
+  system,
+  ...
+}:
+let
+  inherit (inputs) snowfall-flake;
+in
+mkShell {
+  buildInputs = with pkgs; [
+    deadnix
+    hydra-check
+    nix-inspect
+    nix-bisect
+    nix-diff
+    nix-health
+    nix-index
+    nix-melt
+    nix-prefetch-git
+    nix-search-cli
+    nix-tree
+    nix-update
+    nixpkgs-hammering
+    nixpkgs-lint
+    nixpkgs-review
+    snowfall-flake.packages.${system}.flake
+    statix
+  ];
+
+  shellHook = ''
+    echo 🔨 Welcome to khanelinix
+
+
+  '';
+}
