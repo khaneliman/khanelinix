68d97b3365603a92825f8d03a084e5071b765e14
Austin Horstman <khaneliman12@gmail.com>
2024-05-09 17:49:36 -0500

    terminal/emulators: add foot

---
+++ modules/home/programs/terminal/emulators/foot/default.nix
@@ -0,0 +1,82 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.internal) mkBoolOpt;
+
+  cfg = config.khanelinix.programs.terminal.emulators.foot;
+  colors = import ../../../theme/catppuccin.nix;
+in
+{
+  options.khanelinix.programs.terminal.emulators.foot = {
+    enable = mkBoolOpt false "Whether or not to enable foot.";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [ libsixel ];
+
+    programs.foot = {
+      enable = true;
+      package = pkgs.foot;
+
+      server.enable = false;
+
+      settings = {
+        main = {
+          # window settings
+          app-id = "foot";
+          locked-title = "no";
+          pad = "16x16 center";
+          shell = "zsh";
+          term = "xterm-256color";
+          title = "foot";
+
+          # notifications
+          notify = "notify-send -a \${app-id} -i \${app-id} \${title} \${body}";
+          selection-target = "clipboard";
+
+          # font and font rendering
+          dpi-aware = false; # this looks more readable on a laptop, but it's unreasonably large
+          font = "Iosevka Nerd Font:size=14";
+          font-bold = "Iosevka Nerd Font:size=14";
+          vertical-letter-offset = "-0.90";
+        };
+
+        scrollback = {
+          lines = 10000;
+          multiplier = 3;
+        };
+
+        tweak = {
+          font-monospace-warn = "no"; # reduces startup time
+          sixel = "yes";
+        };
+
+        cursor = {
+          beam-thickness = 2;
+          style = "beam";
+        };
+
+        mouse = {
+          hide-when-typing = "yes";
+        };
+
+        url = {
+          label-letters = "sadfjklewcmpgh";
+          launch = "xdg-open \${url}";
+          osc8-underline = "url-mode";
+          protocols = "http, https, ftp, ftps, file";
+          uri-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.,~:;/?#@!$&%*+=\"'()[]";
+        };
+
+        colors = import ./presets/catppuccin-macchiato/colors.nix { inherit colors; } // {
+          alpha = "0.85";
+        };
+      };
+    };
+  };
+}
---
+++ modules/home/programs/terminal/emulators/foot/presets/catppuccin-macchiato/colors.nix
@@ -0,0 +1,23 @@
+{ colors }:
+{
+  foreground = "${colors.text.hex}"; # Text
+  background = "${colors.base.hex}"; # Base
+
+  regular0 = "${colors.surface1.hex}"; # Surface 1
+  regular1 = "${colors.red.hex}"; # red
+  regular2 = "${colors.green.hex}"; # green
+  regular3 = "${colors.yellow.hex}"; # yellow
+  regular4 = "${colors.blue.hex}"; # blue
+  regular5 = "${colors.pink.hex}"; # pink
+  regular6 = "${colors.teal.hex}"; # teal
+  regular7 = "${colors.subtext0.hex}"; # Subtext 0
+  # Subtext 1 ???
+  bright0 = "${colors.surface2.hex}"; # Surface 2
+  bright1 = "${colors.red.hex}"; # red
+  bright2 = "${colors.green.hex}"; # green
+  bright3 = "${colors.yellow.hex}"; # yellow
+  bright4 = "${colors.blue.hex}"; # blue
+  bright5 = "${colors.pink.hex}"; # pink
+  bright6 = "${colors.teal.hex}"; # teal
+  bright7 = "${colors.subtext0.hex}"; # Subtext 0
+}
