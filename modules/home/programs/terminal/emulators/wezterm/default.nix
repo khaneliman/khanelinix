5a7cbb06bf73fc652ea0c83c108387442c8e68fb
Austin Horstman <khaneliman12@gmail.com>
2024-05-09 17:22:14 -0500

    wezterm: config refactor

--- modules/home/programs/terminal/emulators/wezterm/default.nix
+++ modules/home/programs/terminal/emulators/wezterm/default.nix
@@ -35,21 +35,58 @@ in
           local custom = wezterm.color.get_builtin_schemes()[scheme_for_appearance(wezterm.gui.get_appearance())]
 
           return {
+            -- general
+            audible_bell = "Disabled",
+            check_for_updates = false,
+            enable_scroll_bar = false,
+            exit_behavior = "CloseOnCleanExit",
+            warn_about_missing_glyphs =  false,
+            term = "xterm-256color",
+
+            -- anims
+            animation_fps = 1,
+
+            -- Color scheme
+            color_schemes = {
+              ["Catppuccin"] = custom,
+            },
+            color_scheme = "Catppuccin",
+
+            -- Cursor
+            cursor_blink_ease_in = 'Constant',
+            cursor_blink_ease_out = 'Constant',
+            cursor_blink_rate = 700,
+            default_cursor_style = "SteadyBar",
+
+            -- font
             font = wezterm.font_with_fallback {
               'MonaspiceKr Nerd Font',
               'CaskaydiaCove Nerd Font',
               'Noto Color Emoji',
             },
-            color_schemes = {
-              ["Catppuccin"] = custom,
-            },
-            color_scheme = "Catppuccin",
-            use_fancy_tab_bar = false,
+
+            -- Tab bar
+            enable_tab_bar = true,
+            hide_tab_bar_if_only_one_tab = true,
+            show_tab_index_in_tab_bar = false,
             tab_bar_at_bottom = true,
-            term = "xterm-256color",
-            window_close_confirmation = 'NeverPrompt',
+            use_fancy_tab_bar = false,
+
+            -- perf
+            enable_wayland = true,
+            front_end = "WebGpu",
+            scrollback_lines = 10000,
+
+            -- term window settings
+            adjust_window_size_when_changing_font_size = false,
+            inactive_pane_hsb = {
+              saturation = 1.0,
+              brightness = 0.8
+            },
+            window_background_opacity = 0.85,
+            window_close_confirmation = "NeverPrompt",
             window_decorations = "RESIZE",
-            warn_about_missing_glyphs=false,
+            window_padding = { left = 12, right = 12, top = 12, bottom = 12, },
           }
         '';
     };
