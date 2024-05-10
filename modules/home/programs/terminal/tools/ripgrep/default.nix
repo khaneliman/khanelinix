8239c7e844f707e10a8a48138bb40488c03544f9
Austin Horstman <khaneliman12@gmail.com>
2024-05-09 22:18:19 -0500

    terminal/tools/ripgrep: add arguments

--- modules/home/programs/terminal/tools/ripgrep/default.nix
+++ modules/home/programs/terminal/tools/ripgrep/default.nix
@@ -20,7 +20,17 @@ in
       enable = true;
       package = pkgs.ripgrep;
 
-      arguments = [ ];
+      arguments = [
+        # Don't have ripgrep vomit a bunch of stuff on the screen
+        # show a preview of the match
+        "--max-columns=150"
+        "--max-columns-preview"
+
+        # ignore git files
+        "--glob=!.git/*"
+
+        "--smart-case"
+      ];
     };
 
     home.shellAliases = {
