c452f5a188a4c5b414142240e7e26ef74ec7089a
Austin Horstman <khaneliman12@gmail.com>
2024-05-12 17:32:15 -0500

    terminal/editors/neovim: add statuscol

---
+++ modules/home/programs/terminal/editors/neovim/plugins/statuscol.nix
@@ -0,0 +1,52 @@
+_: {
+  programs.nixvim = {
+    plugins.statuscol = {
+      enable = true;
+
+      settings = {
+        relculright = true;
+
+        segments = [
+          {
+            text = [
+              {
+                __raw = # lua
+                  "require('statuscol.builtin').foldfunc";
+              }
+            ];
+            click = "v:lua.ScFa";
+          }
+          {
+            text = [ ];
+            sign = {
+              name = [ "Diagnostic" ];
+              maxwidth = 2;
+              auto = true;
+            };
+            click = "v:lua.ScSa";
+          }
+          {
+            text = [
+              {
+                __raw = # lua
+                  "require('statuscol.builtin').lnumfunc";
+              }
+            ];
+            click = "v:lua.ScLa";
+          }
+          {
+            text = [ ];
+            sign = {
+              name = [ ".*" ];
+              maxwidth = 2;
+              colwidth = 1;
+              auto = true;
+              wrap = true;
+            };
+            click = "v:lua.ScSa";
+          }
+        ];
+      };
+    };
+  };
+}
