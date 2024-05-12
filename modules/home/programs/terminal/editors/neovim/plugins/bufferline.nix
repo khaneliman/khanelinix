55893bd67271e637f901fd44ee6a026584ca7039
Austin Horstman <khaneliman12@gmail.com>
2024-05-12 09:48:12 -0500

    terminal/editors/neovim: bufferline fix diagnostic spacing

--- modules/home/programs/terminal/editors/neovim/plugins/bufferline.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/bufferline.nix
@@ -32,8 +32,8 @@ in
             function(count, level, diagnostics_dict, context)
                local s = ""
                for e, n in pairs(diagnostics_dict) do
-                  local sym = e == "error" and ""
-                     or (e == "warning" and "" or "" )
+                  local sym = e == "error" and " "
+                     or (e == "warning" and " " or "" )
                   if(sym ~= "") then
                   s = s .. " " .. n .. sym
                   end
