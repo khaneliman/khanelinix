bdd7e5a538d3204741dfd54e12c60f1496cd15f2
Austin Horstman <khaneliman12@gmail.com>
2024-05-12 09:15:14 -0500

    terminal/editors/neovim: add gitignore generate

---
+++ modules/home/programs/terminal/editors/neovim/plugins/gitignore.nix
@@ -0,0 +1,29 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkIf;
+
+  cfg = config.programs.nixvim.plugins.gitignore;
+in
+{
+  programs.nixvim = {
+    plugins = {
+      gitignore = {
+        enable = true;
+      };
+    };
+
+    keymaps = mkIf cfg.enable [
+      {
+        mode = "n";
+        key = "<leader>gi";
+        lua = true;
+        action = # lua
+          ''require('gitignore').generate'';
+        options = {
+          desc = "Gitignore generate";
+          silent = true;
+        };
+      }
+    ];
+  };
+}
