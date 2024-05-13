0ebb1a08812a46739a477304beb67042406217ee
Austin Horstman <khaneliman12@gmail.com>
2024-05-13 16:26:45 -0500

    terminal/editors/neovim: noice background doc

--- modules/home/programs/terminal/editors/neovim/plugins/noice.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/noice.nix
@@ -54,7 +54,7 @@ in
           command_palette = true;
           long_message_to_split = true;
           inc_rename = true;
-          lsp_doc_border = false;
+          lsp_doc_border = true;
         };
 
         routes = [
