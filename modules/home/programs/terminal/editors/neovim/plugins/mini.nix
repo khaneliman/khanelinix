27208f00f7a25bd92a39faa1732661cdf16e107a
Austin Horstman <khaneliman12@gmail.com>
2024-05-19 15:05:04 -0500

    terminal/editors/neovim: mini-surround keymapping change

--- modules/home/programs/terminal/editors/neovim/plugins/mini.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/mini.nix
@@ -52,7 +52,17 @@ _: {
           };
         };
 
-        surround = { };
+        surround = {
+          mappings = {
+            add = "gsa"; # -- Add surrounding in Normal and Visual modes
+            delete = "gsd"; # -- Delete surrounding
+            find = "gsf"; # -- Find surrounding (to the right)
+            find_left = "gsF"; # -- Find surrounding (to the left)
+            highlight = "gsh"; # -- Highlight surrounding
+            replace = "gsr"; # -- Replace surrounding
+            update_n_lines = "gsn"; # -- Update `n_lines`
+          };
+        };
       };
     };
 
