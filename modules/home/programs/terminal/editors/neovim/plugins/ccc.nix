55a61c8e71710e755f1821bd7779a836f5c933d7
Austin Horstman <khaneliman12@gmail.com>
2024-05-21 10:55:33 -0500

    flake.lock: update

--- modules/home/programs/terminal/editors/neovim/plugins/ccc.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/ccc.nix
@@ -9,41 +9,39 @@ _: {
             auto_enable = true;
             max_byte = 2 * 1024 * 1024;
             lsp = true;
-            filetypes.__raw = "colorPickerFts";
+            filetypes = [ "colorPickerFts" ];
           };
           pickers = [
-            { __raw = ''require("ccc").picker.hex''; }
-            { __raw = ''require("ccc").picker.css_rgb''; }
-            { __raw = ''require("ccc").picker.css_hsl''; }
-            {
-              __raw = ''
-                require("ccc").picker.ansi_escape {
-                  meaning1 = "bright"
-                }'';
-            }
+            ''require("ccc").picker.hex''
+            ''require("ccc").picker.css_rgb''
+            ''require("ccc").picker.css_hsl''
+            ''
+              require("ccc").picker.ansi_escape {
+                              meaning1 = "bright"
+                            }''
           ];
           alpha_show = "hide";
           recognize = {
             output = true;
           };
-          inputs = [ { __raw = "require('ccc').input.hsl"; } ];
+          inputs = [ "require('ccc').input.hsl" ];
           outputs = [
-            { __raw = "require('ccc').output.css_hsl"; }
-            { __raw = "require('ccc').output.css_rgb"; }
-            { __raw = "require('ccc').output.hex"; }
+            "require('ccc').output.css_hsl"
+            "require('ccc').output.css_rgb"
+            "require('ccc').output.hex"
           ];
           convert = [
             [
-              { __raw = "require('ccc').picker.hex"; }
-              { __raw = "require('ccc').output.css_hsl"; }
+              "require('ccc').picker.hex"
+              "require('ccc').output.css_hsl"
             ]
             [
-              { __raw = "require('ccc').picker.css_rgb"; }
-              { __raw = "require('ccc').output.css_hsl"; }
+              "require('ccc').picker.css_rgb"
+              "require('ccc').output.css_hsl"
             ]
             [
-              { __raw = "require('ccc').picker.css_hsl"; }
-              { __raw = "require('ccc').output.hex"; }
+              "require('ccc').picker.css_hsl"
+              "require('ccc').output.hex"
             ]
           ];
           mappings = {
--- modules/home/programs/terminal/editors/neovim/plugins/rustaceanvim.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/rustaceanvim.nix
@@ -7,55 +7,57 @@
     plugins = {
       rustaceanvim = {
         enable = true;
+        settings = {
 
-        dap = {
-          autoloadConfigurations = true;
-        };
+          dap = {
+            autoloadConfigurations = true;
+          };
 
-        server = {
-          settings = {
-            cargo = {
-              buildScripts.enable = true;
-              features = "all";
+          server = {
+            settings = {
+              cargo = {
+                buildScripts.enable = true;
+                features = "all";
+              };
+
+              diagnostics = {
+                enable = true;
+                styleLints.enable = true;
+              };
+
+              checkOnSave = true;
+              check = {
+                command = "clippy";
+                features = "all";
+              };
+
+              files = {
+                excludeDirs = [
+                  ".cargo"
+                  ".direnv"
+                  ".git"
+                  "node_modules"
+                  "target"
+                ];
+              };
+
+              inlayHints = {
+                bindingModeHints.enable = true;
+                closureStyle = "rust_analyzer";
+                closureReturnTypeHints.enable = "always";
+                discriminantHints.enable = "always";
+                expressionAdjustmentHints.enable = "always";
+                implicitDrops.enable = true;
+                lifetimeElisionHints.enable = "always";
+                rangeExclusiveHints.enable = true;
+              };
+
+              procMacro = {
+                enable = true;
+              };
+
+              rustc.source = "discover";
             };
-
-            diagnostics = {
-              enable = true;
-              styleLints.enable = true;
-            };
-
-            checkOnSave = true;
-            check = {
-              command = "clippy";
-              features = "all";
-            };
-
-            files = {
-              excludeDirs = [
-                ".cargo"
-                ".direnv"
-                ".git"
-                "node_modules"
-                "target"
-              ];
-            };
-
-            inlayHints = {
-              bindingModeHints.enable = true;
-              closureStyle = "rust_analyzer";
-              closureReturnTypeHints.enable = "always";
-              discriminantHints.enable = "always";
-              expressionAdjustmentHints.enable = "always";
-              implicitDrops.enable = true;
-              lifetimeElisionHints.enable = "always";
-              rangeExclusiveHints.enable = true;
-            };
-
-            procMacro = {
-              enable = true;
-            };
-
-            rustc.source = "discover";
           };
         };
       };
