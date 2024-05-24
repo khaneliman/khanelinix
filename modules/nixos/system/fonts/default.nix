f14b25e3e1270ed65d1adae9bd79659dcfcb358b
Austin Horstman <khaneliman12@gmail.com>
2024-05-23 20:21:27 -0500

    nixos/system/fonts: missed relative import

--- modules/nixos/system/fonts/default.nix
+++ modules/nixos/system/fonts/default.nix
@@ -11,7 +11,7 @@ let
   cfg = config.${namespace}.system.fonts;
 in
 {
-  imports = [ ../../../shared/system/fonts/default.nix ];
+  imports = [ (lib.snowfall.fs.get-file "modules/shared/system/fonts/default.nix") ];
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
