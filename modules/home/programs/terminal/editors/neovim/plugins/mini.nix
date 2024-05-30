aa468998370ebaa4ee447090c2e1710c90a9438f
Austin Horstman <khaneliman12@gmail.com>
2024-05-29 22:41:42 -0500

    terminal/editors/neovim: add lua comment mini

--- modules/home/programs/terminal/editors/neovim/plugins/mini.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/mini.nix
@@ -39,16 +39,23 @@ _: {
           evaluate_single = true;
 
           items = {
-            "__unkeyed.buildtin_actions".__raw = "require('mini.starter').sections.builtin_actions()";
-            "__unkeyed.recent_files_current_directory".__raw = "require('mini.starter').sections.recent_files(10, false)";
-            "__unkeyed.recent_files".__raw = "require('mini.starter').sections.recent_files(10, true)";
-            "__unkeyed.sessions".__raw = "require('mini.starter').sections.sessions(5, true)";
+            "__unkeyed.buildtin_actions".__raw = # Lua
+              "require('mini.starter').sections.builtin_actions()";
+            "__unkeyed.recent_files_current_directory".__raw = # Lua
+              "require('mini.starter').sections.recent_files(10, false)";
+            "__unkeyed.recent_files".__raw = # Lua
+              "require('mini.starter').sections.recent_files(10, true)";
+            "__unkeyed.sessions".__raw = # Lua
+              "require('mini.starter').sections.sessions(5, true)";
           };
 
           content_hooks = {
-            "__unkeyed.adding_bullet".__raw = "require('mini.starter').gen_hook.adding_bullet()";
-            "__unkeyed.indexing".__raw = "require('mini.starter').gen_hook.indexing('all', { 'Builtin actions' })";
-            "__unkeyed.padding".__raw = "require('mini.starter').gen_hook.aligning('center', 'center')";
+            "__unkeyed.adding_bullet".__raw = # lua
+              "require('mini.starter').gen_hook.adding_bullet()";
+            "__unkeyed.indexing".__raw = # lua
+              "require('mini.starter').gen_hook.indexing('all', { 'Builtin actions' })";
+            "__unkeyed.padding".__raw = # Lua
+              "require('mini.starter').gen_hook.aligning('center', 'center')";
           };
         };
 
