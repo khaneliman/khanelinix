03573143c4d79076c98348a7b7fd34546efd73e8
Austin Horstman <khaneliman12@gmail.com>
2024-05-31 10:13:51 -0500

    terminal/tools/fastfetch: use home-manager module

--- modules/home/programs/terminal/tools/fastfetch/default.nix
+++ modules/home/programs/terminal/tools/fastfetch/default.nix
@@ -17,142 +17,146 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.fastfetch.enable = true;
 
-    xdg.configFile = {
-      "fastfetch/config.jsonc".text =
-        # json
-        ''
-          {
-            "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
-            "logo": {
-              "padding": {
-                "top": 2
-              }
-            },
-            "display": {
-              "color": {
-                "keys": "green",
-                "title": "blue"
-              },
-              "percent": {
-                "type": 9
-              },
-              "separator": " 󰁔 ",
-            },
-            "modules": [
-              {
-                "type": "custom",
-                "format": "\u001b[34m┌──────────── \u001b[1mOS Information\u001b[0m \u001b[34m────────────┐"
-              },
-              {
-                "type": "title",
-                "key": " ╭─ ",
-                "keyColor": "green",
-                "color": {
-                  "user": "green",
-                  "host": "green"
-                }
-              },${lib.optionalString pkgs.stdenv.isDarwin # json
-                ''
-                  {
-                        "type": "os",
-                        "key": " ├─  ",
-                        "keyColor": "green"
-                      },
-                      {
-                        "type": "kernel",
-                        "key": " ├─  ",
-                        "keyColor": "green"
-                      },
-                      {
-                        "type": "packages",
-                        "key": " ├─  ",
-                        "keyColor": "green"
-                      },
-                ''}${lib.optionalString pkgs.stdenv.isLinux # json
-                ''
-                  {
-                        "type": "os",
-                        "key": " ├─ ",
-                        "keyColor": "green"
-                      },
-                      {
-                        "type": "kernel",
-                        "key": " ├─ ",
-                        "keyColor": "green"
-                      },
-                      {
-                        "type": "packages",
-                        "key": " ├─ ",
-                        "keyColor": "green"
-                      },
-                ''}
-              {
-                "type": "shell",
-                "key": " ╰─  ",
-                "keyColor": "green"
-              },
-              {
-                "type": "custom",
-                "format": "\u001b[34m├───────── \u001b[1mHardware Information\u001b[0m \u001b[34m─────────┤"
-              },
-              {
-                "type": "display",
-                "key": " ╭─ 󰍹 ",
-                "keyColor": "blue"
-              },
-              {
-                "type": "cpu",
-                "key": " ├─ 󰍛 ",
-                "keyColor": "blue"
-              },
-              {
-                "type": "gpu",
-                "key": " ├─ 󰍛 ",
-                "keyColor": "blue"
-              },
-              {
-                "type": "memory",
-                "key": " ├─   ",
-                "keyColor": "blue"
-              },
-              {
-                "type": "disk",
-                "key": " ╰─ 󱛟 ",
-                "keyColor": "blue"
-              },
-              {
-                "type": "custom",
-                "format": "\u001b[34m├───────── \u001b[1mSoftware Information\u001b[0m \u001b[34m─────────┤"
-              },
-              {
-                "type": "wm",
-                "key": " ╭─  ",
-                "keyColor": "yellow"
-              },
-              {
-                "type": "terminal",
-                "key": " ├─  ",
-                "keyColor": "yellow"
-              },
-              {
-                "type": "font",
-                "key": " ╰─  ",
-                "keyColor": "yellow"
-              },
-              {
-                "type": "custom",
-                "format": "\u001b[34m└────────────────────────────────────────┘"
-              },
-              {
-                "type": "custom",
-                "format": "   \u001b[38m   \u001b[34m    \u001b[36m    \u001b[35m    \u001b[34m    \u001b[33m    \u001b[32m    \u001b[31m "
-              },
-              "break"
-            ]
-          }
-        '';
+    programs.fastfetch = {
+      enable = true;
+
+      settings = {
+        "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+        logo = {
+          padding = {
+            top = 2;
+          };
+        };
+        display = {
+          color = {
+            keys = "green";
+            title = "blue";
+          };
+          percent = {
+            type = 9;
+          };
+          separator = " 󰁔 ";
+        };
+        modules =
+          [
+            {
+              type = "custom";
+              outputColor = "blue";
+              format = ''┌──────────── OS Information ────────────┐'';
+            }
+            {
+              type = "title";
+              key = " ╭─ ";
+              keyColor = "green";
+              color = {
+                user = "green";
+                host = "green";
+              };
+            }
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [
+            {
+              type = "os";
+              key = " ├─  ";
+              keyColor = "green";
+            }
+            {
+              type = "kernel";
+              key = " ├─  ";
+              keyColor = "green";
+            }
+            {
+              type = "packages";
+              key = " ├─  ";
+              keyColor = "green";
+            }
+          ]
+          ++ lib.optionals pkgs.stdenv.isLinux [
+            {
+              type = "os";
+              key = " ├─ ";
+              keyColor = "green";
+            }
+            {
+              type = "kernel";
+              key = " ├─ ";
+              keyColor = "green";
+            }
+            {
+              type = "packages";
+              key = " ├─ ";
+              keyColor = "green";
+            }
+          ]
+          ++ [
+            {
+              type = "shell";
+              key = " ╰─  ";
+              keyColor = "green";
+            }
+            {
+              type = "custom";
+              outputColor = "blue";
+              format = ''├───────── Hardware Information ─────────┤'';
+            }
+            {
+              type = "display";
+              key = " ╭─ 󰍹 ";
+              keyColor = "blue";
+            }
+            {
+              type = "cpu";
+              key = " ├─ 󰍛 ";
+              keyColor = "blue";
+            }
+            {
+              type = "gpu";
+              key = " ├─ 󰍛 ";
+              keyColor = "blue";
+            }
+            {
+              type = "memory";
+              key = " ├─   ";
+              keyColor = "blue";
+            }
+            {
+              type = "disk";
+              key = " ╰─ 󱛟 ";
+              keyColor = "blue";
+            }
+            {
+              type = "custom";
+              outputColor = "blue";
+              format = ''├───────── Software Information ─────────┤'';
+            }
+            {
+              type = "wm";
+              key = " ╭─  ";
+              keyColor = "yellow";
+            }
+            {
+              type = "terminal";
+              key = " ├─  ";
+              keyColor = "yellow";
+            }
+            {
+              type = "font";
+              key = " ╰─  ";
+              keyColor = "yellow";
+            }
+            {
+              type = "custom";
+              outputColor = "blue";
+              format = ''└────────────────────────────────────────┘'';
+            }
+            {
+              type = "custom";
+              format = "   {#39}   {#34}    {#36}    {#35}    {#34}    {#33}    {#32}    {#31} ";
+            }
+            "break"
+          ];
+      };
     };
   };
 }
