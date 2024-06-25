2bd51e1435b5948921bb2fe212bc5564bbc06d54
Austin Horstman <khaneliman12@gmail.com>
2024-06-25 17:01:34 -0500

    nixos/services/openssh: tweaks

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -10,6 +10,9 @@
 let
   inherit (lib)
     types
+    mapAttrs
+    mkDefault
+    mkForce
     mkIf
     foldl
     optionalString
@@ -72,27 +75,96 @@ in
     services.openssh = {
       enable = true;
 
-      extraConfig = ''
-        StreamLocalBindUnlink yes
-      '';
+      hostKeys = mkDefault [
+        {
+          bits = 4096;
+          path = "/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+        }
+        {
+          bits = 4096;
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
 
+      openFirewall = true;
       ports = [
         22
         cfg.port
       ];
 
       settings = {
+        AuthenticationMethods = "publickey";
+        ChallengeResponseAuthentication = "no";
         PasswordAuthentication = false;
         PermitRootLogin = if format == "install-iso" then "yes" else "no";
-        UseDns = true;
+        PubkeyAuthentication = "yes";
+        StreamLocalBindUnlink = "yes";
+        UseDns = false;
+        UsePAM = false;
+        X11Forwarding = false;
+
+        # key exchange algorithms recommended by `nixpkgs#ssh-audit`
+        KexAlgorithms = [
+          "curve25519-sha256"
+          "curve25519-sha256@libssh.org"
+          "diffie-hellman-group16-sha512"
+          "diffie-hellman-group18-sha512"
+          "diffie-hellman-group-exchange-sha256"
+          "sntrup761x25519-sha512@openssh.com"
+        ];
+
+        # message authentication code algorithms recommended by `nixpkgs#ssh-audit`
+        Macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+        ];
       };
+
+      startWhenNeeded = true;
     };
 
-    programs.ssh.extraConfig = ''
-      ${other-hosts-config}
+    programs.ssh = {
+      extraConfig = ''
+        ${other-hosts-config}
 
-      ${cfg.extraConfig}
-    '';
+        ${cfg.extraConfig}
+      '';
+
+      # ship github/gitlab/sourcehut host keys to avoid MiM (man in the middle) attacks
+      knownHosts = mapAttrs (_: mkForce) {
+        github-rsa = {
+          hostNames = [ "github.com" ];
+          publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
+        };
+
+        github-ed25519 = {
+          hostNames = [ "github.com" ];
+          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+        };
+
+        gitlab-rsa = {
+          hostNames = [ "gitlab.com" ];
+          publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsj2bNKTBSpIYDEGk9KxsGh3mySTRgMtXL583qmBpzeQ+jqCMRgBqB98u3z++J1sKlXHWfM9dyhSevkMwSbhoR8XIq/U0tCNyokEi/ueaBMCvbcTHhO7FcwzY92WK4Yt0aGROY5qX2UKSeOvuP4D6TPqKF1onrSzH9bx9XUf2lEdWT/ia1NEKjunUqu1xOB/StKDHMoX4/OKyIzuS0q/T1zOATthvasJFoPrAjkohTyaDUz2LN5JoH839hViyEG82yB+MjcFV5MU3N1l1QL3cVUCh93xSaua1N85qivl+siMkPGbO5xR/En4iEY6K2XPASUEMaieWVNTRCtJ4S8H+9";
+        };
+        gitlab-ed25519 = {
+          hostNames = [ "gitlab.com" ];
+          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+        };
+
+        sourcehut-rsa = {
+          hostNames = [ "git.sr.ht" ];
+          publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZ+l/lvYmaeOAPeijHL8d4794Am0MOvmXPyvHTtrqvgmvCJB8pen/qkQX2S1fgl9VkMGSNxbp7NF7HmKgs5ajTGV9mB5A5zq+161lcp5+f1qmn3Dp1MWKp/AzejWXKW+dwPBd3kkudDBA1fa3uK6g1gK5nLw3qcuv/V4emX9zv3P2ZNlq9XRvBxGY2KzaCyCXVkL48RVTTJJnYbVdRuq8/jQkDRA8lHvGvKI+jqnljmZi2aIrK9OGT2gkCtfyTw2GvNDV6aZ0bEza7nDLU/I+xmByAOO79R1Uk4EYCvSc1WXDZqhiuO2sZRmVxa0pQSBDn1DB3rpvqPYW+UvKB3SOz";
+        };
+
+        sourcehut-ed25519 = {
+          hostNames = [ "git.sr.ht" ];
+          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
+        };
+      };
+    };
 
     khanelinix = {
       user.extraOptions.openssh.authorizedKeys.keys = cfg.authorizedKeys;
