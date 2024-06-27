6f9d6a6587bd47f35baa08e4ae09ed0ba1c0597d
Austin Horstman <khaneliman12@gmail.com>
2024-06-26 23:10:43 -0500

    terminal/editors/neovim: navic lsp priority

--- modules/home/programs/terminal/editors/neovim/plugins/navic.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/navic.nix
@@ -3,7 +3,14 @@ _: {
     # Shows the breadcrumb lsp node path in lualine
     plugins.navic = {
       enable = true;
-      lsp.autoAttach = true;
+
+      lsp = {
+        autoAttach = true;
+        preference = [
+          "clangd"
+          "tsserver"
+        ];
+      };
     };
   };
 }
