085df885877e52fc50c8eceb48cb99c39ce18b36
Austin Horstman <khaneliman12@gmail.com>
2024-06-29 00:33:31 -0500

    terminal/media/mpv: init

---
+++ modules/home/programs/terminal/media/mpv/default.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.programs.terminal.media.mpv;
+
+in
+{
+  options.${namespace}.programs.terminal.media.mpv = {
+    enable = mkBoolOpt false "Whether or not to enable support for mpv.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.mpv = {
+      enable = true;
+
+      defaultProfiles = [ "gpu-hq" ];
+      scripts = lib.optionals pkgs.stdenv.isLinux [ pkgs.mpvScripts.mpris ];
+    };
+  };
+}
