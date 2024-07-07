11718b03f7af1f322c399ef44bb0f77d855f5be3
Austin Horstman <khaneliman12@gmail.com>
2024-07-06 22:53:34 -0500

    terminal/editors/neovim: indent-blankline add keymaps

--- modules/home/programs/terminal/editors/neovim/plugins/indent-blankline.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/indent-blankline.nix
@@ -1,11 +1,35 @@
-_: {
-  programs.nixvim.plugins = {
-    indent-blankline = {
-      enable = true;
+{ config, lib, ... }:
+{
+  programs.nixvim = {
+    plugins = {
+      indent-blankline = {
+        enable = true;
 
-      settings = {
-        scope.enabled = false;
+        settings = {
+          scope.enabled = false;
+        };
       };
     };
+
+    keymaps = lib.optionals config.programs.nixvim.plugins.indent-blankline.enable [
+      {
+        mode = "n";
+        key = "<leader>ui";
+        action = ":IBLToggle<CR>";
+        options = {
+          desc = "Indent-Blankline toggle";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>uI";
+        action = ":IBLToggleScope<CR>";
+        options = {
+          desc = "Indent-Blankline Scope toggle";
+          silent = true;
+        };
+      }
+    ];
   };
 }
