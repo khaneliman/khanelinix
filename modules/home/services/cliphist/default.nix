80918d411579951a9424ea3270bc714107ccc956
Austin Horstman <khaneliman12@gmail.com>
2024-07-14 12:56:56 -0500

    home/services/cliphist: init

---
+++ modules/home/services/cliphist/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.${namespace}.services.cliphist;
+in
+{
+  options.${namespace}.services.cliphist = {
+    enable = mkEnableOption "cliphist";
+
+    systemdTargets = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = ''
+        Systemd targets for cliphist
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services = {
+
+      cliphist = {
+        enable = true;
+        allowImages = true;
+
+        systemdTarget = lib.concatStringsSep " " cfg.systemdTargets;
+      };
+    };
+  };
+}
