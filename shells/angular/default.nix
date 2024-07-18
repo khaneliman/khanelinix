69cc1b353b94e58c016ee1d4e869c074014f4511
Austin Horstman <khaneliman12@gmail.com>
2024-07-18 13:17:05 -0500

    shells/angular: add pnpm

--- shells/angular/default.nix
+++ shells/angular/default.nix
@@ -1,11 +1,12 @@
 { mkShell, pkgs, ... }:
 mkShell {
   packages = with pkgs; [
+    nodePackages."@angular/cli"
+    nodejs-18_x
+    pnpm
     vimPlugins.nvim-treesitter-parsers.angular
     vscode-extensions.angular.ng-template
-    nodejs-18_x
     yarn
-    nodePackages."@angular/cli"
   ];
 
   shellHook = ''
