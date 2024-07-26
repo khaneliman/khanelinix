9db28dacdc418b13ee3ec79df474abeaaef164e9
Austin Horstman <khaneliman12@gmail.com>
2024-07-26 13:44:11 -0500

    terminal/editors/neovim: add more neotest adapters

--- modules/home/programs/terminal/editors/neovim/plugins/neotest.nix
+++ modules/home/programs/terminal/editors/neovim/plugins/neotest.nix
@@ -13,41 +13,28 @@
       };
 
       adapters = {
-        bash = {
-          enable = true;
-        };
-
-        deno = {
-          enable = true;
-        };
-
-        dotnet = {
-          enable = true;
-        };
-
-        java = {
-          enable = true;
-
-          settings = {
-            # NOTE: just run NeotestJava setup
-            # Not sure why this wasn't working
-            # junit_jar =
-            #   pkgs.fetchurl
-            #     {
-            #       url = "https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.10.1/junit-platform-console-standalone-1.10.1.jar";
-            #       hash = "sha256-tC6qU9E1dtF9tfuLKAcipq6eNtr5X0JivG6W1Msgcl8=";
-            #     }
-            #     .outPath;
-          };
-        };
-
-        jest = {
-          enable = true;
-        };
-
-        playwright = {
-          enable = true;
-        };
+        bash.enable = true;
+        deno.enable = true;
+        dotnet.enable = true;
+        go.enable = true;
+        java.enable = true;
+        # NOTE: just run NeotestJava setup
+        # java.settings = {
+        # Not sure why this wasn't working
+        # junit_jar =
+        #   pkgs.fetchurl
+        #     {
+        #       url = "https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.10.1/junit-platform-console-standalone-1.10.1.jar";
+        #       hash = "sha256-tC6qU9E1dtF9tfuLKAcipq6eNtr5X0JivG6W1Msgcl8=";
+        #     }
+        #     .outPath;
+        # };
+        jest.enable = true;
+        playwright.enable = true;
+        plenary.enable = true;
+        python.enable = true;
+        # rust.enable = true;
+        zig.enable = true;
       };
     };
 
