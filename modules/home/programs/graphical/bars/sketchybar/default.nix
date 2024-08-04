60bc9ff5b65ca7e107de3b288e16998fd2b01c88
Austin Horstman <khaneliman12@gmail.com>
2024-08-03 23:34:19 -0500

    graphical/bars/sketchybar: use sbarlua derivation

--- modules/home/programs/graphical/bars/sketchybar/default.nix
+++ modules/home/programs/graphical/bars/sketchybar/default.nix
@@ -42,6 +42,29 @@ in
         recursive = true;
       };
 
+      "sketchybar/sketchybarrc" = {
+        executable = true;
+        text = # Lua
+          ''
+            #!/usr/bin/env lua
+
+            -- Add the sketchybar module to the package cpath (the module could be
+            -- installed into the default search path then this would not be needed)
+            package.cpath = package.cpath .. ";${pkgs.khanelinix.sbarlua}/lib/lua/5.4/sketchybar.so"
+
+            Sbar = require("sketchybar")
+
+            Sbar.exec("killall sketchyhelper || sketchyhelper git.felix.sketchyhelper >/dev/null 2>&1 &")
+
+            Sbar.begin_config()
+            require("init")
+            Sbar.hotload(true)
+            Sbar.end_config()
+
+            Sbar.event_loop()
+          '';
+      };
+
       "dynamic-island-sketchybar" = {
         source = lib.cleanSourceWith { src = lib.cleanSource ./dynamic-island-sketchybar/.; };
 
