caac43f7b9a406f6348cca756e69e6ca413703c0
Austin Horstman <khaneliman12@gmail.com>
2024-08-24 10:35:19 -0500

    terminal/emulators/kitty: add emoji font maps

--- modules/home/programs/terminal/emulators/kitty/default.nix
+++ modules/home/programs/terminal/emulators/kitty/default.nix
@@ -21,6 +21,14 @@ in
     programs.kitty = {
       enable = true;
 
+      extraConfig = ''
+        # Emoji font
+        symbol_map U+1F600-U+1F64F Noto Color Emoji
+
+        # Fallback to Nerd Font Symbols
+        symbol_map U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A3,U+E0B0-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6AA,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F32F,U+F400-U+F4A9,U+F500-U+F8FF,U+F0001-U+F1AF0 Symbols Nerd Font Mono
+      '';
+
       keybindings = {
         "ctrl+shift+v" = "paste_from_clipboard";
         "ctrl+shift+s" = "paste_from_selection";
