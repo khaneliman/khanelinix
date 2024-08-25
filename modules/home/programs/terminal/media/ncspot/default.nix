dd06fd0cfb6d6cc75fc7498ac276a624e68f6192
Austin Horstman <khaneliman12@gmail.com>
2024-08-24 20:18:02 -0500

    theme/catppuccin: move ncspot theme

--- modules/home/programs/terminal/media/ncspot/default.nix
+++ modules/home/programs/terminal/media/ncspot/default.nix
@@ -18,28 +18,7 @@ in
     programs.ncspot = {
       enable = true;
 
-      settings = {
-        # TODO: move to theme module
-        theme = {
-          background = "#24273A";
-          primary = "#CAD3F5";
-          secondary = "#1E2030";
-          title = "#8AADF4";
-          playing = "#8AADF4";
-          playing_selected = "#B7BDF8";
-          playing_bg = "#181926";
-          highlight = "#C6A0F6";
-          highlight_bg = "#494D64";
-          error = "#CAD3F5";
-          error_bg = "#ED8796";
-          statusbar = "#181926";
-          statusbar_progress = "#CAD3F5";
-          statusbar_bg = "#8AADF4";
-          cmdline = "#CAD3F5";
-          cmdline_bg = "#181926";
-          search_match = "#f5bde6";
-        };
-      };
+      settings = { };
     };
   };
 }
