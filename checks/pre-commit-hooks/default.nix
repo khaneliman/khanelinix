4ed286225210bee92931b859b2a4b241766f0776
Austin Horstman <khaneliman12@gmail.com>
2024-09-05 09:59:17 -0500

    checks/pre-commit-hooks: nixfmt -> nixfmt-rfc-style

--- checks/pre-commit-hooks/default.nix
+++ checks/pre-commit-hooks/default.nix
@@ -51,10 +51,7 @@ pre-commit-hooks-nix.lib.${pkgs.system}.run {
 
       luacheck.enable = true;
 
-      nixfmt = {
-        enable = true;
-        package = pkgs.nixfmt-rfc-style;
-      };
+      nixfmt-rfc-style.enable = true;
 
       pre-commit-hook-ensure-sops.enable = true;
 
