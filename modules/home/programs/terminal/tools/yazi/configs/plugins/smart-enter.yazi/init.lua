de682743c6b74a02882e56c806f05e7b7ea58dfc
Austin Horstman <khaneliman12@gmail.com>
2024-09-25 23:35:36 -0500

    tools/yazi/plugins: smart-enter update

--- modules/home/programs/terminal/tools/yazi/configs/plugins/smart-enter.yazi/init.lua
+++ modules/home/programs/terminal/tools/yazi/configs/plugins/smart-enter.yazi/init.lua
@@ -1,6 +1,12 @@
 return {
 	entry = function()
 		local h = cx.active.current.hovered
-		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", {})
+		if h and h.cha.is_dir then
+			ya.manager_emit("enter", { hovered = true })
+		elseif h and h:is_selected() then
+			ya.manager_emit("open", {})
+		else
+			ya.manager_emit("open", { hovered = true })
+		end
 	end,
 }
