4a2ba90e74daebcb4f6fff4f4af4dd2a7fef3e94
Austin Horstman <khaneliman12@gmail.com>
2024-09-25 22:33:06 -0500

    systems/khanelilab: fix defaultSopsFile path

--- systems/x86_64-linux/khanelilab/default.nix
+++ systems/x86_64-linux/khanelilab/default.nix
@@ -115,7 +115,7 @@ in
       sops = {
         enable = true;
         sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        defaultSopsFile = lib.snowfall.fs.get-file secrets/khanelilab/default.yaml;
+        defaultSopsFile = lib.snowfall.fs.get-file "secrets/khanelilab/default.yaml";
       };
     };
 
