a32d930cfd57d6e85682a57b3a60d8b9d2910924
Austin Horstman <khaneliman12@gmail.com>
2024-09-30 19:58:58 -0500

    home/suites/business: remove outdated note
    Textual was pinned to a prior working version the program requires.

--- modules/home/suites/business/default.nix
+++ modules/home/suites/business/default.nix
@@ -21,9 +21,6 @@ in
       with pkgs;
       [
         calcurse
-        # FIXME: package broken because of dependency textual being broken because of a dependency
-        # on tree-sitter-languages being broken because of a dependency on tree-sitter0_21 which is
-        # marked as incompatible with python3.12
         dooit
         jrnl
         np
