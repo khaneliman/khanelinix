2108cd288877098bb386c4f27a65399a5dbe15e9
Austin Horstman <khaneliman12@gmail.com>
2024-10-01 21:08:52 -0500

    darwin/security/sops: init

---
+++ modules/darwin/security/sops/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib.${namespace}) mkBoolOpt mkOpt;
+
+  cfg = config.${namespace}.security.sops;
+in
+{
+  options.${namespace}.security.sops = with lib.types; {
+    enable = mkBoolOpt false "Whether to enable sops.";
+    defaultSopsFile = mkOpt path null "Default sops file.";
+    sshKeyPaths = mkOpt (listOf path) [ "/etc/ssh/ssh_host_ed25519_key" ] "SSH Key paths to use.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    sops = {
+      inherit (cfg) defaultSopsFile;
+
+      age = {
+        inherit (cfg) sshKeyPaths;
+
+        keyFile = "${config.users.users.${config.${namespace}.user.name}.home}/.config/sops/age/keys.txt";
+      };
+    };
+
+    sops.secrets = {
+      "khanelimac_khaneliman_ssh_key" = {
+        sopsFile = lib.snowfall.fs.get-file "secrets/khanelimac/khaneliman/default.yaml";
+      };
+    };
+  };
+}
