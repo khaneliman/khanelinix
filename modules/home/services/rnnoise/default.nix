26af2966182c5dac2db7187df1b47d467e07544b
Austin Horstman <khaneliman12@gmail.com>
2024-10-07 14:22:08 -0500

    home/services/rnnoise: inline config to file generation

--- modules/home/services/rnnoise/default.nix
+++ modules/home/services/rnnoise/default.nix
@@ -10,43 +10,6 @@ let
 
   json = pkgs.formats.json { };
 
-  pw_rnnoise_config = {
-    "context.modules" = [
-      {
-        "name" = "libpipewire-module-filter-chain";
-        "args" = {
-          "node.description" = "Noise Canceling source";
-          "media.name" = "Noise Canceling source";
-          "filter.graph" = {
-            "nodes" = [
-              {
-                "type" = "ladspa";
-                "name" = "rnnoise";
-                "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-                "label" = "noise_suppressor_stereo";
-                "control" = {
-                  "VAD Threshold (%)" = 70.0;
-                };
-              }
-            ];
-          };
-          "audio.position" = [
-            "FL"
-            "FR"
-          ];
-          "capture.props" = {
-            "node.name" = "effect_input.rnnoise";
-            "node.passive" = true;
-          };
-          "playback.props" = {
-            "node.name" = "effect_output.rnnoise";
-            "media.class" = "Audio/Source";
-          };
-        };
-      }
-    ];
-  };
-
   cfg = config.${namespace}.services.rnnoise;
 in
 {
@@ -58,7 +21,42 @@ in
 
   config = lib.mkIf cfg.enable {
     xdg.configFile."pipewire/pipewire.conf.d/99-input-denoising.conf" = {
-      source = json.generate "99-input-denoising.conf" pw_rnnoise_config;
+      source = json.generate "99-input-denoising.conf" {
+        "context.modules" = [
+          {
+            "name" = "libpipewire-module-filter-chain";
+            "args" = {
+              "node.description" = "Noise Canceling source";
+              "media.name" = "Noise Canceling source";
+              "filter.graph" = {
+                "nodes" = [
+                  {
+                    "type" = "ladspa";
+                    "name" = "rnnoise";
+                    "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                    "label" = "noise_suppressor_stereo";
+                    "control" = {
+                      "VAD Threshold (%)" = 70.0;
+                    };
+                  }
+                ];
+              };
+              "audio.position" = [
+                "FL"
+                "FR"
+              ];
+              "capture.props" = {
+                "node.name" = "effect_input.rnnoise";
+                "node.passive" = true;
+              };
+              "playback.props" = {
+                "node.name" = "effect_output.rnnoise";
+                "media.class" = "Audio/Source";
+              };
+            };
+          }
+        ];
+      };
     };
   };
 }
