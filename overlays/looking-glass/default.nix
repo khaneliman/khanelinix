bfcec66334dfd696d0ada618845d19d87a2434d0
Austin Horstman <khaneliman12@gmail.com>
2024-10-24 13:07:44 -0500

    overlays/looking-glass: init with latest master
    Few months of bug fixes that I'd like

---
+++ overlays/looking-glass/default.nix
@@ -0,0 +1,14 @@
+_: _final: prev: {
+  looking-glass-client = prev.looking-glass-client.overrideAttrs (_oldAttrs: rec {
+    rev = "0-unstable-2024-10-24";
+    src = prev.fetchFromGitHub {
+      owner = "gnif";
+      repo = "LookingGlass";
+      rev = "e25492a3a36f7e1fde6e3c3014620525a712a64a";
+      hash = "sha256-DBmCJRlB7KzbWXZqKA0X4VTpe+DhhYG5uoxsblPXVzg=";
+      fetchSubmodules = true;
+    };
+
+    patches = [ ];
+  });
+}
