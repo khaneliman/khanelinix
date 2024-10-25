22bb564d561a6ac7f5db425d216739a855205e58
Austin Horstman <khaneliman12@gmail.com>
2024-10-24 21:35:43 -0500

    terminal/tools/yazi: add sudo.yazi keymaps

--- modules/home/programs/terminal/tools/yazi/keymap/manager/operation.nix
+++ modules/home/programs/terminal/tools/yazi/keymap/manager/operation.nix
@@ -111,5 +111,72 @@ _: {
       run = "plugin fzf";
       desc = "Jump to a directory, or reveal a file using fzf";
     }
+    {
+      on = [
+        "R"
+        "p"
+        "p"
+      ];
+      run = "plugin sudo --args='paste'";
+      desc = "sudo paste";
+    }
+    {
+      on = [
+        "R"
+        "P"
+      ];
+      run = "plugin sudo --args='paste -f'";
+      desc = "sudo paste (force)";
+    }
+    {
+      on = [
+        "R"
+        "r"
+      ];
+      run = "plugin sudo --args='rename'";
+      desc = "sudo rename";
+    }
+    {
+      on = [
+        "R"
+        "p"
+        "l"
+      ];
+      run = "plugin sudo --args='link'";
+      desc = "sudo link (absolute path)";
+    }
+    {
+      on = [
+        "R"
+        "p"
+        "L"
+      ];
+      run = "plugin sudo --args='link -r'";
+      desc = "sudo link (relative path)";
+    }
+    {
+      on = [
+        "R"
+        "a"
+      ];
+      run = "plugin sudo --args='create'";
+      desc = "sudo create (file or directory)";
+    }
+    {
+      on = [
+        "R"
+        "d"
+      ];
+      run = "plugin sudo --args='remove'";
+      desc = "sudo trash";
+    }
+    {
+      on = [
+        "R"
+        "D"
+      ];
+      run = "plugin sudo --args='remove -P'";
+      desc = "sudo delete (permanent)";
+    }
   ];
 }
