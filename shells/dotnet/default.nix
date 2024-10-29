08ab5c33956713819b540eb75c6e2c805769d4d6
Austin Horstman <khaneliman12@gmail.com>
2024-10-28 21:13:25 -0500

    shells/dotnet: move artifacts-credprovider package reference

--- shells/dotnet/default.nix
+++ shells/dotnet/default.nix
@@ -1,22 +1,6 @@
 { mkShell, pkgs, ... }:
 let
   # TODO: upstream
-  artifacts-credprovider = pkgs.stdenv.mkDerivation rec {
-    name = "artifacts-credprovider";
-    version = "1.3.0";
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/microsoft/artifacts-credprovider/releases/download/v${version}/Microsoft.Net8.NuGet.CredentialProvider.tar.gz";
-      hash = "sha256-WEssfdOKMjJ3WD/egD4wA69k+JdB9O/ZWM8RstRJGkA=";
-    };
-
-    buildPhase = ''
-      mkdir -p $out/bin
-      cp -r netcore $out/bin
-    '';
-  };
-
-  # TODO: upstream
   avrogen = pkgs.buildDotnetGlobalTool {
     pname = "avrogen";
     nugetName = "Apache.Avro.Tools";
@@ -56,7 +40,7 @@ mkShell {
 
   shellHook = ''
 
-    export NUGET_PLUGIN_PATHS=${artifacts-credprovider}/bin/netcore/CredentialProvider.Microsoft/CredentialProvider.Microsoft.dll
+    export NUGET_PLUGIN_PATHS=${pkgs.khanelinix.artifacts-credprovider}/bin/netcore/CredentialProvider.Microsoft/CredentialProvider.Microsoft.dll
 
     echo 🔨 Dotnet DevShell
 
