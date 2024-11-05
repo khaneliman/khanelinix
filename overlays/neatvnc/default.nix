07923984cdae0fc46fd4669a291d5c8c7f12b14b
Austin Horstman <khaneliman12@gmail.com>
2024-11-05 08:30:29 -0600

    flake.lock: update

--- overlays/neatvnc/default.nix
+++
@@ -1,6 +0,0 @@
-_: _final: prev: {
-  # TODO: remove after https://github.com/NixOS/nixpkgs/pull/353338 is available
-  neatvnc = prev.neatvnc.overrideAttrs (oldAttrs: {
-    patches = oldAttrs.patches or [ ] ++ [ ./fix-ffmpeg.patch ];
-  });
-}
