afed185cea4ea8cca3a12a0842eec1533d2d62c9
Austin Horstman <khaneliman12@gmail.com>
2024-11-06 08:23:25 -0600

    overlays/btop: remove

--- overlays/btop/default.nix
+++
@@ -1,9 +0,0 @@
-_: _final: prev: {
-  # TODO: remove stdenv and buildinputs override after next unstable
-  btop = (prev.btop.override { inherit (prev) stdenv; }).overrideAttrs (_oldAttrs: {
-    buildInputs = prev.lib.optionals prev.stdenv.hostPlatform.isDarwin [
-      prev.apple-sdk_15
-      (prev.darwinMinVersionHook "10.15")
-    ];
-  });
-}
