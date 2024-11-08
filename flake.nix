41286b378a09d248a656d9fe8346646d574c643c
Austin Horstman <khaneliman12@gmail.com>
2024-11-07 21:35:01 -0600

    flake.lock: update

--- flake.nix
+++ flake.nix
@@ -181,7 +181,8 @@
 
     sops-nix-darwin = {
       # url = "github:Mic92/sops-nix/nix-darwin";
-      url = "github:khaneliman/sops-nix/nix-darwin";
+      # url = "github:khaneliman/sops-nix/nix-darwin";
+      url = "github:khaneliman/sops-nix/darwin";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
