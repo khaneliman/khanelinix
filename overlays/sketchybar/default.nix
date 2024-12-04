689077eb006db68879607a2cb19445d46a748794
Austin Horstman <khaneliman12@gmail.com>
2024-12-04 13:38:21 -0600

    overlays/sketchybar: remove version lock

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,14 +1,5 @@
 _: _final: prev: {
-  sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: rec {
-    # TODO: remove when https://github.com/NixOS/nixpkgs/pull/357010 available
-    version = "2.22.0";
-    src = prev.fetchFromGitHub {
-      owner = "FelixKratz";
-      repo = "sketchybar";
-      rev = "v${version}";
-      hash = "sha256-0082rRSfIKJFTAzmJ65ItEdLSwjFks5ZkTlVZqaWKEw=";
-    };
-
+  sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
     # Create secondary sketchybar executable for dynamic island
     installPhase = # bash
       ''
