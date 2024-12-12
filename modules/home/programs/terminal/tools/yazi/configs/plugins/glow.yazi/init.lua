7b7c5575ebab8e54d613769fcb6fdb42ffe45524
Austin Horstman <khaneliman12@gmail.com>
2024-12-12 09:44:22 -0600

    terminal/tools/yazi: glow 0.4 fix

--- modules/home/programs/terminal/tools/yazi/configs/plugins/glow.yazi/init.lua
+++ modules/home/programs/terminal/tools/yazi/configs/plugins/glow.yazi/init.lua
@@ -4,71 +4,51 @@
 
 local M = {}
 
-function M:peek()
+function M:peek(job)
 	local child = Command("glow")
 		:args({
 			"--style",
 			"$GLAMOUR_STYLE",
 			"--width",
-			tostring(self.area.w),
-			tostring(self.file.url),
+			tostring(job.area.w),
+			tostring(job.file.url),
 		})
+		:env("CLICOLOR_FORCE", "1")
 		:stdout(Command.PIPED)
 		:stderr(Command.PIPED)
 		:spawn()
 
 	if not child then
-		return self:fallback_to_builtin()
+		return require("code").peek(job)
 	end
 
-	local limit = self.area.h
+	local limit = job.area.h
 	local i, lines = 0, ""
 	repeat
 		local next, event = child:read_line()
 		if event == 1 then
-			return self:fallback_to_builtin()
+			return require("code").peek(job)
 		elseif event ~= 0 then
 			break
 		end
 
 		i = i + 1
-		if i > self.skip then
+		if i > job.skip then
 			lines = lines .. next
 		end
-	until i >= self.skip + limit
+	until i >= job.skip + limit
 
 	child:start_kill()
-	if self.skip > 0 and i < self.skip + limit then
-		ya.manager_emit(
-			"peek",
-			{ tostring(math.max(0, i - limit)), only_if = tostring(self.file.url), upper_bound = "" }
-		)
+	if job.skip > 0 and i < job.skip + limit then
+		ya.manager_emit("peek", { math.max(0, i - limit), only_if = job.file.url, upper_bound = true })
 	else
 		lines = lines:gsub("\t", string.rep(" ", PREVIEW.tab_size))
-		ya.preview_widgets(self, { ui.Text.parse(self.area, lines) })
+		ya.preview_widgets(job, { ui.Text.parse(lines):area(job.area) })
 	end
 end
 
-function M:seek(units)
-	local h = cx.active.current.hovered
-	if h and h.url == self.file.url then
-		local step = math.floor(units * self.area.h / 10)
-		ya.manager_emit("peek", {
-			tostring(math.max(0, cx.active.preview.skip + step)),
-			only_if = tostring(self.file.url),
-		})
-	end
-end
-
-function M:fallback_to_builtin()
-	local err, bound = ya.preview_code(self)
-	if bound then
-		ya.manager_emit("peek", { bound, only_if = self.file.url, upper_bound = true })
-	elseif err and not err:find("cancelled", 1, true) then
-		ya.preview_widgets(self, {
-			ui.Text(self.area, { ui.Line(err):reverse() }),
-		})
-	end
+function M:seek(job)
+	require("code").seek(job, job.units)
 end
 
 return M
