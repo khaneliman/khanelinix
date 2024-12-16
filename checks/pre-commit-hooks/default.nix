86b347e22945f30297ba9e79a077e171a06c7a60
Austin Horstman <khaneliman12@gmail.com>
2024-12-15 22:14:38 -0600

    checks/pre-commit-hooks: sync with treefmt

--- checks/pre-commit-hooks/default.nix
+++ checks/pre-commit-hooks/default.nix
@@ -1,7 +1,5 @@
 {
   inputs,
-  lib,
-  namespace,
   pkgs,
   ...
 }:
@@ -10,69 +8,55 @@ let
 in
 pre-commit-hooks-nix.lib.${pkgs.system}.run {
   src = ./.;
-  hooks =
-    let
-      excludes = [
-        "flake.lock"
-        "CHANGELOG.md"
-      ];
-      fail_fast = true;
-      verbose = true;
-    in
-    {
-      actionlint.enable = true;
-      clang-format.enable = true;
-      clang-tidy.enable = true;
-      # conform.enable = true;
+  hooks = {
+    actionlint.enable = true;
 
-      deadnix = {
-        enable = true;
+    biome.enable = true;
 
-        settings = {
-          edit = true;
-        };
+    clang-format.enable = true;
+
+    clang-tidy.enable = true;
+
+    deadnix = {
+      enable = true;
+
+      settings = {
+        edit = true;
       };
-
-      eslint = {
-        enable = true;
-        package = pkgs.eslint_d;
-      };
-
-      git-cliff = {
-        enable = false;
-        inherit excludes fail_fast verbose;
-
-        always_run = true;
-        description = "pre-push hook for git-cliff";
-        entry = "${lib.getExe pkgs.${namespace}.git-cliff}";
-        language = "system";
-        stages = [ "pre-push" ];
-      };
-
-      luacheck.enable = true;
-
-      nixfmt-rfc-style.enable = true;
-
-      pre-commit-hook-ensure-sops.enable = true;
-
-      prettier = {
-        enable = true;
-        inherit excludes fail_fast verbose;
-
-        description = "pre-commit hook for prettier";
-        settings = {
-          binPath = "${lib.getExe pkgs.prettierd}";
-          write = true;
-        };
-      };
-
-      shfmt = {
-        enable = true;
-
-        excludes = [ ".*.p10k.zsh$" ];
-      };
-
-      statix.enable = true;
-      # treefmt.enable = true;
     };
+
+    denofmt = {
+      enable = true;
+      excludes = [
+        ".*.ts$"
+        ".*.js$"
+      ];
+    };
+
+    isort.enable = true;
+
+    luacheck.enable = true;
+
+    nixfmt-rfc-style.enable = true;
+
+    pre-commit-hook-ensure-sops.enable = true;
+
+    ruff.enable = true;
+
+    rustfmt.enable = true;
+
+    shfmt = {
+      enable = true;
+
+      excludes = [ ".*.p10k.zsh$" ];
+    };
+
+    statix.enable = true;
+
+    stylua.enable = true;
+
+    # treefmt.enable = true;
+
+    yamlfmt.enable = true;
+  };
 }
