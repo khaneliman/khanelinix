19b19ba32221a711fa06f6d65f659dc31c65611f
Austin Horstman <khaneliman12@gmail.com>
2024-12-19 08:50:25 -0600

    overlays/nixpkgs-review: bump to latest

--- overlays/nixpkgs-review/default.nix
+++ overlays/nixpkgs-review/default.nix
@@ -1,14 +1,13 @@
 _: _final: prev: {
   nixpkgs-review = prev.nixpkgs-review.overridePythonAttrs {
     src = prev.fetchFromGitHub {
-      owner = "GaetanLepage";
+      owner = "Mic92";
       repo = "nixpkgs-review";
-      rev = "b39402126bdd48d7604634f428687267cf8f3b77";
-      hash = "sha256-nDNxnyu3M7yuL/Md4BJZzFGc5kFzOIUhpwi45pafjw8=";
+      rev = "b657186a6f2d8242106963742079b31d69c777f4";
+      hash = "sha256-go08YwZbua0R3xSZrTEfe5h/OUJGgxrheX2rYN326/o=";
     };
     dependencies = with prev.python3Packages; [
       argcomplete
-      requests
     ];
   };
 }
