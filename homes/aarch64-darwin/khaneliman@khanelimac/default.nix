d6451cd0d8f99df586b11a47bcd521ee889fd391
Austin Horstman <khaneliman12@gmail.com>
2024-12-19 20:12:09 -0600

    khanelimac: add thunderbird accounts

--- homes/aarch64-darwin/khaneliman@khanelimac/default.nix
+++ homes/aarch64-darwin/khaneliman@khanelimac/default.nix
@@ -17,7 +17,28 @@ in
 
     programs = {
       graphical = {
-        apps.thunderbird = enabled;
+        apps = {
+          thunderbird = {
+            enable = true;
+            extraAccounts =
+              let
+                # Not super secret, just doesn't need to be scraped so easily.
+                outlook = lib.${namespace}.decode "a2hhbmVsaW1hbjEyQG91dGxvb2suY29t";
+                personal = lib.${namespace}.decode "YXVzdGluLm0uaG9yc3RtYW5AZ21haWwuY29t";
+
+              in
+              {
+                ${outlook} = {
+                  address = outlook;
+                  flavor = "outlook.office365.com";
+                };
+                ${personal} = {
+                  address = personal;
+                  flavor = "gmail.com";
+                };
+              };
+          };
+        };
 
         bars = {
           sketchybar = enabled;
