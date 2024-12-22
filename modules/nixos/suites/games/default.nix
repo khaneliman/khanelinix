475d07090660edaec16a7e945df739c945a764b5
Austin Horstman <khaneliman12@gmail.com>
2024-12-22 11:14:21 -0600

    nixos/suites/games: add sober flatpak package
    Used for roblox

--- modules/nixos/suites/games/default.nix
+++ modules/nixos/suites/games/default.nix
@@ -31,5 +31,13 @@ in
         };
       };
     };
+
+    services.flatpak.packages = [
+      {
+        # Sober for Roblox
+        flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
+        sha256 = "sha256:1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
+      }
+    ];
   };
 }
