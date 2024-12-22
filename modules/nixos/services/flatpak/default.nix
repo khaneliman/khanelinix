8bd3f2b15307a7c624470ca39828fe6734bcee28
Austin Horstman <khaneliman12@gmail.com>
2024-12-22 10:27:41 -0600

    nixos/services/flatpak: add support for adding flatpakrepos

--- modules/nixos/services/flatpak/default.nix
+++ modules/nixos/services/flatpak/default.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   namespace,
+  pkgs,
   ...
 }:
 let
@@ -13,7 +14,31 @@ in
 {
   options.${namespace}.services.flatpak = {
     enable = mkBoolOpt false "Whether or not to enable flatpak support.";
+    extraRepos = lib.mkOption {
+      default = {
+        flathub = "https://flathub.org/repo/flathub.flatpakrepo";
+      };
+      type = lib.types.attrsOf lib.types.str;
+      description = "Extra flatpak repositories to add.";
+    };
   };
 
-  config = mkIf cfg.enable { services.flatpak.enable = true; };
+  config = mkIf cfg.enable {
+    services.flatpak.enable = true;
+    systemd.services.flatpak-repos = {
+      wantedBy = [ "multi-user.target" ];
+      path = [ pkgs.flatpak ];
+      script =
+        let
+          generateRepoScript =
+            repos:
+            lib.concatStringsSep "\n" (
+              lib.mapAttrsToList (name: url: ''
+                flatpak remote-add --if-not-exists ${name} ${url}
+              '') repos
+            );
+        in
+        generateRepoScript cfg.extraRepos;
+    };
+  };
 }
