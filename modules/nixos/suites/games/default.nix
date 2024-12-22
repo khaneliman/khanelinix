c40fee55e5715aff1bdcb656d2134a1c08bf49b7
Austin Horstman <khaneliman12@gmail.com>
2024-12-22 11:46:15 -0600

    nixos/suites/games: use my module that forwards options

--- modules/nixos/suites/games/default.nix
+++ modules/nixos/suites/games/default.nix
@@ -30,14 +30,14 @@ in
           };
         };
       };
-    };
 
-    services.flatpak.packages = [
-      {
-        # Sober for Roblox
-        flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
-        sha256 = "sha256:1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
-      }
-    ];
+      services.flatpak.extraPackages = [
+        {
+          # Sober for Roblox
+          flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
+          sha256 = "sha256:1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
+        }
+      ];
+    };
   };
 }
