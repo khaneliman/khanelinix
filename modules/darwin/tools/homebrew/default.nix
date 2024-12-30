f731f6466dccc8328a63f4c7b4d07ba2b20f650c
Austin Horstman <khaneliman12@gmail.com>
2024-12-29 22:15:03 -0600

    homebrew: add homebrew variables

--- modules/darwin/tools/homebrew/default.nix
+++ modules/darwin/tools/homebrew/default.nix
@@ -17,6 +17,13 @@ in
   };
 
   config = mkIf cfg.enable {
+    # https://docs.brew.sh/Manpage#environment
+    environment.variables = {
+      HOMEBREW_BAT = "1";
+      HOMEBREW_NO_ANALYTICS = "1";
+      HOMEBREW_NO_INSECURE_REDIRECT = "1";
+    };
+
     homebrew = {
       enable = true;
 
