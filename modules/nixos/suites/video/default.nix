93cb8643b2093a8da5637aa312eb5fbb77ebfa9e
Austin Horstman <khaneliman12@gmail.com>
2024-12-30 22:54:02 -0600

    nixos/suites/video: replace k3b module with kdePackages.k3b

--- modules/nixos/suites/video/default.nix
+++ modules/nixos/suites/video/default.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   namespace,
+  pkgs,
   ...
 }:
 let
@@ -15,19 +16,9 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    # NOTE: dvd burning software requires cdrom group and k3b dependencies in nix store do not have those permissions
-    # https://github.com/NixOS/nixpkgs/issues/19154#issuecomment-647005545
-    # Additionally to installing `k3b` enabling this will
-    # add `setuid` wrappers in `/run/wrappers/bin`
-    # for both `cdrdao` and `cdrecord`. On first
-    # run you must manually configure the path of `cdrdae` and
-    # `cdrecord` to correspond to the appropriate paths under
-    # `/run/wrappers/bin` in the "Setup External Programs" menu.
-    # FIXME: broken nixpkgs
-    # programs.k3b.enable = true;
-
-    khanelinix = {
-      user.extraGroups = [ "cdrom" ];
-    };
+    environment.systemPackages = with pkgs; [
+      # FIXME: broken nixpkgs
+      # kdePackages.k3b
+    ];
   };
 }
