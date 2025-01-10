48c93d3b0ed17ababd3fce1a65512ac17a1326e6
Austin Horstman <khaneliman12@gmail.com>
2025-01-10 10:24:59 -0600

    neovim: disable passing config options to nixd
    Massive memory usage which has been causing oom

--- modules/home/programs/terminal/editors/neovim/default.nix
+++ modules/home/programs/terminal/editors/neovim/default.nix
@@ -30,17 +30,18 @@ in
       };
       packages = [
         (khanelivim.packages.${system}.default.extend {
-          plugins.lsp.servers.nixd.settings =
-            let
-              flake = ''(builtins.getFlake "${inputs.self}")'';
-            in
-            {
-              options = rec {
-                nix-darwin.expr = ''${flake}.darwinConfigurations.khanelimac.options'';
-                nixos.expr = ''${flake}.nixosConfigurations.khanelinix.options'';
-                home-manager.expr = ''${nixos.expr}.home-manager.users.type.getSubOptions [ ]'';
-              };
-            };
+          # FIXME: insane memory usage
+          # plugins.lsp.servers.nixd.settings =
+          #   let
+          #     flake = ''(builtins.getFlake "${inputs.self}")'';
+          #   in
+          #   {
+          #     options = rec {
+          #       nix-darwin.expr = ''${flake}.darwinConfigurations.khanelimac.options'';
+          #       nixos.expr = ''${flake}.nixosConfigurations.khanelinix.options'';
+          #       home-manager.expr = ''${nixos.expr}.home-manager.users.type.getSubOptions [ ]'';
+          #     };
+          #   };
         })
         pkgs.nvrh
       ];
