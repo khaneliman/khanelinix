05ab23f9eab8095c66f3db399f6fcb32e9d7796a
Austin Horstman <khaneliman12@gmail.com>
2025-01-22 22:30:33 -0600

    WIP

---
+++ modules/home/programs/terminal/editors/default.nix
@@ -0,0 +1,7 @@
+{
+  imports = [
+    ./helix
+    ./micro
+    ./neovim
+  ];
+}
---
+++ modules/home/programs/terminal/emulators/default.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ./alacritty
+    ./foot
+    ./ghostty
+    ./kitty
+    ./warp
+    ./wezterm
+  ];
+}
