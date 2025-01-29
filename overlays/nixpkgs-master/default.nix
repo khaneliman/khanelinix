29b4ed9b582c37a3c4bf01d7c4854e9ff27cca22
Austin Horstman <khaneliman12@gmail.com>
2025-01-28 21:03:52 -0600

    overlays/nixpkgs-master: add hyprland and xdph

--- overlays/nixpkgs-master/default.nix
+++ overlays/nixpkgs-master/default.nix
@@ -1,8 +1,10 @@
 { channels, ... }:
 _final: _prev: {
   inherit (channels.nixpkgs-master)
+    hyprland
     hyprland-qt-support
     hyprpolkitagent
     hyprsysteminfo
+    xdg-desktop-portal-hyprland
     ;
 }
