a332834f22e88b0bad4e58c075aa4defa2c4963f
Austin Horstman <khaneliman12@gmail.com>
2025-01-29 12:41:29 -0600

    wezterm: disable window decorations always
    Not sure why it worked before?

--- modules/home/programs/terminal/emulators/wezterm/default.nix
+++ modules/home/programs/terminal/emulators/wezterm/default.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   namespace,
-  pkgs,
   ...
 }:
 let
@@ -178,7 +177,7 @@ in
             },
             window_background_opacity = 0.85,
             window_close_confirmation = "NeverPrompt",
-            window_decorations = ${if pkgs.stdenv.isDarwin then "RESIZE" else "NONE"},
+            window_decorations = "NONE",
             window_padding = { left = 12, right = 12, top = 12, bottom = 12, },
           }
         '';
