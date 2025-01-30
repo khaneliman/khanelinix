a13a5271bbcc5acdfb6b04bec28ecb757426cb3f
Austin Horstman <khaneliman12@gmail.com>
2025-01-30 09:34:03 -0600

    overlays/sketchybar: bump to latest
    Has nice improvements for the popups. Remove after
    https://github.com/NixOS/nixpkgs/pull/377896 is available

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,5 +1,13 @@
 _: _final: prev: {
-  sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
+  sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: rec {
+    version = "2.22.1";
+    src = prev.fetchFromGitHub {
+      owner = "FelixKratz";
+      repo = "SketchyBar";
+      rev = "v${version}";
+      hash = "sha256-272lrH0ee4z4hcY4Hqt/UxjGwH6RFPEP4n0jz6Ab/+c=";
+    };
+
     # Create secondary sketchybar executable for dynamic island
     installPhase = # bash
       ''
