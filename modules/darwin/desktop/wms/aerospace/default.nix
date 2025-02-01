bcf4990e70c704d39f70d4b012b094932cfbcb70
Austin Horstman <khaneliman12@gmail.com>
2025-01-31 18:49:47 -0600

    darwin/wms/aerospace: init

---
+++ modules/darwin/desktop/wms/aerospace/default.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+let
+  inherit (lib.${namespace}) mkBoolOpt mkOpt enabled;
+  cfg = config.${namespace}.desktop.wms.aerospace;
+in
+{
+  options.${namespace}.desktop.wms.aerospace = {
+    enable = mkBoolOpt false "Whether or not to enable aerospace.";
+    debug = mkBoolOpt false "Whether to enable debug output.";
+    logFile =
+      mkOpt lib.types.str "/Users/khaneliman/Library/Logs/aerospace.log"
+        "Filepath of log output";
+  };
+
+  config = lib.mkIf cfg.enable {
+    ${namespace} = {
+      desktop.addons.jankyborders = enabled;
+
+      home.extraOptions = {
+        home.shellAliases = {
+          restart-aerospace = ''launchctl kickstart -k gui/"$(id -u)"/org.nixos.aerospace'';
+        };
+      };
+    };
+
+    services.aerospace = {
+      enable = true;
+      package = pkgs.aerospace;
+
+      settings = {
+        mode.main.binding = {
+        };
+
+        mode.resize.binding = {
+        };
+      };
+    };
+  };
+}
