eb928d65147671a8e644d1224f1f032d41f693f6
Austin Horstman <khaneliman12@gmail.com>
2025-02-03 09:27:39 -0600

    nixos/services/ollama: init

---
+++ modules/nixos/services/ollama/default.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.services.ollama;
+
+  amdCfg = config.hardware.gpu.amd;
+
+in
+{
+  options.${namespace}.services.ollama = {
+    enable = mkBoolOpt false "Whether to enable ollama.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.ollama = {
+      enable = true;
+      rocmOverrideGfx = lib.mkIf (amdCfg.enable && amdCfg.enableRocmSupport) "11.0.2";
+
+      environmentVariables = {
+        HCC_AMDGPU_TARGET = lib.mkIf (amdCfg.enable && amdCfg.enableRocmSupport) "gfx1102";
+      };
+    };
+  };
+}
