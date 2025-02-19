98b1ddf53eb3dec64db5df35c82c0a230c9652d8
Austin Horstman <khaneliman12@gmail.com>
2025-02-18 21:16:06 -0600

    flake.nix: clean up imports

--- flake.nix
+++ flake.nix
@@ -2,93 +2,60 @@
   description = "KhaneliNix";
 
   inputs = {
-
-    #          ╭──────────────────────────────────────────────────────────╮
-    #          │                       Core System                        │
-    #          ╰──────────────────────────────────────────────────────────╯
+    # Core inputs
     darwin = {
-      # url = "github:lnl7/nix-darwin";
-      # NOTE: Upstream slow to respond to PRs, using own fork.
-      url = "github:khaneliman/nix-darwin/cherry-picks";
-      # url = "git+file:///Users/khaneliman/github/nix-darwin";
+      url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-
-    git-hooks-nix.url = "github:cachix/git-hooks.nix";
-
     home-manager.url = "github:nix-community/home-manager";
-    # home-manager.url = "github:khaneliman/home-manager/thunderbird";
-    # home-manager.url = "git+file:///home/khaneliman/Documents/github/home-manager";
-    # home-manager.url = "git+file:///Users/khaneliman/Documents/github/home-manager";
-
-    # Secure boot
-    lanzaboote.url = "github:nix-community/lanzaboote/v0.4.2";
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs-master.url = "github:nixos/nixpkgs";
-
     nixos-wsl = {
       url = "github:nix-community/nixos-wsl";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     snowfall-lib = {
       url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Secrets management
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-    };
-
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-
-    #          ╭──────────────────────────────────────────────────────────╮
-    #          │                    System Deployment                     │
-    #          ╰──────────────────────────────────────────────────────────╯
     deploy-rs.url = "github:serokell/deploy-rs";
     disko.url = "github:nix-community/disko/latest";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    lanzaboote.url = "github:nix-community/lanzaboote/v0.4.2";
+    sops-nix.url = "github:Mic92/sops-nix";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
 
-    #          ╭──────────────────────────────────────────────────────────╮
-    #          │                       Applications                       │
-    #          ╰──────────────────────────────────────────────────────────╯
+    # Applications
     anyrun.url = "github:anyrun-org/anyrun";
     anyrun-nixos-options.url = "github:n3oney/anyrun-nixos-options";
-
     catppuccin-cursors.url = "github:catppuccin/cursors";
     catppuccin.url = "github:catppuccin/nix";
-
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     hyprland.url = "github:hyprwm/Hyprland";
     hypr-socket-watch.url = "github:khaneliman/hypr-socket-watch";
-
     khanelivim = {
       url = "github:khaneliman/khanelivim";
-      # url = "git+file:///Users/khaneliman/Documents/github/khanelivim";
-      # url = "git+file:///home/khaneliman/Documents/github/khanelivim";
       inputs = {
-        # nixpkgs.follows = "nixpkgs";
         yazi.follows = "yazi";
         git-hooks-nix.follows = "git-hooks-nix";
       };
     };
-
     nix-flatpak.url = "github:gmodena/nix-flatpak/latest";
     nix-index-database.url = "github:nix-community/nix-index-database";
     snowfall-flake.url = "github:snowfallorg/flake";
     waybar.url = "github:Alexays/Waybar";
     yazi.url = "github:sxyazi/yazi";
-
     yazi-plugins = {
       url = "github:yazi-rs/plugins";
       flake = false;
     };
+
+    # Extra nixpkgs
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-master.url = "github:nixos/nixpkgs";
   };
 
   outputs =
