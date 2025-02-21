af9cfa2bc0fb9b4a7490fc6afed2da53821c8675
Austin Horstman <khaneliman12@gmail.com>
2025-02-21 14:08:24 -0600

    systems/nixos: fix defaultSession

--- systems/x86_64-linux/nixos/default.nix
+++ systems/x86_64-linux/nixos/default.nix
@@ -50,7 +50,7 @@ in
     };
   };
 
-  services.displayManager.defaultSession = "hyprland";
+  services.displayManager.defaultSession = "hyprland-uwsm";
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
