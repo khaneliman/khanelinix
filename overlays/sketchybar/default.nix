4337bbc2a4df8096bf35d1ae36030759742a7b8a
Austin Horstman <khaneliman12@gmail.com>
2025-02-28 09:07:54 -0500

    overlays/sketchybar: remove fixed version

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,13 +1,5 @@
 _: _final: prev: {
-  sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: rec {
-    version = "2.22.1";
-    src = prev.fetchFromGitHub {
-      owner = "FelixKratz";
-      repo = "SketchyBar";
-      rev = "v${version}";
-      hash = "sha256-272lrH0ee4z4hcY4Hqt/UxjGwH6RFPEP4n0jz6Ab/+c=";
-    };
-
+  sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
     # Create secondary sketchybar executable for dynamic island
     installPhase = # bash
       ''
