932488152f20468ba9af964331c90cc275683164
Austin Horstman <khaneliman12@gmail.com>
2025-03-06 09:57:51 -0600

    overlays/looking-glass: bump to latest release

--- overlays/looking-glass/default.nix
+++ overlays/looking-glass/default.nix
@@ -1,16 +1,20 @@
 _: _final: prev: {
   looking-glass-client =
     (prev.looking-glass-client.override { stdenv = prev.gcc13Stdenv; }).overrideAttrs
-      (_oldAttrs: rec {
-        rev = "0-unstable-2024-10-24";
+      (_oldAttrs: {
+        rev = "B7";
         src = prev.fetchFromGitHub {
           owner = "gnif";
           repo = "LookingGlass";
-          rev = "e25492a3a36f7e1fde6e3c3014620525a712a64a";
-          hash = "sha256-DBmCJRlB7KzbWXZqKA0X4VTpe+DhhYG5uoxsblPXVzg=";
+          rev = "B7";
+          hash = "sha256-I84oVLeS63mnR19vTalgvLvA5RzCPTXV+tSsw+ImDwQ=";
           fetchSubmodules = true;
         };
 
         patches = [ ];
+        postInstall = ''
+          mkdir -p $out/share/pixmaps
+          cp $src/resources/lg-logo.png $out/share/pixmaps
+        '';
       });
 }
