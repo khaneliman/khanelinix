c63b3ca6521dae2945e10c0a4e3ffba805caa7d2
Austin Horstman <khaneliman12@gmail.com>
2025-03-08 13:12:24 -0600

    terminal/tools/navi: init

---
+++ modules/home/programs/terminal/tools/navi/default.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) mkBoolOpt;
+
+  cfg = config.${namespace}.programs.terminal.tools.navi;
+in
+{
+  options.${namespace}.programs.terminal.tools.navi = {
+    enable = mkBoolOpt false "Whether or not to enable navi.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.navi = {
+      enable = true;
+
+      settings = {
+        style = {
+          tag = {
+            color = "green"; # text color. possible values: https://bit.ly/3gloNNI
+            # width_percentage = 26; # column width relative to the terminal window
+            # min_width = 20; # minimum column width as number of characters
+          };
+          comment = {
+            color = "blue";
+            # width_percentage = 42;
+            # min_width = 45;
+          };
+          snippet = {
+            color = "white";
+          };
+        };
+      };
+    };
+  };
+}
