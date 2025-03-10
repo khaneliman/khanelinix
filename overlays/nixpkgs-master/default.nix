c709fbfe2058f51fbfbad3ab91f1ee78f75af241
Austin Horstman <khaneliman12@gmail.com>
2025-03-10 14:14:34 -0500

    flake.lock: update

--- overlays/nixpkgs-master/default.nix
+++ overlays/nixpkgs-master/default.nix
@@ -3,10 +3,5 @@ _final: _prev: {
   inherit (channels.nixpkgs-master)
     # TODO: remove when it makes it to nixos-unstable
     firefox-devedition
-    looking-glass-client
-    linuxKernel
-    tailscale
-    # Broken hash on python
-    vscode-extensions
     ;
 }
