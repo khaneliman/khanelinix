d0ef9ac327ed60d1de9029e87af54d151ef03bfa
Austin Horstman <khaneliman12@gmail.com>
2025-03-12 21:53:38 -0500

    sketchybar: swap to spaces windowrule

--- modules/home/programs/graphical/bars/sketchybar/config/items/init.lua
+++ modules/home/programs/graphical/bars/sketchybar/config/items/init.lua
@@ -1,8 +1,8 @@
 #!/usr/bin/env lua
 
 require("items.apple")
-require("items.spaces")
--- require("items.spaces-windowrule")
+-- require("items.spaces")
+require("items.spaces-windowrule")
 require("items.skhd")
 require("items.front_app")
 require("items.today")
