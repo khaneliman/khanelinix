35714f256d8145a954d5df4da036634c6882bf54
Austin Horstman <khaneliman12@gmail.com>
2025-03-17 16:31:23 -0500

    homes: all use default authorizedKeys
    Default is centralized list of my host keys

--- homes/aarch64-darwin/khaneliman@khanelimac-m1/default.nix
+++ homes/aarch64-darwin/khaneliman@khanelimac-m1/default.nix
@@ -17,18 +17,7 @@ in
     programs = {
       terminal = {
         tools = {
-          ssh = {
-            enable = true;
-
-            authorizedKeys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEpfTVxQKmkAYOrsnroZoTk0LewcBIC4OjlsoJY6QbB0"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7UBwfd7+K0mdkAIb2TE6RzMu6L4wZnG/anuoYqJMPB"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuMXeT21L3wnxnuzl0rKuE5+8inPSi8ca/Y3ll4s9pC"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEilFPAgSUwW3N7PTvdTqjaV2MD3cY2oZGKdaS7ndKB"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAZIwy7nkz8CZYR/ZTSNr+7lRBW2AYy1jw06b44zaID" # Khanelimac
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBG8l3jQ2EPLU+BlgtaQZpr4xr97n2buTLAZTxKHSsD" # Khaneliman@khanelimac
-            ];
-          };
+          ssh = enabled;
         };
       };
     };
