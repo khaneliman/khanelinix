564229911ffa650b8f6bb4a8b53bc5d038b98503
Austin Horstman <khaneliman12@gmail.com>
2025-03-17 16:29:24 -0500

    home/ssh: centralized host authorizedKeys
    Store list as the default option value instead of copy pasting
    throughout code

--- modules/home/programs/terminal/tools/ssh/default.nix
+++ modules/home/programs/terminal/tools/ssh/default.nix
@@ -18,7 +18,17 @@ let
   user = config.users.users.${config.${namespace}.user.name};
   user-id = builtins.toString user.uid;
 
-  default-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAZIwy7nkz8CZYR/ZTSNr+7lRBW2AYy1jw06b44zaID";
+  authorizedKeys = [
+    # `khanelinix`
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuMXeT21L3wnxnuzl0rKuE5+8inPSi8ca/Y3ll4s9pC"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEilFPAgSUwW3N7PTvdTqjaV2MD3cY2oZGKdaS7ndKB"
+    # `khanelimac`
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAZIwy7nkz8CZYR/ZTSNr+7lRBW2AYy1jw06b44zaID"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINBG8l3jQ2EPLU+BlgtaQZpr4xr97n2buTLAZTxKHSsD"
+    # `bruddynix`
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFeLt5cnRnKeil39Ds+CimMJQq/5dln32YqQ+EfYSCvc"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqCiZgjOmhsBTAFD0LbuwpfeuCnwXwMl2wByxC1UiRt"
+  ];
 
   other-hosts = lib.filterAttrs (_key: host: (host.config.${namespace}.user.name or null) != null) (
     (inputs.self.nixosConfigurations or { }) // (inputs.self.darwinConfigurations or { })
@@ -51,7 +61,7 @@ in
 {
   options.${namespace}.programs.terminal.tools.ssh = with types; {
     enable = mkBoolOpt false "Whether or not to configure ssh support.";
-    authorizedKeys = mkOpt (listOf str) [ default-key ] "The public keys to apply.";
+    authorizedKeys = mkOpt (listOf str) authorizedKeys "The public keys to apply.";
     extraConfig = mkOpt str "" "Extra configuration to apply.";
     port = mkOpt port 2222 "The port to listen on (in addition to 22).";
   };
