e79d694fd2d195642f772c80509e86baaffa8a2e
Austin Horstman <khaneliman12@gmail.com>
2025-03-21 16:49:43 -0500

    nix: khanelimac only builds on m1 for x86
    Still haven't figured out how to properly prefer local builds while
    allowing remotes for same arch...

--- modules/shared/nix/default.nix
+++ modules/shared/nix/default.nix
@@ -181,6 +181,10 @@ in
               }
               // lib.optionalAttrs (host == "khanelimac") {
                 sshKey = config.sops.secrets.khanelimac_khaneliman_ssh_key.path;
+                # Prefer local builds for personal usage
+                systems = [
+                  "x86_64-darwin"
+                ];
               }
             )
             (
