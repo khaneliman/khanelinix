f78cec2fc81a1596aabe82ce18d62d60f1c948c6
Austin Horstman <khaneliman12@gmail.com>
2025-03-21 22:48:58 -0500

    nixos/system/locale: explicitly declare more options

--- modules/nixos/system/locale/default.nix
+++ modules/nixos/system/locale/default.nix
@@ -15,16 +15,31 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.variables = {
-      # Set locale archive variable in case it isn't being set properly
-      LOCALE_ARCHIVE = "/run/current-system/sw/lib/locale/locale-archive";
-    };
-
-    i18n.defaultLocale = "en_US.UTF-8";
-
     console = {
       font = "Lat2-Terminus16";
       keyMap = mkForce "us";
+    };
+
+    environment.variables = {
+      LOCALE_ARCHIVE = "/run/current-system/sw/lib/locale/locale-archive";
+    };
+
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS = config.i18n.defaultLocale;
+        LC_IDENTIFICATION = config.i18n.defaultLocale;
+        LC_MEASUREMENT = config.i18n.defaultLocale;
+        LC_MONETARY = config.i18n.defaultLocale;
+        LC_NAME = config.i18n.defaultLocale;
+        LC_NUMERIC = config.i18n.defaultLocale;
+        LC_PAPER = config.i18n.defaultLocale;
+        LC_TELEPHONE = config.i18n.defaultLocale;
+        LC_TIME = config.i18n.defaultLocale;
+      };
+      supportedLocales = [
+        "en_US.UTF-8/UTF-8"
+      ];
     };
   };
 }
