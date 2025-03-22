4ad13c15c5160d81a83d46d49ee225dbf555174d
Austin Horstman <khaneliman12@gmail.com>
2025-03-22 10:20:41 -0500

    overlays/sketchybar: bump to latest git commit
    Added some new features

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,5 +1,13 @@
 _: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
+    doInstallCheck = false;
+    version = "2.22.1-2025-03-22";
+    src = prev.fetchFromGitHub {
+      owner = "FelixKratz";
+      repo = "SketchyBar";
+      rev = "a8691a3fca676557bc6d8c69167e61df7462bbfe";
+      hash = "sha256-oJ5fzyub5CYp3Is06uPflH63+3KS8R1FjVmQVG3VFIw=";
+    };
     # Create secondary sketchybar executable for dynamic island
     installPhase = # bash
       ''
