af0f4df77b4da2f880d079b2e1450db90119fff4
Austin Horstman <khaneliman12@gmail.com>
2025-03-25 16:23:17 -0500

    davmail: tweaks for work email

--- modules/home/services/davmail/default.nix
+++ modules/home/services/davmail/default.nix
@@ -17,6 +17,13 @@ in
   config = mkIf cfg.enable {
     services.davmail = {
       enable = true;
+
+      # NOTE: https://davmail.sourceforge.net/serversetup.html
+      settings = {
+        # FIXME: doesn't work with interactive
+        "davmail.mode" = "O365Manual";
+        "davmail.url" = "https://outlook.office365.com";
+      };
     };
   };
 }
