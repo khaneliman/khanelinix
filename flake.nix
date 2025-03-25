82a599f0b532de1d11e598ec826a6bf0f7c3c63b
Austin Horstman <khaneliman12@gmail.com>
2025-03-25 09:39:27 -0500

    flake.nix: add catppuccin-sddm-corners
    Add flake input until package update upstreamed

--- flake.nix
+++ flake.nix
@@ -81,6 +81,7 @@
       url = "github:catppuccin/nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    catppuccin-sddm-corners.url = "github:khaneliman/catppuccin-sddm-corners";
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
