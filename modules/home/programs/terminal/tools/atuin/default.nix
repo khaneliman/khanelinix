95f3e51cd6c57b686ad0472cdfb1e27a2b6cf7e3
Austin Horstman <khaneliman12@gmail.com>
2025-04-01 00:13:06 -0500

    atuin: customize to preference

--- modules/home/programs/terminal/tools/atuin/default.nix
+++ modules/home/programs/terminal/tools/atuin/default.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   namespace,
+  pkgs,
   ...
 }:
 let
@@ -31,6 +32,25 @@ in
         // lib.optionalAttrs cfg.enableDebug {
           logLevel = "debug";
         };
+
+      # flags = [
+      #   "--disable-up-arrow"
+      # ];
+
+      settings = {
+        enter_accept = true;
+        # Filter modes can still be toggled via `ctrl-r`
+        filter_mode = "workspace";
+        keymap_mode = "auto";
+        show_preview = true;
+        # NOTE: Whether to store commands that failed.
+        # Can be useful to auto prune, but lose commands that might have
+        # a simple typo to fix and would need to type again.
+        # store_failed = false;
+        style = "auto";
+        update_check = false;
+        workspaces = true;
+      };
     };
   };
 }
