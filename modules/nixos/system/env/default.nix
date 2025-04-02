cbdf4667860928c95e5272ae3bc9e11d5af13160
Austin Horstman <khaneliman12@gmail.com>
2025-04-02 14:21:42 -0500

    nixos/system/env: remove pointless XDG sessionVariables
    Handled through xdg home-manager module

--- modules/nixos/system/env/default.nix
+++ modules/nixos/system/env/default.nix
@@ -33,14 +33,6 @@ in
 
   config = {
     environment = {
-      sessionVariables = {
-        XDG_BIN_HOME = "$HOME/.local/bin";
-        XDG_CACHE_HOME = "$HOME/.cache";
-        XDG_CONFIG_HOME = "$HOME/.config";
-        XDG_DATA_HOME = "$HOME/.local/share";
-        XDG_DESKTOP_DIR = "$HOME";
-      };
-
       extraInit = lib.concatStringsSep "\n" (
         lib.mapAttrsToList (
           n: v: # bash
