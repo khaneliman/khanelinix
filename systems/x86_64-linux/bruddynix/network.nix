77cba6fc8c867884e188cea2145b9b8141bcbd58
Austin Horstman <khaneliman12@gmail.com>
2025-04-03 08:54:56 -0500

    bruddynix: use cloudflare dns for families

--- systems/x86_64-linux/bruddynix/network.nix
+++ systems/x86_64-linux/bruddynix/network.nix
@@ -1,4 +1,15 @@
-_: {
+{ lib, ... }:
+{
+  networking = {
+    nameservers = lib.mkForce [
+      # Cloudflare for families
+      "1.1.1.3"
+      "1.0.0.3"
+      "2606:4700:4700::1113"
+      "2606:4700:4700::1003"
+    ];
+  };
+
   systemd = {
     network.networks = {
       "30-network-defaults-wired" = {
