06816157682347dd196b9748b6e6e80d420d8df0
Austin Horstman <khaneliman12@gmail.com>
2025-04-08 22:03:16 -0500

    yazi: refactor config structure more

--- modules/home/programs/terminal/tools/yazi/default.nix
+++ modules/home/programs/terminal/tools/yazi/default.nix
@@ -87,13 +87,29 @@ in
 
       settings = lib.mkMerge [
         (import ./settings/input.nix)
-        (import ./settings/manager.nix)
         (import ./settings/open.nix)
         (import ./settings/opener.nix { inherit config lib pkgs; })
         (import ./settings/plugin.nix)
-        (import ./settings/preview.nix)
-        (import ./settings/tasks.nix)
         {
+          log = {
+            enabled = false;
+          };
+
+          manager = {
+            layout = [
+              1
+              3
+              4
+            ];
+            linemode = "custom";
+            show_hidden = true;
+            show_symlink = true;
+            sort_by = "alphabetical";
+            sort_dir_first = true;
+            sort_reverse = false;
+            sort_sensitive = false;
+          };
+
           pick = {
             open_title = "Open with:";
             open_origin = "hovered";
@@ -105,14 +121,39 @@ in
             ];
           };
 
+          preview = {
+            tab_size = 2;
+            max_width = 600;
+            max_height = 900;
+            image_filter = "triangle";
+            image_quality = 75;
+            sixel_fraction = 15;
+            ueberzug_scale = 1;
+            ueberzug_offset = [
+              0
+              0
+              0
+              0
+            ];
+            wrap = "yes";
+          };
+
+          tasks = {
+            micro_workers = 10;
+            macro_workers = 25;
+            bizarre_retry = 5;
+            image_alloc = 536870912; # 512MB
+            image_bound = [
+              0
+              0
+            ];
+            suppress_preload = false;
+          };
+
           which = {
             sort_by = "none";
             sort_sensitive = false;
             sort_reverse = false;
-          };
-
-          log = {
-            enabled = false;
           };
         }
       ];
