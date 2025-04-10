5eac840ef5865cc547a53b6e0b5a7ed605e58ed9
Austin Horstman <khaneliman12@gmail.com>
2025-04-10 09:30:12 -0500

    nixos/networking/networkmanager: disable resolvconf

--- modules/nixos/system/networking/networkmanager/default.nix
+++ modules/nixos/system/networking/networkmanager/default.nix
@@ -14,29 +14,33 @@ in
   config = mkIf (cfg.enable && cfg.manager == "networkmanager") {
     khanelinix.user.extraGroups = [ "networkmanager" ];
 
-    networking.networkmanager = {
-      enable = true;
+    networking = {
+      networkmanager = {
+        enable = true;
 
-      connectionConfig = {
-        # Enable if `mdns` is not handled by avahi
-        "connection.mdns" = lib.mkIf (!config.services.avahi.enable) "2";
+        connectionConfig = {
+          # Enable if `mdns` is not handled by avahi
+          "connection.mdns" = lib.mkIf (!config.services.avahi.enable) "2";
+        };
+
+        plugins = with pkgs; [
+          networkmanager-l2tp
+          networkmanager-openvpn
+          networkmanager-sstp
+          networkmanager-vpnc
+        ];
+
+        unmanaged =
+          [
+            "interface-name:br-*"
+            "interface-name:rndis*"
+          ]
+          ++ lib.optionals config.${namespace}.services.tailscale.enable [ "interface-name:tailscale*" ]
+          ++ lib.optionals config.${namespace}.virtualisation.podman.enable [ "interface-name:docker*" ]
+          ++ lib.optionals config.${namespace}.virtualisation.kvm.enable [ "interface-name:virbr*" ];
       };
 
-      plugins = with pkgs; [
-        networkmanager-l2tp
-        networkmanager-openvpn
-        networkmanager-sstp
-        networkmanager-vpnc
-      ];
-
-      unmanaged =
-        [
-          "interface-name:br-*"
-          "interface-name:rndis*"
-        ]
-        ++ lib.optionals config.${namespace}.services.tailscale.enable [ "interface-name:tailscale*" ]
-        ++ lib.optionals config.${namespace}.virtualisation.podman.enable [ "interface-name:docker*" ]
-        ++ lib.optionals config.${namespace}.virtualisation.kvm.enable [ "interface-name:virbr*" ];
+      resolvconf.enable = false;
     };
 
     systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
