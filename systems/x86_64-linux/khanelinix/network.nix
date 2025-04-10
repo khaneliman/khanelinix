d7d57b522c0299021790eae0c34cc667d56ee486
Austin Horstman <khaneliman12@gmail.com>
2025-04-10 14:18:27 -0500

    khanelinix: tweak network

--- systems/x86_64-linux/khanelinix/network.nix
+++ systems/x86_64-linux/khanelinix/network.nix
@@ -15,9 +15,9 @@
       "30-network-defaults-wired" = {
         matchConfig.Name = "en* | eth* | usb*";
         networkConfig = {
-          # Address = "192.168.1.3/24";
-          # Gateway = "192.168.1.1";
-          DHCP = "ipv4";
+          Address = "192.168.1.3/24";
+          Gateway = "192.168.1.1";
+          # DHCP = "ipv4";
           # Enable if `mdns` is not handled by avahi
           MulticastDNS = !config.services.avahi.enable;
           # IPv6AcceptRA = true;
