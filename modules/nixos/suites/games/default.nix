28cf476163c639694311e94c178cba62a7d4c2a5
Austin Horstman <khaneliman12@gmail.com>
2025-04-11 15:38:24 -0500

    nixos/suites/games: sober moved to flathub

--- modules/nixos/suites/games/default.nix
+++ modules/nixos/suites/games/default.nix
@@ -32,10 +32,10 @@ in
       };
 
       services.flatpak.extraPackages = [
+        # Sober for Roblox
         {
-          # Sober for Roblox
-          flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
-          sha256 = "sha256:1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
+          appId = "org.vinegarhq.Sober";
+          origin = "flathub";
         }
       ];
     };
