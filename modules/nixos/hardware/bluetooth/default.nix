9f798feaec3612afcfdeaca9a9a3df4455a1c368
Austin Horstman <khaneliman12@gmail.com>
2025-04-11 12:54:54 -0500

    nixos/hardware/bluetooth: add autoConnect option

--- modules/nixos/hardware/bluetooth/default.nix
+++ modules/nixos/hardware/bluetooth/default.nix
@@ -13,6 +13,11 @@ in
 {
   options.${namespace}.hardware.bluetooth = {
     enable = lib.mkEnableOption "support for extra bluetooth devices";
+    autoConnect = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to automatically connect to paired devices on startup";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -20,13 +25,19 @@ in
       enable = true;
 
       package = pkgs.bluez-experimental;
-      powerOnBoot = true;
+      powerOnBoot = false;
 
       settings = {
         General = {
           Experimental = true;
           JustWorksRepairing = "always";
+          KernelExperimental = true;
           MultiProfile = "multiple";
+        };
+        Policy = {
+          AutoEnable = cfg.autoConnect;
+          ReconnectAttempts = if cfg.autoConnect then 7 else 0;
+          ReconnectIntervals = "1,2,4,8,16,32,64";
         };
       };
     };
