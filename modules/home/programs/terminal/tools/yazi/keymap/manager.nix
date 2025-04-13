59bf72b7b62cf439642bc41150f267563569ef8b
Austin Horstman <khaneliman12@gmail.com>
2025-04-13 10:38:56 -0500

    yazi: keymaps proper plugin mapping conditionals

--- modules/home/programs/terminal/tools/yazi/keymap/manager.nix
+++ modules/home/programs/terminal/tools/yazi/keymap/manager.nix
@@ -17,82 +17,79 @@ let
       ;
   };
   navigation = import ./manager/navigation.nix;
-  operation = import ./manager/operation.nix;
+  operation = import ./manager/operation.nix { inherit config lib; };
 in
 {
   manager = {
     prepend_keymap =
-      [
+      lib.optional pkgs.stdenv.hostPlatform.isLinux {
+        on = [ "<C-v>" ];
+        run = "shell 'dragon -x -i -T \"$1\"'";
+        desc = "Drag and drop files";
+      }
+      ++ lib.optionals (lib.hasAttr "smart-enter" enabledPlugins) [
         {
           on = [ "l" ];
           run = "plugin smart-enter";
           desc = "Enter the child directory, or open the file";
         }
-        {
-          on = [ "M" ];
-          run = "plugin mount";
-          desc = "Mount devices";
-        }
+
         {
           on = [ "<Right>" ];
           run = "plugin smart-enter";
           desc = "Enter the child directory, or open the file";
         }
-        {
-          on = [ "<C-v>" ];
-          run = "shell 'dragon -x -i -T \"$1\"'";
-          desc = "Drag and drop files";
-        }
-        {
-          on = [ "<A-d>" ];
-          run = "plugin diff";
-          desc = "Diff the selected with the hovered file";
-        }
-        {
-          on = [
-            "c"
-            "m"
-          ];
-          run = "plugin chmod";
-          desc = "Chmod on selected files";
-        }
-        {
-          on = [ "T" ];
-          run = "plugin toggle-pane min-preview";
-          desc = "Hide or show preview";
-        }
-        # {
-        #   on = [ "T" ];
-        #   run = "plugin toggle-pane max-preview";
-        #   desc = "Maximize or restore preview";
-        # }
-        {
-          on = [ "f" ];
-          run = "plugin jump-to-char";
-          desc = "Jump to char";
-        }
-        {
-          on = [ "F" ];
-          run = "plugin smart-filter";
-          desc = "Smart filter";
-        }
       ]
-      ++ lib.optionals (lib.hasAttr "ouch" enabledPlugins) [
-        {
-          on = [ "C" ];
-          run = "plugin ouch";
-          desc = "Compress with outch";
-        }
-      ]
-      ++ lib.optionals config.${namespace}.suites.wlroots.enable [
-        {
-          on = [ "y" ];
-          run = [
-            ''shell -- for path in "$@"; do echo "file://$path"; done | wl-copy -t text/uri-list''
-            "yank"
-          ];
-        }
-      ]
+      ++ lib.optional (lib.hasAttr "mount" enabledPlugins) {
+        on = [ "M" ];
+        run = "plugin mount";
+        desc = "Mount devices";
+      }
+      ++ lib.optional (lib.hasAttr "diff" enabledPlugins) {
+        on = [ "<A-d>" ];
+        run = "plugin diff";
+        desc = "Diff the selected with the hovered file";
+      }
+      ++ lib.optional (lib.hasAttr "chmod" enabledPlugins) {
+        on = [
+          "c"
+          "m"
+        ];
+        run = "plugin chmod";
+        desc = "Chmod on selected files";
+      }
+      ++ lib.optional (lib.hasAttr "toggle-pane" enabledPlugins) {
+        on = [ "T" ];
+        run = "plugin toggle-pane min-preview";
+        desc = "Hide or show preview";
+      }
+      # {
+      #   on = [ "T" ];
+      #   run = "plugin toggle-pane max-preview";
+      #   desc = "Maximize or restore preview";
+      # }
+      ++ lib.optional (lib.hasAttr "jump-to-char" enabledPlugins) {
+        on = [ "f" ];
+        run = "plugin jump-to-char";
+        desc = "Jump to char";
+      }
+      ++ lib.optional (lib.hasAttr "smart-filter" enabledPlugins) {
+        on = [ "F" ];
+        run = "plugin smart-filter";
+        desc = "Smart filter";
+      }
+      ++ lib.optional (lib.hasAttr "ouch" enabledPlugins) {
+        on = [ "C" ];
+        run = "plugin ouch";
+        desc = "Compress with outch";
+      }
+      ++ lib.optional config.${namespace}.suites.wlroots.enable {
+        on = [ "y" ];
+        run = [
+          ''shell -- for path in "$@"; do echo "file://$path"; done | wl-copy -t text/uri-list''
+          "yank"
+        ];
+      }
       ++ goto.prepend_keymap
       ++ navigation.prepend_keymap
       ++ operation.prepend_keymap;
--- modules/home/programs/terminal/tools/yazi/keymap/manager/operation.nix
+++ modules/home/programs/terminal/tools/yazi/keymap/manager/operation.nix
@@ -1,76 +1,81 @@
+{ config, lib, ... }:
+let
+  enabledPlugins = config.programs.yazi.plugins;
+in
 {
-  prepend_keymap = [
-    {
-      on = [
-        "R"
-        "p"
-        "p"
-      ];
-      run = "plugin sudo --args='paste'";
-      desc = "sudo paste";
-    }
-    {
-      on = [
-        "R"
-        "P"
-      ];
-      run = "plugin sudo --args='paste -f'";
-      desc = "sudo paste (force)";
-    }
-    {
-      on = [
-        "R"
-        "r"
-      ];
-      run = "plugin sudo --args='rename'";
-      desc = "sudo rename";
-    }
-    {
-      on = [
-        "R"
-        "p"
-        "l"
-      ];
-      run = "plugin sudo --args='link'";
-      desc = "sudo link (absolute path)";
-    }
-    {
-      on = [
-        "R"
-        "p"
-        "L"
-      ];
-      run = "plugin sudo --args='link -r'";
-      desc = "sudo link (relative path)";
-    }
-    {
-      on = [
-        "R"
-        "a"
-      ];
-      run = "plugin sudo --args='create'";
-      desc = "sudo create (file or directory)";
-    }
-    {
-      on = [
-        "R"
-        "d"
-      ];
-      run = "plugin sudo --args='remove'";
-      desc = "sudo trash";
-    }
-    {
-      on = [
-        "R"
-        "D"
-      ];
-      run = "plugin sudo --args='remove -P'";
-      desc = "sudo delete (permanent)";
-    }
-    {
+  prepend_keymap =
+    lib.optionals (lib.hasAttr "sudo" enabledPlugins) [
+      {
+        on = [
+          "R"
+          "p"
+          "p"
+        ];
+        run = "plugin sudo --args='paste'";
+        desc = "sudo paste";
+      }
+      {
+        on = [
+          "R"
+          "P"
+        ];
+        run = "plugin sudo --args='paste -f'";
+        desc = "sudo paste (force)";
+      }
+      {
+        on = [
+          "R"
+          "r"
+        ];
+        run = "plugin sudo --args='rename'";
+        desc = "sudo rename";
+      }
+      {
+        on = [
+          "R"
+          "p"
+          "l"
+        ];
+        run = "plugin sudo --args='link'";
+        desc = "sudo link (absolute path)";
+      }
+      {
+        on = [
+          "R"
+          "p"
+          "L"
+        ];
+        run = "plugin sudo --args='link -r'";
+        desc = "sudo link (relative path)";
+      }
+      {
+        on = [
+          "R"
+          "a"
+        ];
+        run = "plugin sudo --args='create'";
+        desc = "sudo create (file or directory)";
+      }
+      {
+        on = [
+          "R"
+          "d"
+        ];
+        run = "plugin sudo --args='remove'";
+        desc = "sudo trash";
+      }
+      {
+        on = [
+          "R"
+          "D"
+        ];
+        run = "plugin sudo --args='remove -P'";
+        desc = "sudo delete (permanent)";
+      }
+    ]
+    ++ lib.optional (lib.hasAttr "restore" enabledPlugins) {
       on = "u";
       run = "plugin restore";
       desc = "Restore last deleted files/folders";
-    }
-  ];
+    };
 }
