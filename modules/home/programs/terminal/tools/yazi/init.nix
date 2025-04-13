5dd1ab7a81876249bd6560677a2bb2a68496206f
Austin Horstman <khaneliman12@gmail.com>
2025-04-12 23:47:05 -0500

    yazi: fix linemode
    Accidentally removed during yatline configuration

--- modules/home/programs/terminal/tools/yazi/init.nix
+++ modules/home/programs/terminal/tools/yazi/init.nix
@@ -22,6 +22,20 @@ in
       require("session"):setup({
       	sync_yanked = true,
       })
+
+      function Linemode:custom()
+      	local year = os.date("%Y")
+      	local time = (self._file.cha.mtime or 0) // 1
+
+      	if time > 0 and os.date("%Y", time) == year then
+      		time = os.date("%b %d %H:%M", time)
+      	else
+      		time = time and os.date("%b %d  %Y", time) or ""
+      	end
+
+      	local size = self._file:size()
+      	return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
+      end
     ''
     +
       lib.optionalString (lib.hasAttr "yatline" enabledPlugins) # Lua
@@ -150,19 +164,5 @@ in
           		ui.Span(" "),
           	})
           end, 400, Status.RIGHT)
-
-          function Linemode:custom()
-          	local year = os.date("%Y")
-          	local time = (self._file.cha.mtime or 0) // 1
-
-          	if time > 0 and os.date("%Y", time) == year then
-          		time = os.date("%b %d %H:%M", time)
-          	else
-          		time = time and os.date("%b %d  %Y", time) or ""
-          	end
-
-          	local size = self._file:size()
-          	return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
-          end
         '';
 }
