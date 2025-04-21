ead49c7c764d6a5d72cd9859b0c144fb300c0c11
Austin Horstman <khaneliman12@gmail.com>
2025-04-20 19:15:09 -0500

    khanelinix: network tweak
    Trying to fix mdns

--- systems/x86_64-linux/khanelinix/network.nix
+++ systems/x86_64-linux/khanelinix/network.nix
@@ -14,12 +14,15 @@
       # wired interfaces e.g. ethernet
       "30-network-defaults-wired" = {
         matchConfig.Name = "en* | eth* | usb*";
+        linkConfig = {
+          Multicast = if !config.services.avahi.enable then "yes" else "no";
+        };
         networkConfig = {
           Address = "192.168.1.3/24";
           Gateway = "192.168.1.1";
           # DHCP = "ipv4";
           # Enable if `mdns` is not handled by avahi
-          MulticastDNS = !config.services.avahi.enable;
+          MulticastDNS = if !config.services.avahi.enable then "yes" else "no";
           # IPv6AcceptRA = true;
           # IPForward = "yes";
           # IPMasquerade = "no";
