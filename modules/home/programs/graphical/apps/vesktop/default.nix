145386e61f3a4fc7227a7adf64e141f957a53b39
Austin Horstman <khaneliman12@gmail.com>
2025-04-23 10:09:15 -0500

    vesktop: expand vencord configuration

--- modules/home/programs/graphical/apps/vesktop/default.nix
+++ modules/home/programs/graphical/apps/vesktop/default.nix
@@ -18,13 +18,102 @@ in
 
       settings = {
         discordBranch = "stable";
-        minimizeToTray = true;
+        # Easier to close and be done with it
+        minimizeToTray = false;
         arRPC = true;
         customTitleBar = false;
       };
 
       vencord = {
-        settings = { };
+        settings = {
+          # Can't auto update on nix
+          autoUpdate = false;
+          autoUpdateNotification = false;
+
+          useQuickCss = true;
+          themeLinks = [ ];
+          eagerPatches = false;
+          enableReactDevtools = true;
+          frameless = false;
+          transparent = true;
+          winCtrlQ = false;
+          disableMinSize = true;
+          winNativeTitleBar = false;
+          plugins = {
+            CommandsAPI = {
+              enabled = true;
+            };
+            MessageAccessoriesAPI = {
+              enabled = true;
+            };
+            UserSettingsAPI = {
+              enabled = true;
+            };
+            AlwaysAnimate = {
+              enabled = true;
+            };
+            AlwaysExpandRoles = {
+              enabled = true;
+            };
+            AlwaysTrust = {
+              enabled = true;
+            };
+            BetterSessions = {
+              enabled = true;
+            };
+            CrashHandler = {
+              enabled = true;
+            };
+            FixImagesQuality = {
+              enabled = true;
+            };
+            PlatformIndicators = {
+              enabled = true;
+            };
+            ReplyTimestamp = {
+              enabled = true;
+            };
+            ShowHiddenChannels = {
+              enabled = true;
+            };
+            ShowHiddenThings = {
+              enabled = true;
+            };
+            VencordToolbox = {
+              enabled = true;
+            };
+            WebKeybinds = {
+              enabled = true;
+            };
+            WebScreenShareFixes = {
+              enabled = true;
+            };
+            # Lag inducing on large servers
+            # WhoReacted= {
+            #     enabled= false
+            # };
+            YoutubeAdblock = {
+              enabled = true;
+            };
+            BadgeAPI = {
+              enabled = true;
+            };
+            NoTrack = {
+              enabled = true;
+              disableAnalytics = true;
+            };
+            Settings = {
+              enabled = true;
+              settingsLocation = "aboveNitro";
+            };
+          };
+          notifications = {
+            timeout = 5000;
+            position = "bottom-right";
+            useNative = "not-focused";
+            logLimit = 50;
+          };
+        };
       };
     };
   };
