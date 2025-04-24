34dc8fc8c87cc897a3f0ed3dd52b00dccea94582
Austin Horstman <khaneliman12@gmail.com>
2025-04-23 20:16:10 -0500

    ghostty: set priorities for theme support

--- modules/home/programs/terminal/emulators/ghostty/default.nix
+++ modules/home/programs/terminal/emulators/ghostty/default.nix
@@ -28,40 +28,47 @@ in
       enableFishIntegration = true;
       enableZshIntegration = true;
 
-      settings = {
-        adw-toolbar-style = "flat";
+      settings =
+        let
+          monaspaceKrypton =
+            if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Krypton Var" else "MonaspaceKrypton";
+          monaspaceNeon = if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Neon Var" else "MonaspaceNeon";
+          monaspaceRadon =
+            if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Radon Var" else "MonaspaceRadon";
+          monaspaceXenon =
+            if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Xenon Var" else "MonaspaceXenon";
+        in
+        {
+          adw-toolbar-style = "flat";
 
-        background-opacity = 0.8;
+          background-opacity = lib.mkDefault 0.8;
 
-        clipboard-trim-trailing-spaces = true;
-        copy-on-select = "clipboard";
+          clipboard-trim-trailing-spaces = true;
+          copy-on-select = "clipboard";
 
-        focus-follows-mouse = true;
+          focus-follows-mouse = true;
 
-        font-size = 13;
-        font-family = if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Neon Var" else "MonaspaceNeon";
-        font-family-bold =
-          if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Xenon Var" else "MonaspaceXenon";
-        font-family-italic =
-          if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Radon Var" else "MonaspaceRadon";
-        font-family-bold-italic =
-          if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Krypton Var" else "MonaspaceKrypton";
-        font-feature = "+ss01,+ss02,+ss03,+ss04,+ss05,+ss06,+ss07,+ss08,+ss09,+ss10,+liga,+dlig,+calt";
+          font-size = lib.mkDefault 13;
+          font-family = lib.mkForce monaspaceNeon;
+          font-family-bold = lib.mkForce monaspaceXenon;
+          font-family-italic = lib.mkForce monaspaceRadon;
+          font-family-bold-italic = lib.mkForce monaspaceKrypton;
+          font-feature = "+ss01,+ss02,+ss03,+ss04,+ss05,+ss06,+ss07,+ss08,+ss09,+ss10,+liga,+dlig,+calt";
 
-        # NOTE: Different methods of using cgroups for every surface.
-        # linux-cgroup = "always";
-        gtk-single-instance = false;
+          # NOTE: Different methods of using cgroups for every surface.
+          # linux-cgroup = "always";
+          gtk-single-instance = false;
 
-        # Breaks tab functionality, but tab functionality is broken with yabai
-        macos-titlebar-style = "hidden";
-        macos-option-as-alt = true;
+          # Breaks tab functionality, but tab functionality is broken with yabai
+          macos-titlebar-style = "hidden";
+          macos-option-as-alt = true;
 
-        quit-after-last-window-closed = true;
+          quit-after-last-window-closed = true;
 
-        # Disables some functionality available through window
-        # But, doesn't fit theme of a clean WM with its GTK interface
-        window-decoration = lib.mkIf pkgs.stdenv.hostPlatform.isLinux false;
-      };
+          # Disables some functionality available through window
+          # But, doesn't fit theme of a clean WM with its GTK interface
+          window-decoration = lib.mkIf pkgs.stdenv.hostPlatform.isLinux false;
+        };
     };
   };
 }
