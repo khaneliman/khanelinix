b9baa92f0a2e7f567e149c7e8cadc3c62b0cc8d3
Austin Horstman <khaneliman12@gmail.com>
2025-04-24 13:52:55 -0500

    stylix: prefer catppuccin module
    Custom themes tailored should actually have preference over the generic
    stylix implementation.

--- modules/home/theme/stylix/default.nix
+++ modules/home/theme/stylix/default.nix
@@ -46,7 +46,7 @@ in
       # autoEnable = false;
       base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
 
-      inherit (cfg) cursor;
+      cursor = lib.mkIf (!config.catppuccin.enable) cfg.cursor;
 
       fonts = {
         sizes = {
@@ -74,7 +74,7 @@ in
         };
       };
 
-      iconTheme = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+      iconTheme = lib.mkIf (pkgs.stdenv.hostPlatform.isLinux && (!config.catppuccin.enable)) {
         enable = true;
         inherit (cfg.icon) package;
         dark = cfg.icon.name;
@@ -90,10 +90,46 @@ in
       };
 
       targets = {
+        # Set profile names for firefox
         firefox.profileNames = [ config.${namespace}.user.name ];
+
         # FIXME:: upstream needs module fix
         hyprlock.useWallpaper = false;
         hyprlock.enable = false;
+
+        # TODO: Very custom styling, integrate with their variables
+        # Currently setup only for catppuccin/nix
+        waybar.enable = false;
+
+        # FIXME: not working
+        gtk.enable = false;
+
+        # Disable targets when catppuccin is enabled
+        alacritty.enable = !config.catppuccin.enable;
+        bat.enable = !config.catppuccin.enable;
+        btop.enable = !config.catppuccin.enable;
+        cava.enable = !config.catppuccin.enable;
+        fish.enable = !config.catppuccin.enable;
+        foot.enable = !config.catppuccin.enable;
+        fzf.enable = !config.catppuccin.enable;
+        ghostty.enable = !config.catppuccin.enable;
+        gitui.enable = !config.catppuccin.enable;
+        gnome.enable = !config.catppuccin.enable;
+        helix.enable = !config.catppuccin.enable;
+        hyprland.enable = !config.catppuccin.enable;
+        k9s.enable = !config.catppuccin.enable;
+        kitty.enable = !config.catppuccin.enable;
+        lazygit.enable = !config.catppuccin.enable;
+        ncspot.enable = !config.catppuccin.enable;
+        neovim.enable = !config.catppuccin.enable;
+        qt.enable = !config.catppuccin.enable;
+        sway.enable = !config.catppuccin.enable;
+        # swaync.enable = !config.catppuccin.enable;
+        tmux.enable = !config.catppuccin.enable;
+        vesktop.enable = !config.catppuccin.enable;
+        yazi.enable = !config.catppuccin.enable;
+        zathura.enable = !config.catppuccin.enable;
+        zellij.enable = !config.catppuccin.enable;
       };
     };
   };
