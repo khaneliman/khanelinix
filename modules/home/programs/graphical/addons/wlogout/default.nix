7a7f9e73d6cf12644828f12508313ef9c2d4c79a
Austin Horstman <khaneliman12@gmail.com>
2025-04-24 11:44:04 -0500

    wlogout: internal style default prio
    Easier to override

--- modules/home/programs/graphical/addons/wlogout/default.nix
+++ modules/home/programs/graphical/addons/wlogout/default.nix
@@ -58,81 +58,82 @@ in
           keybind = "r";
         }
       ];
-      style = # css
-        ''
-          * {
-            background-image: none;
-            font-family: MonaspaceNeon;
-            font-size: 20px;
-          }
-          window {
-            background-color: rgba(36, 39, 58, 0.9);
-          }
-          button {
-            color: #cad3f5;
-            background-color: #1e2030;
-            border-style: solid;
-            border-width: 2px;
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: 15%;
-          }
+      style =
+        lib.mkDefault # css
+          ''
+            * {
+              background-image: none;
+              font-family: MonaspaceNeon;
+              font-size: 20px;
+            }
+            window {
+              background-color: rgba(36, 39, 58, 0.9);
+            }
+            button {
+              color: #cad3f5;
+              background-color: #1e2030;
+              border-style: solid;
+              border-width: 2px;
+              background-repeat: no-repeat;
+              background-position: center;
+              background-size: 15%;
+            }
 
-          button:focus,
-          button:active,
-          button:hover {
-            background-color: #181926;
-            outline-style: none;
-          }
+            button:focus,
+            button:active,
+            button:hover {
+              background-color: #181926;
+              outline-style: none;
+            }
 
-          #lock {
-            background-image: image(
-              url("${config.programs.wlogout.package}/share/wlogout/icons/lock.png"),
-              url("/usr/local/share/wlogout/icons/lock.png")
-            );
-            color: #8aadf4;
-          }
+            #lock {
+              background-image: image(
+                url("${config.programs.wlogout.package}/share/wlogout/icons/lock.png"),
+                url("/usr/local/share/wlogout/icons/lock.png")
+              );
+              color: #8aadf4;
+            }
 
-          #logout {
-            background-image: image(
-              url("${config.programs.wlogout.package}/share/wlogout/icons/logout.png"),
-              url("/usr/local/share/wlogout/icons/logout.png")
-            );
-            color: #8aadf4;
-          }
+            #logout {
+              background-image: image(
+                url("${config.programs.wlogout.package}/share/wlogout/icons/logout.png"),
+                url("/usr/local/share/wlogout/icons/logout.png")
+              );
+              color: #8aadf4;
+            }
 
-          #suspend {
-            background-image: image(
-              url("/${config.programs.wlogout.package}/share/wlogout/icons/suspend.png"),
-              url("/usr/local/share/wlogout/icons/suspend.png")
-            );
-            color: #8aadf4;
-          }
+            #suspend {
+              background-image: image(
+                url("/${config.programs.wlogout.package}/share/wlogout/icons/suspend.png"),
+                url("/usr/local/share/wlogout/icons/suspend.png")
+              );
+              color: #8aadf4;
+            }
 
-          #hibernate {
-            background-image: image(
-              url("/${config.programs.wlogout.package}/share/wlogout/icons/hibernate.png"),
-              url("/usr/local/share/wlogout/icons/hibernate.png")
-            );
-            color: #8aadf4;
-          }
+            #hibernate {
+              background-image: image(
+                url("/${config.programs.wlogout.package}/share/wlogout/icons/hibernate.png"),
+                url("/usr/local/share/wlogout/icons/hibernate.png")
+              );
+              color: #8aadf4;
+            }
 
-          #shutdown {
-            background-image: image(
-              url("/${config.programs.wlogout.package}/share/wlogout/icons/shutdown.png"),
-              url("/usr/local/share/wlogout/icons/shutdown.png")
-            );
-            color: #8aadf4;
-          }
+            #shutdown {
+              background-image: image(
+                url("/${config.programs.wlogout.package}/share/wlogout/icons/shutdown.png"),
+                url("/usr/local/share/wlogout/icons/shutdown.png")
+              );
+              color: #8aadf4;
+            }
 
-          #reboot {
-            background-image: image(
-              url("/${config.programs.wlogout.package}/share/wlogout/icons/reboot.png"),
-              url("/usr/local/share/wlogout/icons/reboot.png")
-            );
-            color: #8aadf4;
-          }
-        '';
+            #reboot {
+              background-image: image(
+                url("/${config.programs.wlogout.package}/share/wlogout/icons/reboot.png"),
+                url("/usr/local/share/wlogout/icons/reboot.png")
+              );
+              color: #8aadf4;
+            }
+          '';
     };
   };
 }
