1a8e4c5088e9121e6aa6a8ea0799cd23e39c0e5b
Austin Horstman <khaneliman12@gmail.com>
2025-04-28 15:27:45 -0500

    shells/migrator: init

---
+++ shells/migrator/default.nix
@@ -0,0 +1,35 @@
+{
+  mkShell,
+  pkgs,
+  lib,
+  ...
+}:
+mkShell {
+  packages =
+    with pkgs;
+    [
+      nodejs_22
+      typescript-language-server
+      typescript
+      claude-code
+      csharpier
+      (
+        with dotnetCorePackages;
+        combinePackages [
+          dotnet-aspnetcore_8
+          dotnet-runtime_8
+          dotnet-sdk_8
+        ]
+      )
+    ]
+    ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+      react-native-debugger
+    ];
+
+  shellHook = ''
+
+    echo 🔨 Migrator DevShell
+
+
+  '';
+}
