2734f087c21004651966eed24a4eb5700baf4d27
Austin Horstman <khaneliman12@gmail.com>
2025-04-28 11:35:57 -0500

    rmpc: add default config

--- modules/home/programs/terminal/media/rmpc/default.nix
+++ modules/home/programs/terminal/media/rmpc/default.nix
@@ -17,6 +17,25 @@ in
   config = mkIf cfg.enable {
     programs.rmpc = {
       enable = true;
+
+      config = ''
+        (
+            address: "127.0.0.1:6600",
+            password: None,
+            theme: None,
+            cache_dir: None,
+            on_song_change: None,
+            volume_step: 5,
+            max_fps: 30,
+            scrolloff: 0,
+            wrap_navigation: false,
+            enable_mouse: true,
+            enable_config_hot_reload: true,
+            status_update_interval_ms: 1000,
+            select_current_song_on_change: false,
+            browser_song_sort: [Disc, Track, Artist, Title],
+        )
+      '';
     };
   };
 }
