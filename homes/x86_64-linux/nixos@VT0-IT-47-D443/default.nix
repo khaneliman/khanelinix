013d31c0cd8f14685935a3fe71527322a8ce66ef
Austin Horstman <khaneliman12@gmail.com>
2025-04-30 15:13:42 -0500

    homes/nixos@VT0-IT-47-D443: init
    work VM

---
+++ homes/x86_64-linux/nixos@VT0-IT-47-D443/default.nix
@@ -0,0 +1,84 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkForce;
+  inherit (lib.${namespace}) enabled;
+in
+{
+  khanelinix = {
+    user = {
+      enable = true;
+      inherit (config.snowfallorg.user) name;
+    };
+
+    programs = {
+      graphical = {
+        apps = {
+        };
+      };
+      terminal = {
+        editors = {
+          neovim = {
+            enable = true;
+            extraModules = [
+              {
+                config = {
+                  plugins = {
+                    # NOTE: Disabling some plugins I won't need on work devices
+                    avante.enable = mkForce false;
+                    windsurf-nvim.enable = mkForce false;
+                    firenvim.enable = mkForce false;
+                    neorg.enable = mkForce false;
+                  };
+                };
+              }
+            ];
+          };
+        };
+
+        tools = {
+          git = {
+            enable = true;
+            wslAgentBridge = true;
+            wslGitCredentialManagerPath = ''/mnt/c/Users/au09163/AppData/Local/Programs/Git/mingw64/bin/git-credential-manager.exe'';
+            includes = [
+              {
+                condition = "gitdir:/mnt/c/";
+                path = "${./git/windows-compat-config}";
+              }
+            ];
+          };
+
+          ssh = enabled;
+        };
+      };
+    };
+
+    services = {
+      sops = {
+        # enable = true;
+        defaultSopsFile = lib.snowfall.fs.get-file "secrets/CORE/nixos/default.yaml";
+        sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+      };
+    };
+
+    system = {
+      xdg = enabled;
+    };
+
+    suites = {
+      common = enabled;
+      development = {
+        enable = true;
+      };
+    };
+
+    theme.catppuccin = enabled;
+  };
+
+  home.stateVersion = "25.05";
+}
