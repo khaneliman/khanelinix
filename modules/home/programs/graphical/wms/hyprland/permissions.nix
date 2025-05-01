8ba42e8013c1c12f2daaa961c87cb0887daff263
Austin Horstman <khaneliman12@gmail.com>
2025-05-01 14:41:57 -0500

    hyprland: permissions disable version check
    Version isn't updated properly upstream...

--- modules/home/programs/graphical/wms/hyprland/permissions.nix
+++ modules/home/programs/graphical/wms/hyprland/permissions.nix
@@ -13,16 +13,14 @@ in
   config = mkIf cfg.enable {
     wayland.windowManager.hyprland = {
       settings = {
-        permission =
-          lib.mkIf
-            (lib.versionAtLeast (lib.getVersion config.wayland.windowManager.hyprland.package) "0.48.2")
-            [
-              "${lib.getExe pkgs.grim}, screencopy, allow"
-              "${lib.getExe pkgs.hyprpicker}, screencopy, allow"
-              "${lib.getExe pkgs.${namespace}.record_screen}, screencopy, allow"
-              "${lib.getExe config.wayland.windowManager.hyprland.portalPackage}, screencopy, allow"
-              "${lib.getExe config.programs.hyprlock.package}, screencopy, allow"
-            ];
+        permission = [
+          "${lib.getExe pkgs.grimblast}, screencopy, allow"
+          "${lib.getExe pkgs.grim}, screencopy, allow"
+          "${lib.getExe pkgs.hyprpicker}, screencopy, allow"
+          "${lib.getExe pkgs.${namespace}.record_screen}, screencopy, allow"
+          "${lib.getExe config.wayland.windowManager.hyprland.portalPackage}, screencopy, allow"
+          "${lib.getExe config.programs.hyprlock.package}, screencopy, allow"
+        ];
       };
     };
   };
