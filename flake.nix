e1c97cf16e4cea4324fc37eb07b1469dafbb58d9
Austin Horstman <khaneliman12@gmail.com>
2025-05-17 10:10:35 -0500

    flake.lock: update

--- flake.nix
+++ flake.nix
@@ -4,8 +4,7 @@
   inputs = {
     # Core inputs
     darwin = {
-      # url = "github:lnl7/nix-darwin";
-      url = "github:khaneliman/nix-darwin/darwin-rewrite";
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     home-manager = {
