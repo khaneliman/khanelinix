498280775c257fdfabcfc02ceefc3349ecd88d89
Austin Horstman <khaneliman12@gmail.com>
2025-05-22 12:48:50 -0500

    overlays/nixpkgs-master: add claude-code for v1

--- overlays/nixpkgs-master/default.nix
+++ overlays/nixpkgs-master/default.nix
@@ -1,6 +1,9 @@
 { channels, ... }:
 _final: _prev: {
   inherit (channels.nixpkgs-master)
+    # TODO: remove after 1.0 hits os-unstable
+    claude-code
+    # TODO: remove after package hits pkgs-unstable
     sbarlua
     yaziPlugins
     ;
