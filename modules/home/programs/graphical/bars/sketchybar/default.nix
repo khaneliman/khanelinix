dd7b058083d105773ed1d95e9b5d5f7d80c3cde0
Austin Horstman <khaneliman12@gmail.com>
2025-05-24 20:00:31 -0500

    home/bars/sketchybar: use new home manager module

--- modules/home/programs/graphical/bars/sketchybar/default.nix
+++ modules/home/programs/graphical/bars/sketchybar/default.nix
@@ -13,6 +13,7 @@ let
   shellAliases = with pkgs; {
     push = # bash
       ''command git push && ${getExe sketchybar} --trigger git_push'';
+    restart-sketchybar = ''launchctl kickstart -k gui/"$(id -u)"/org.nix-community.home.sketchybar'';
   };
 in
 {
@@ -23,44 +24,48 @@ in
   config = mkIf cfg.enable {
     home.shellAliases = shellAliases;
 
-    programs.zsh.initContent = # bash
-      ''
-        brew() {
-          command brew "$@" && ${getExe pkgs.sketchybar} --trigger brew_update
-        }
+    programs = {
+      sketchybar = {
+        enable = true;
+        configType = "lua";
 
-        mas() {
-          command mas "$@" && ${getExe pkgs.sketchybar} --trigger brew_update
-        }
-      '';
+        sbarLuaPackage = pkgs.sbarlua;
+
+        extraPackages = with pkgs; [
+          blueutil
+          coreutils
+          curl
+          gh
+          gh-notify
+          gnugrep
+          gnused
+          jankyborders
+          jq
+          pkgs.${namespace}.dynamic-island-helper
+          pkgs.${namespace}.sketchyhelper
+          wttrbar
+          yabai
+        ];
+
+        config = {
+          source = ./config;
+          recursive = true;
+        };
+      };
+
+      zsh.initContent = # bash
+        ''
+          brew() {
+            command brew "$@" && ${getExe pkgs.sketchybar} --trigger brew_update
+          }
+
+          mas() {
+            command mas "$@" && ${getExe pkgs.sketchybar} --trigger brew_update
+          }
+        '';
+    };
 
     xdg.configFile = {
-      "sketchybar" = {
-        source = lib.cleanSourceWith { src = lib.cleanSource ./config/.; };
-
-        recursive = true;
-      };
-
-      "sketchybar/sketchybarrc" = {
-        executable = true;
-        text = # Lua
-          ''
-            #!/usr/bin/env lua
-
-            -- Add the sketchybar module to the package cpath (the module could be
-            -- installed into the default search path then this would not be needed)
-            package.cpath = package.cpath .. ";${pkgs.lua54Packages.getLuaCPath pkgs.sbarlua}"
-
-            Sbar = require("sketchybar")
-            Sbar.exec("killall sketchyhelper || sketchyhelper git.felix.sketchyhelper >/dev/null 2>&1 &")
-            Sbar.begin_config()
-            require("init")
-            Sbar.hotload(true)
-            Sbar.end_config()
-            Sbar.event_loop()
-          '';
-      };
-
       "dynamic-island-sketchybar" = {
         source = lib.cleanSourceWith { src = lib.cleanSource ./dynamic-island-sketchybar/.; };
 
