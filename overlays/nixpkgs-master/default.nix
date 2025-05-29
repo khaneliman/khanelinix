5e78ee27c69fb5299850b1fb4a5ff7c3b8d016a2
Austin Horstman <khaneliman12@gmail.com>
2025-05-28 22:53:42 -0500

    overlays/nixpkgs-master: remove claude-code and sbarlua

--- overlays/nixpkgs-master/default.nix
+++ overlays/nixpkgs-master/default.nix
@@ -1,10 +1,6 @@
 { channels, ... }:
 _final: _prev: {
   inherit (channels.nixpkgs-master)
-    # TODO: remove after 1.0 hits os-unstable
-    claude-code
-    # TODO: remove after package hits pkgs-unstable
-    sbarlua
     yaziPlugins
     ;
 }
