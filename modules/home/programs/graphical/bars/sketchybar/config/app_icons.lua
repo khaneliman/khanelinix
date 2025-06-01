1af5a149b8ac2fc7f2797dbba5c5f2fdb514a35d
Austin Horstman <khaneliman12@gmail.com>
2025-05-31 19:20:49 -0500

    sketchybar: simplify app_icons with metatable
    Define lookup table with fallback to icon_map

--- modules/home/programs/graphical/bars/sketchybar/config/app_icons.lua
+++ modules/home/programs/graphical/bars/sketchybar/config/app_icons.lua
@@ -2,16 +2,11 @@
 
 local app_icons = require("icon_map")
 
--- Extend with custom mappings and aliases
-local extensions = {
+-- Return the base icons extended with custom mappings and aliases
+return setmetatable({
 	-- Element from nixpkgs doesn't have name set properly on macos
 	["Electron"] = ":element:",
 	["Vesktop"] = ":discord:",
-}
-
--- Apply extensions
-for app, icon in pairs(extensions) do
-	app_icons[app] = icon
-end
-
-return app_icons
+}, {
+	__index = app_icons,
+})
