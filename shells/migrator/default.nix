1015fef00f2fe011fb9fe684096e727d7713abcb
Austin Horstman <khaneliman12@gmail.com>
2025-06-05 22:00:27 -0500

    shells/migrator: inline csharp-ls override

--- shells/migrator/default.nix
+++ shells/migrator/default.nix
@@ -15,7 +15,13 @@ mkShell {
       typescript
       claude-code
       csharpier
-      csharp-ls
+      (csharp-ls.overrideAttrs (_oldAttrs: {
+        # NOTE: csharp-ls requires a very new dotnet 8 sdk. This causes issues with workspace dotnet
+        # collisions because dotnet commands will run off the newest SDK breaking working with lower
+        # version projects.
+        useDotnetFromEnv = false;
+        meta.badPlatforms = [ ];
+      }))
       (
         with dotnetCorePackages;
         combinePackages [
