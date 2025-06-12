b3e0f8e45ab4272cd88aeb14149ab8f6fa310cde
Austin Horstman <khaneliman12@gmail.com>
2025-06-11 21:06:29 -0500

    overlays/csharp-ls: remove

--- overlays/csharp-ls/default.nix
+++
@@ -1,9 +0,0 @@
-_: _final: prev: {
-  csharp-ls = prev.csharp-ls.overrideAttrs (_oldAttrs: {
-    # NOTE: csharp-ls requires a very new dotnet 8 sdk. This causes issues with workspace dotnet
-    # collisions because dotnet commands will run off the newest SDK breaking working with lower
-    # version projects.
-    useDotnetFromEnv = false;
-    meta.badPlatforms = [ ];
-  });
-}
