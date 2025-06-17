73c204f44db14f7537fb3f5bd54e308b20a58613
Austin Horstman <khaneliman12@gmail.com>
2025-06-17 17:23:56 -0500

    overlays/firefox-devedition-bin: remove unused overlay

--- overlays/firefox-devedition-bin/default.nix
+++
@@ -1,6 +0,0 @@
-_: _final: prev: {
-  # FIXME: Upstream broken after https://github.com/NixOS/nixpkgs/pull/410404
-  firefox-devedition-bin-unwrapped = prev.firefox-devedition-bin-unwrapped.overrideAttrs (_oldAttrs: {
-    dontFixup = false;
-  });
-}
