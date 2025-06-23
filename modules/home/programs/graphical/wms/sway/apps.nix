4d5ecd3cce9db329a09d5ceb048c66b5633dd677
Austin Horstman <khaneliman12@gmail.com>
2025-06-22 23:36:16 -0500

    sway: sync up startup apps with hyprland preferences

--- modules/home/programs/graphical/wms/sway/apps.nix
+++ modules/home/programs/graphical/wms/sway/apps.nix
@@ -15,16 +15,28 @@ in
     wayland.windowManager.sway = {
       config = {
         startup = [
+          # 1/2
           { command = getExe config.programs.firefox.package; }
+
+          # 4
           { command = getExe pkgs.steam; }
-          { command = getExe pkgs.discord; }
+
+          # 5
+          { command = getExe config.programs.vesktop.package; }
+          { command = getExe pkgs.element-desktop; }
+          { command = getExe pkgs.teams-for-linux; }
+
+          # 6
           { command = getExe pkgs.thunderbird; }
 
           # Startup background apps
-          { command = "${getExe pkgs.openrgb-with-all-plugins} --startminimized --profile default"; }
+          { command = "${getExe pkgs.openrgb-with-all-plugins} -c blue"; }
           { command = "${getExe pkgs._1password-gui} --silent"; }
           { command = getExe pkgs.tailscale-systray; }
-          { command = "run-as-service $(${getExe pkgs.wayvnc} $(${getExe pkgs.tailscale} ip --4))"; }
+          { command = getExe pkgs.networkmanagerapplet; }
+          { command = "${getExe pkgs.wl-clip-persist} --clipboard both"; }
+
+          { command = "$(${getExe pkgs.wayvnc} $(${getExe pkgs.tailscale} ip --4))"; }
 
           { command = "${lib.getExe pkgs.libnotify} --icon ~/.face -u normal \"Hello $(whoami)\""; }
         ];
