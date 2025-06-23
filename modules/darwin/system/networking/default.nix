11403f594d15d8e14b13666acb7b608fec54455f
Austin Horstman <khaneliman12@gmail.com>
2025-06-22 23:09:31 -0500

    flake.lock: update

--- modules/darwin/system/networking/default.nix
+++ modules/darwin/system/networking/default.nix
@@ -23,15 +23,5 @@ in
         "2606:4700:4700::1001"
       ];
     };
-
-    system.defaults = {
-      # firewall settings
-      alf = {
-        # 0 = disabled 1 = enabled 2 = blocks all connections except for essential services
-        globalstate = 1;
-        loggingenabled = 0;
-        stealthenabled = 0;
-      };
-    };
   };
 }
