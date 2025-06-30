f73880ad26a790ae3b0c6b7119460297784b5dec
Austin Horstman <khaneliman12@gmail.com>
2025-06-29 21:56:06 -0500

    home/theme/stylix: only evaluate when option exists

--- modules/home/theme/stylix/default.nix
+++ modules/home/theme/stylix/default.nix
@@ -3,6 +3,7 @@
   lib,
   namespace,
   pkgs,
+  options,
   ...
 }:
 let
@@ -41,105 +42,107 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    stylix = {
-      enable = true;
-      # autoEnable = false;
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/${cfg.theme}.yaml";
+  config = mkIf cfg.enable (
+    lib.optionalAttrs (options ? stylix) {
+      stylix = {
+        enable = true;
+        # autoEnable = false;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/${cfg.theme}.yaml";
 
-      cursor = if (!config.${namespace}.theme.catppuccin.enable) then cfg.cursor else null;
+        cursor = if (!config.${namespace}.theme.catppuccin.enable) then cfg.cursor else null;
 
-      fonts = {
-        sizes = {
-          desktop = 11;
-          applications = 12;
-          terminal = 13;
-          popups = 12;
-        };
-
-        serif = {
-          package = pkgs.monaspace;
-          name = if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Neon" else "MonaspaceNeon";
-        };
-        sansSerif = {
-          package = pkgs.monaspace;
-          name = if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Neon" else "MonaspaceNeon";
-        };
-        monospace = {
-          package = pkgs.monaspace;
-          name = if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Krypton" else "MonaspaceKrypton";
-        };
-        emoji = {
-          package = pkgs.noto-fonts-color-emoji;
-          name = "Noto Color Emoji";
-        };
-      };
-
-      iconTheme =
-        lib.mkIf (pkgs.stdenv.hostPlatform.isLinux && (!config.${namespace}.theme.catppuccin.enable))
-          {
-            enable = true;
-            inherit (cfg.icon) package;
-            dark = cfg.icon.name;
-            # TODO: support custom light
-            light = cfg.icon.name;
+        fonts = {
+          sizes = {
+            desktop = 11;
+            applications = 12;
+            terminal = 13;
+            popups = 12;
           };
 
-      polarity = "dark";
-
-      opacity = {
-        desktop = 1.0;
-        applications = 0.90;
-        terminal = 0.90;
-        popups = 1.0;
-      };
-
-      targets =
-        {
-          # Set profile names for firefox
-          firefox.profileNames = [ config.${namespace}.user.name ];
-
-          # TODO: Very custom styling, integrate with their variables
-          # Currently setup only for catppuccin/nix
-          vscode.enable = false;
-
-          # Disable targets when catppuccin is enabled
-          alacritty.enable = !config.${namespace}.theme.catppuccin.enable;
-          bat.enable = !config.${namespace}.theme.catppuccin.enable;
-          btop.enable = !config.${namespace}.theme.catppuccin.enable;
-          cava.enable = !config.${namespace}.theme.catppuccin.enable;
-          fish.enable = !config.${namespace}.theme.catppuccin.enable;
-          foot.enable = !config.${namespace}.theme.catppuccin.enable;
-          fzf.enable = !config.${namespace}.theme.catppuccin.enable;
-          ghostty.enable = !config.${namespace}.theme.catppuccin.enable;
-          gitui.enable = !config.${namespace}.theme.catppuccin.enable;
-          helix.enable = !config.${namespace}.theme.catppuccin.enable;
-          k9s.enable = !config.${namespace}.theme.catppuccin.enable;
-          kitty.enable = !config.${namespace}.theme.catppuccin.enable;
-          lazygit.enable = !config.${namespace}.theme.catppuccin.enable;
-          ncspot.enable = !config.${namespace}.theme.catppuccin.enable;
-          neovim.enable = !config.${namespace}.theme.catppuccin.enable;
-          tmux.enable = !config.${namespace}.theme.catppuccin.enable;
-          vesktop.enable = !config.${namespace}.theme.catppuccin.enable;
-          yazi.enable = !config.${namespace}.theme.catppuccin.enable;
-          zathura.enable = !config.${namespace}.theme.catppuccin.enable;
-          zellij.enable = !config.${namespace}.theme.catppuccin.enable;
-        }
-        // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-          gnome.enable = !config.${namespace}.theme.catppuccin.enable;
-          # FIXME: not working
-          gtk.enable = false;
-          hyprland.enable = !config.${namespace}.theme.catppuccin.enable;
-          # FIXME:: upstream needs module fix
-          hyprlock.useWallpaper = false;
-          hyprlock.enable = false;
-          qt.enable = !config.${namespace}.theme.catppuccin.enable;
-          sway.enable = !config.${namespace}.theme.catppuccin.enable;
-          # TODO: Very custom styling, integrate with their variables
-          # Currently setup only for catppuccin/nix
-          swaync.enable = false;
-          waybar.enable = !config.${namespace}.theme.catppuccin.enable;
+          serif = {
+            package = pkgs.monaspace;
+            name = if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Neon" else "MonaspaceNeon";
+          };
+          sansSerif = {
+            package = pkgs.monaspace;
+            name = if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Neon" else "MonaspaceNeon";
+          };
+          monospace = {
+            package = pkgs.monaspace;
+            name = if pkgs.stdenv.hostPlatform.isDarwin then "Monaspace Krypton" else "MonaspaceKrypton";
+          };
+          emoji = {
+            package = pkgs.noto-fonts-color-emoji;
+            name = "Noto Color Emoji";
+          };
         };
-    };
-  };
+
+        iconTheme =
+          lib.mkIf (pkgs.stdenv.hostPlatform.isLinux && (!config.${namespace}.theme.catppuccin.enable))
+            {
+              enable = true;
+              inherit (cfg.icon) package;
+              dark = cfg.icon.name;
+              # TODO: support custom light
+              light = cfg.icon.name;
+            };
+
+        polarity = "dark";
+
+        opacity = {
+          desktop = 1.0;
+          applications = 0.90;
+          terminal = 0.90;
+          popups = 1.0;
+        };
+
+        targets =
+          {
+            # Set profile names for firefox
+            firefox.profileNames = [ config.${namespace}.user.name ];
+
+            # TODO: Very custom styling, integrate with their variables
+            # Currently setup only for catppuccin/nix
+            vscode.enable = false;
+
+            # Disable targets when catppuccin is enabled
+            alacritty.enable = !config.${namespace}.theme.catppuccin.enable;
+            bat.enable = !config.${namespace}.theme.catppuccin.enable;
+            btop.enable = !config.${namespace}.theme.catppuccin.enable;
+            cava.enable = !config.${namespace}.theme.catppuccin.enable;
+            fish.enable = !config.${namespace}.theme.catppuccin.enable;
+            foot.enable = !config.${namespace}.theme.catppuccin.enable;
+            fzf.enable = !config.${namespace}.theme.catppuccin.enable;
+            ghostty.enable = !config.${namespace}.theme.catppuccin.enable;
+            gitui.enable = !config.${namespace}.theme.catppuccin.enable;
+            helix.enable = !config.${namespace}.theme.catppuccin.enable;
+            k9s.enable = !config.${namespace}.theme.catppuccin.enable;
+            kitty.enable = !config.${namespace}.theme.catppuccin.enable;
+            lazygit.enable = !config.${namespace}.theme.catppuccin.enable;
+            ncspot.enable = !config.${namespace}.theme.catppuccin.enable;
+            neovim.enable = !config.${namespace}.theme.catppuccin.enable;
+            tmux.enable = !config.${namespace}.theme.catppuccin.enable;
+            vesktop.enable = !config.${namespace}.theme.catppuccin.enable;
+            yazi.enable = !config.${namespace}.theme.catppuccin.enable;
+            zathura.enable = !config.${namespace}.theme.catppuccin.enable;
+            zellij.enable = !config.${namespace}.theme.catppuccin.enable;
+          }
+          // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+            gnome.enable = !config.${namespace}.theme.catppuccin.enable;
+            # FIXME: not working
+            gtk.enable = false;
+            hyprland.enable = !config.${namespace}.theme.catppuccin.enable;
+            # FIXME:: upstream needs module fix
+            hyprlock.useWallpaper = false;
+            hyprlock.enable = false;
+            qt.enable = !config.${namespace}.theme.catppuccin.enable;
+            sway.enable = !config.${namespace}.theme.catppuccin.enable;
+            # TODO: Very custom styling, integrate with their variables
+            # Currently setup only for catppuccin/nix
+            swaync.enable = false;
+            waybar.enable = !config.${namespace}.theme.catppuccin.enable;
+          };
+      };
+    }
+  );
 }
