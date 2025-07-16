e02ae4fe368b90ac139699e3d06b3fc366d1ad91
Austin Horstman <khaneliman12@gmail.com>
2025-07-15 20:39:50 -0500

    lib: consolidate common functions between helpers

--- lib/mk-system.nix
+++ lib/mk-system.nix
@@ -7,97 +7,50 @@
   ...
 }:
 let
-  # Import the khaneliparts flake to access the overlay
   flake = inputs.self or (throw "mkSystem requires 'inputs.self' to be passed");
+  common = import ./common.nix { inherit inputs; };
 
-  # Extend nixpkgs lib with khanelinix functions
-  extendedLib = inputs.nixpkgs.lib.extend flake.lib.overlay;
-
-  # Auto-discover home configurations for this system+hostname
-  inherit (flake.lib.file) scanHomes;
-  homesPath = ../homes;
-  allHomes = scanHomes homesPath;
-
-  # Filter for matching system and hostname
-  matchingHomes = extendedLib.filterAttrs (
-    _name: homeConfig: homeConfig.system == system && homeConfig.hostname == hostname
-  ) allHomes;
-
-  # Generate home-manager configuration for matching users
-  homeManagerConfig =
-    if matchingHomes != { } then
-      {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          extraSpecialArgs = {
-            inherit inputs system;
-            inherit (inputs) self;
-            lib = extendedLib;
-            flake-parts-lib = inputs.flake-parts.lib;
-          };
-          sharedModules = [
-            # Provide extended lib to all modules
-            { _module.args.lib = extendedLib; }
-
-            inputs.home-manager.flakeModules.home-manager
-            inputs.catppuccin.homeModules.catppuccin
-            inputs.hypr-socket-watch.homeManagerModules.default
-            inputs.nix-index-database.homeModules.nix-index
-            inputs.sops-nix.homeManagerModules.sops
-            ../modules/home
-          ];
-          users = extendedLib.mapAttrs' (_name: homeConfig: {
-            name = homeConfig.username;
-            value = {
-              imports = [ homeConfig.path ];
-              home = {
-                inherit (homeConfig) username;
-                homeDirectory = inputs.nixpkgs.lib.mkDefault "/home/${homeConfig.username}";
-              };
-              _module.args.username = homeConfig.username;
-            };
-          }) matchingHomes;
-        };
-      }
-    else
-      { };
+  extendedLib = common.mkExtendedLib flake inputs.nixpkgs;
+  matchingHomes = common.mkHomeConfigs {
+    inherit
+      flake
+      system
+      hostname
+      ;
+  };
+  homeManagerConfig = common.mkHomeManagerConfig {
+    inherit
+      extendedLib
+      inputs
+      system
+      matchingHomes
+      ;
+    isNixOS = true;
+  };
 in
 inputs.nixpkgs.lib.nixosSystem {
   inherit system;
 
-  specialArgs = {
-    inherit inputs hostname username;
-    inherit (inputs) self;
-    lib = extendedLib;
-    flake-parts-lib = inputs.flake-parts.lib;
-    format = "system";
-    host = hostname;
+  specialArgs = common.mkSpecialArgs {
+    inherit
+      inputs
+      hostname
+      username
+      extendedLib
+      ;
   };
 
   modules = [
-    # Provide extended lib to all modules
     { _module.args.lib = extendedLib; }
 
     # Configure nixpkgs with overlays
     {
       nixpkgs = {
         inherit system;
-        overlays = builtins.attrValues flake.overlays;
-        config = {
-          allowUnfree = true;
-          permittedInsecurePackages = [
-            # NOTE: citrix
-            "libxml2-2.13.8"
-          ];
-        };
-      };
+      } // common.mkNixpkgsConfig flake;
     }
 
-    # Home Manager NixOS module
     inputs.home-manager.nixosModules.home-manager
-
-    # System modules
     inputs.lanzaboote.nixosModules.lanzaboote
     inputs.sops-nix.nixosModules.sops
     inputs.disko.nixosModules.disko
@@ -109,12 +62,7 @@ inputs.nixpkgs.lib.nixosSystem {
     # Auto-inject home configurations for this system+hostname
     homeManagerConfig
 
-    # Base system modules
     ../modules/nixos
-
-    # Host-specific configuration
     ../systems/${system}/${hostname}
-
-    # Additional modules passed as arguments
   ] ++ modules;
 }
