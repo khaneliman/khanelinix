e28f4690b8881ead8e00350406d01a6b7f88aad7
Austin Horstman <khaneliman12@gmail.com>
2025-07-15 20:39:50 -0500

    lib: consolidate common functions between helpers

---
+++ lib/common.nix
@@ -0,0 +1,125 @@
+{ inputs }:
+let
+  inherit (inputs.nixpkgs.lib) filterAttrs mapAttrs';
+in
+{
+  mkExtendedLib = flake: nixpkgs: nixpkgs.lib.extend flake.lib.overlay;
+
+  mkNixpkgsConfig = flake: {
+    overlays = builtins.attrValues flake.overlays;
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        # NOTE: citrix
+        "libxml2-2.13.8"
+        # NOTE: needed by emulationstation
+        "freeimage-3.18.0-unstable-2024-04-18"
+        # dev shells
+        "aspnetcore-runtime-6.0.36"
+        "aspnetcore-runtime-7.0.20"
+        "aspnetcore-runtime-wrapped-7.0.20"
+        "aspnetcore-runtime-wrapped-6.0.36"
+        "dotnet-combined"
+      ];
+    };
+  };
+
+  # Scan and filter home configurations
+  mkHomeConfigs =
+    {
+      flake,
+      system,
+      hostname,
+    }:
+    let
+      inherit (flake.lib.file) scanHomes;
+      homesPath = ../homes;
+      allHomes = scanHomes homesPath;
+    in
+    filterAttrs (
+      _name: homeConfig: homeConfig.system == system && homeConfig.hostname == hostname
+    ) allHomes;
+
+  # Create home-manager configuration for system builders
+  mkHomeManagerConfig =
+    {
+      flake,
+      extendedLib,
+      inputs,
+      system,
+      matchingHomes,
+      isNixOS ? true,
+    }:
+    if matchingHomes != { } then
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = {
+            inherit inputs system;
+            inherit (inputs) self;
+            lib = extendedLib;
+            flake-parts-lib = inputs.flake-parts.lib;
+          };
+          sharedModules =
+            [
+              { _module.args.lib = extendedLib; }
+            ]
+            ++ (
+              if isNixOS then
+                [
+                  inputs.home-manager.flakeModules.home-manager
+                ]
+              else
+                [ ]
+            )
+            ++ [
+              inputs.catppuccin.homeModules.catppuccin
+              inputs.hypr-socket-watch.homeManagerModules.default
+              inputs.nix-index-database.homeModules.nix-index
+              inputs.sops-nix.homeManagerModules.sops
+              ../modules/home
+            ];
+          users = mapAttrs' (_name: homeConfig: {
+            name = homeConfig.username;
+            value =
+              {
+                imports = [ homeConfig.path ];
+                home = {
+                  inherit (homeConfig) username;
+                  homeDirectory = inputs.nixpkgs.lib.mkDefault (
+                    if isNixOS then "/home/${homeConfig.username}" else "/Users/${homeConfig.username}"
+                  );
+                };
+              }
+              // (
+                if isNixOS then
+                  {
+                    _module.args.username = homeConfig.username;
+                  }
+                else
+                  { }
+              );
+          }) matchingHomes;
+        };
+      }
+    else
+      { };
+
+  # Common specialArgs for system builders
+  mkSpecialArgs =
+    {
+      inputs,
+      hostname,
+      username,
+      extendedLib,
+    }:
+    {
+      inherit inputs hostname username;
+      inherit (inputs) self;
+      lib = extendedLib;
+      flake-parts-lib = inputs.flake-parts.lib;
+      format = "system";
+      host = hostname;
+    };
+}
--- lib/mk-darwin.nix
+++ lib/mk-darwin.nix
@@ -7,89 +7,48 @@
   ...
 }:
 let
-  # Import the khaneliparts flake to access the overlay
   flake = inputs.self or (throw "mkDarwin requires 'inputs.self' to be passed");
+  common = import ./common.nix { inherit inputs; };
 
-  # Extend nixpkgs lib with khanelinix functions
-  extendedLib = inputs.nixpkgs-unstable.lib.extend flake.lib.overlay;
-
-  # Auto-discover home configurations for this system+hostname
-  inherit (flake.lib.file) scanHomes;
-  homesPath = ../homes;
-  allHomes = scanHomes homesPath;
-
-  # Filter for matching system and hostname
-  matchingHomes = extendedLib.filterAttrs (
-    _name: homeConfig: homeConfig.system == system && homeConfig.hostname == hostname
-  ) allHomes;
-
-  # Generate home-manager configuration for matching users
-  homeManagerConfig =
-    if matchingHomes != { } then
-      {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          extraSpecialArgs = {
-            inherit inputs system;
-            inherit (flake) self;
-            lib = extendedLib;
-            flake-parts-lib = inputs.flake-parts.lib;
-          };
-          sharedModules = [
-            # Provide extended lib to all modules
-            { _module.args.lib = extendedLib; }
-
-            inputs.catppuccin.homeModules.catppuccin
-            inputs.hypr-socket-watch.homeManagerModules.default
-            inputs.nix-index-database.homeModules.nix-index
-            inputs.sops-nix.homeManagerModules.sops
-            ../modules/home
-          ];
-          users = extendedLib.mapAttrs' (_name: homeConfig: {
-            name = homeConfig.username;
-            value = {
-              imports = [ homeConfig.path ];
-              home = {
-                inherit (homeConfig) username;
-                homeDirectory = inputs.nixpkgs-unstable.lib.mkDefault "/Users/${homeConfig.username}";
-              };
-            };
-          }) matchingHomes;
-        };
-      }
-    else
-      { };
+  extendedLib = common.mkExtendedLib flake inputs.nixpkgs;
+  matchingHomes = common.mkHomeConfigs {
+    inherit
+      flake
+      system
+      hostname
+      ;
+  };
+  homeManagerConfig = common.mkHomeManagerConfig {
+    inherit
+      flake
+      extendedLib
+      inputs
+      system
+      matchingHomes
+      ;
+    isNixOS = false;
+  };
 in
 inputs.darwin.lib.darwinSystem {
   inherit system;
 
-  specialArgs = {
-    inherit inputs hostname username;
-    inherit (inputs) self;
-    lib = extendedLib;
-    flake-parts-lib = inputs.flake-parts.lib;
-    format = "system";
-    host = hostname;
+  specialArgs = common.mkSpecialArgs {
+    inherit
+      inputs
+      hostname
+      username
+      extendedLib
+      ;
   };
 
   modules = [
-    # Provide extended lib to all modules
     { _module.args.lib = extendedLib; }
 
     # Configure nixpkgs with overlays
     {
       nixpkgs = {
         inherit system;
-        overlays = builtins.attrValues flake.overlays;
-        config = {
-          allowUnfree = true;
-          permittedInsecurePackages = [
-            # NOTE: citrix
-            "libxml2-2.13.8"
-          ];
-        };
-      };
+      } // common.mkNixpkgsConfig flake;
     }
 
     inputs.home-manager.darwinModules.home-manager
@@ -100,12 +59,7 @@ inputs.darwin.lib.darwinSystem {
     # Auto-inject home configurations for this system+hostname
     homeManagerConfig
 
-    # Base Darwin modules
     ../modules/darwin
-
-    # Host-specific configuration
     ../systems/${system}/${hostname}
-
-    # Additional modules passed as arguments
   ] ++ modules;
 }
