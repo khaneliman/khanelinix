eb4d2a15c9b50fda2b5bc97b7d6b3a6ab9c5f191
Austin Horstman <khaneliman12@gmail.com>
2025-07-26 18:09:07 -0500

    anyrun: inline css to nix

--- modules/home/programs/graphical/launchers/anyrun/default.nix
+++ modules/home/programs/graphical/launchers/anyrun/default.nix
@@ -48,16 +48,17 @@ in
       };
 
       extraConfigFiles = {
-        "applications.ron".text = ''
-          Config(
-            // Also show the Desktop Actions defined in the desktop files, e.g. "New Window" from LibreWolf
-            desktop_actions: true,
-            max_entries: 10,
-            // The terminal used for running terminal based desktop entries, if left as `None` a static list of terminals is used
-            // to determine what terminal to use.
-            terminal: Some("foot"),
-          )
-        '';
+        "applications.ron".text = # Ron
+          ''
+            Config(
+              // Also show the Desktop Actions defined in the desktop files, e.g. "New Window" from LibreWolf
+              desktop_actions: true,
+              max_entries: 10,
+              // The terminal used for running terminal based desktop entries, if left as `None` a static list of terminals is used
+              // to determine what terminal to use.
+              terminal: Some("foot"),
+            )
+          '';
 
         "nixos-options.ron".text =
           let
@@ -65,6 +66,7 @@ in
               (osConfig.system.build.manual.optionsJSON or "/dev/null") + "/share/doc/nixos/options.json";
             options = builtins.toJSON { ":nix" = [ nixos-options ]; };
           in
+          # Ron
           ''
             Config(
               options: ${options},
@@ -73,44 +75,83 @@ in
             )
           '';
 
-        "symbols.ron".text = ''
-          Config(
-            // The prefix that the search needs to begin with to yield symbol results
-            prefix: ":sy",
+        "symbols.ron".text = # Ron
+          ''
+            Config(
+              // The prefix that the search needs to begin with to yield symbol results
+              prefix: ":sy",
 
-            // Custom user defined symbols to be included along the unicode symbols
-            symbols: {
-              // "name": "text to be copied"
-              "shrug": "¯\\_(ツ)_/¯",
-            },
+              // Custom user defined symbols to be included along the unicode symbols
+              symbols: {
+                // "name": "text to be copied"
+                "shrug": "¯\\_(ツ)_/¯",
+              },
 
-            // The number of entries to be displayed
-            max_entries: 10,
-          )
-        '';
+              // The number of entries to be displayed
+              max_entries: 10,
+            )
+          '';
 
         # NOTE: usage information
         # <prefix><src lang><language_delimiter><target lang> <text to translate>
         # ie: ':trenglish>spanish test this out'
         # <prefix><target lang> <text to translate>
         # ie: ':trspanish test this out'
-        "translate.ron".text = ''
-          Config(
-            prefix: ":tr",
-            language_delimiter: ">",
-            max_entries: 3,
-          )
-        '';
+        "translate.ron".text = # Ron
+          ''
+            Config(
+              prefix: ":tr",
+              language_delimiter: ">",
+              max_entries: 3,
+            )
+          '';
 
-        "websearch.ron".text = ''
-          Config(
-            prefix: "?",
-            engines: [DuckDuckGo]
-          )
-        '';
+        "websearch.ron".text = # Ron
+          ''
+            Config(
+              prefix: "?",
+              engines: [DuckDuckGo]
+            )
+          '';
       };
 
-      extraCss = builtins.readFile ./styles/dark.css;
+      extraCss = # CSS
+        ''
+          * {
+            transition: 200ms ease;
+            font-family: Lexend;
+            font-size: 1.3rem;
+          }
+
+          #window,
+          #match,
+          #entry,
+          #plugin,
+          #main {
+            background: transparent;
+          }
+
+          #match:selected {
+            background: rgba(203, 166, 247, 0.7);
+          }
+
+          #match {
+            padding: 3px;
+            border-radius: 16px;
+          }
+
+          #entry,
+          #plugin:hover {
+            border-radius: 16px;
+          }
+
+          box#main {
+            background: rgba(30, 30, 46, 1);
+            border: 2px solid #494d64;
+            border-radius: 16px;
+            padding: 8px;
+          }
+        '';
     };
   };
 }
