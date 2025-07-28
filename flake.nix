58c63f2ccc57ffb491354bf99e00412bf62ca5b2
Austin Horstman <khaneliman12@gmail.com>
2025-07-28 13:03:22 -0500

    overlays/hyprland: remove again
    Crash on launch

--- flake.nix
+++ flake.nix
@@ -59,9 +59,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hypr-socket-watch.url = "github:khaneliman/hypr-socket-watch";
-    hyprland = {
-      url = "github:hyprwm/hyprland";
-    };
     khanelivim.url = "github:khaneliman/khanelivim";
     nh.url = "github:nix-community/nh";
     nix-flatpak.url = "github:gmodena/nix-flatpak/latest";
--- overlays/hyprland/default.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, ... }:
-_final: prev: {
-  inherit (inputs.hyprland.packages.${prev.stdenv.hostPlatform.system})
-    hyprland
-    hyprland-unwrapped
-    xdg-desktop-portal-hyprland
-    ;
-}
