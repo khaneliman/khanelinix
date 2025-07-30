fbe2ea788c8ba8f0a6b3acf3691462421787ea5f
Austin Horstman <khaneliman12@gmail.com>
2025-07-29 23:28:41 -0500

    home/theme/catppuccin: protect against missing catppuccin input
    Prevent eval error due to missing catppuccin input with home module.

--- modules/home/theme/catppuccin/default.nix
+++ modules/home/theme/catppuccin/default.nix
@@ -2,7 +2,7 @@
   config,
   lib,
   pkgs,
-
+  inputs,
   ...
 }:
 let
@@ -76,154 +76,160 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    catppuccin = {
-      # NOTE: Need some customization and merging of configuration files so cant just enable all
-      enable = false;
+  config = mkIf cfg.enable (
+    lib.mkMerge [
+      (lib.optionalAttrs (inputs ? catppuccin && inputs.catppuccin ? homeModules) {
+        catppuccin = {
+          # NOTE: Need some customization and merging of configuration files so cant just enable all
+          enable = false;
 
-      accent = "blue";
-      flavor = "macchiato";
+          accent = "blue";
+          flavor = "macchiato";
 
-      alacritty = enabled;
-      bat = enabled;
-      bottom = enabled;
-      btop = enabled;
-      cava = enabled;
-      delta = enabled;
-      fish = enabled;
-      foot = enabled;
-      fzf = enabled;
-      gh-dash = enabled;
-      ghostty = enabled;
-      gitui = enabled;
-      glamour = enabled;
-      helix = enabled;
-      hyprland = mkIf config.khanelinix.programs.graphical.wms.hyprland.enable {
-        enable = true;
-        inherit (cfg) accent;
-      };
-      k9s = {
-        enable = true;
-        transparent = true;
-      };
-      kitty = enabled;
-      kvantum = {
-        enable = true;
-        inherit (cfg) accent;
-      };
-      lazygit = {
-        enable = true;
-        inherit (cfg) accent;
-      };
-      nvim = enabled;
-      sway = enabled;
-      waybar = enabled;
-      zathura = enabled;
-      zellij = enabled;
-      zsh-syntax-highlighting = enabled;
-    };
+          alacritty = enabled;
+          bat = enabled;
+          bottom = enabled;
+          btop = enabled;
+          cava = enabled;
+          delta = enabled;
+          fish = enabled;
+          foot = enabled;
+          fzf = enabled;
+          gh-dash = enabled;
+          ghostty = enabled;
+          gitui = enabled;
+          glamour = enabled;
+          helix = enabled;
+          hyprland = mkIf config.khanelinix.programs.graphical.wms.hyprland.enable {
+            enable = true;
+            inherit (cfg) accent;
+          };
+          k9s = {
+            enable = true;
+            transparent = true;
+          };
+          kitty = enabled;
+          kvantum = {
+            enable = true;
+            inherit (cfg) accent;
+          };
+          lazygit = {
+            enable = true;
+            inherit (cfg) accent;
+          };
+          nvim = enabled;
+          sway = enabled;
+          waybar = enabled;
+          zathura = enabled;
+          zellij = enabled;
+          zsh-syntax-highlighting = enabled;
+        };
+      })
 
-    home = {
-      file = mkMerge [
-        (
-          let
-            warpPkg = pkgs.fetchFromGitHub {
-              owner = "catppuccin";
-              repo = "warp";
-              rev = "11295fa7aed669ca26f81ff44084059952a2b528";
-              hash = "sha256-ym5hwEBtLlFe+DqMrXR3E4L2wghew2mf9IY/1aynvAI=";
+      {
+        home = {
+          file = mkMerge [
+            (
+              let
+                warpPkg = pkgs.fetchFromGitHub {
+                  owner = "catppuccin";
+                  repo = "warp";
+                  rev = "11295fa7aed669ca26f81ff44084059952a2b528";
+                  hash = "sha256-ym5hwEBtLlFe+DqMrXR3E4L2wghew2mf9IY/1aynvAI=";
+                };
+
+                warpStyle = "${warpPkg.outPath}/themes/catppuccin_macchiato.yml";
+              in
+              mkIf config.khanelinix.programs.terminal.emulators.warp.enable {
+                ".warp/themes/catppuccin_macchiato.yaml".source = warpStyle;
+                ".local/share/warp-terminal/themes/catppuccin_macchiato.yaml".source = warpStyle;
+              }
+            )
+            (mkIf pkgs.stdenv.hostPlatform.isDarwin {
+              # TODO: use packaged version
+              "Library/Application Support/BetterDiscord/themes/catppuccin-macchiato.theme.css".source =
+                ./catppuccin-macchiato.theme.css;
+            })
+          ];
+
+          pointerCursor = mkIf pkgs.stdenv.hostPlatform.isLinux {
+            inherit (config.khanelinix.theme.gtk.cursor) name package size;
+          };
+
+          sessionVariables = mkIf pkgs.stdenv.hostPlatform.isLinux {
+            CURSOR_THEME = config.khanelinix.theme.gtk.cursor.name;
+          };
+        };
+
+        programs = {
+          # Additional program settings that don't follow the common pattern
+          ncspot.settings = {
+            theme = {
+              background = "#24273A";
+              primary = "#CAD3F5";
+              secondary = "#1E2030";
+              title = "#8AADF4";
+              playing = "#8AADF4";
+              playing_selected = "#B7BDF8";
+              playing_bg = "#181926";
+              highlight = "#C6A0F6";
+              highlight_bg = "#494D64";
+              error = "#CAD3F5";
+              error_bg = "#ED8796";
+              statusbar = "#181926";
+              statusbar_progress = "#CAD3F5";
+              statusbar_bg = "#8AADF4";
+              cmdline = "#CAD3F5";
+              cmdline_bg = "#181926";
+              search_match = "#f5bde6";
             };
+          };
 
-            warpStyle = "${warpPkg.outPath}/themes/catppuccin_macchiato.yml";
-          in
-          mkIf config.khanelinix.programs.terminal.emulators.warp.enable {
-            ".warp/themes/catppuccin_macchiato.yaml".source = warpStyle;
-            ".local/share/warp-terminal/themes/catppuccin_macchiato.yaml".source = warpStyle;
-          }
-        )
-        (mkIf pkgs.stdenv.hostPlatform.isDarwin {
-          # TODO: use packaged version
-          "Library/Application Support/BetterDiscord/themes/catppuccin-macchiato.theme.css".source =
-            ./catppuccin-macchiato.theme.css;
-        })
-      ];
+          tmux.plugins = [
+            {
+              plugin = pkgs.tmuxPlugins.catppuccin;
+              extraConfig = ''
+                set -g @catppuccin_flavour '${cfg.flavor}'
+                set -g @catppuccin_host 'on'
+                set -g @catppuccin_user 'on'
+              '';
+            }
+          ];
 
-      pointerCursor = mkIf pkgs.stdenv.hostPlatform.isLinux {
-        inherit (config.khanelinix.theme.gtk.cursor) name package size;
-      };
+          vesktop.vencord = {
+            settings.enabledThemes = [
+              "catppuccin.css"
+            ];
+            # TODO: use packaged version
+            themes.catppuccin = ./Catppuccin-Macchiato-BD/src.css;
+          };
 
-      sessionVariables = mkIf pkgs.stdenv.hostPlatform.isLinux {
-        CURSOR_THEME = config.khanelinix.theme.gtk.cursor.name;
-      };
-    };
-
-    programs = {
-      # Additional program settings that don't follow the common pattern
-      ncspot.settings = {
-        theme = {
-          background = "#24273A";
-          primary = "#CAD3F5";
-          secondary = "#1E2030";
-          title = "#8AADF4";
-          playing = "#8AADF4";
-          playing_selected = "#B7BDF8";
-          playing_bg = "#181926";
-          highlight = "#C6A0F6";
-          highlight_bg = "#494D64";
-          error = "#CAD3F5";
-          error_bg = "#ED8796";
-          statusbar = "#181926";
-          statusbar_progress = "#CAD3F5";
-          statusbar_bg = "#8AADF4";
-          cmdline = "#CAD3F5";
-          cmdline_bg = "#181926";
-          search_match = "#f5bde6";
+          yazi.theme = lib.mkMerge [
+            (import ./yazi/filetype.nix)
+            (import ./yazi/manager.nix)
+            (import ./yazi/theme.nix)
+          ];
         };
-      };
 
-      tmux.plugins = [
-        {
-          plugin = pkgs.tmuxPlugins.catppuccin;
-          extraConfig = ''
-            set -g @catppuccin_flavour '${cfg.flavor}'
-            set -g @catppuccin_host 'on'
-            set -g @catppuccin_user 'on'
-          '';
-        }
-      ];
+        wayland.windowManager.hyprland.settings.plugin.hyprbars = {
+          bar_color = palette.colors.base.rgb;
 
-      vesktop.vencord = {
-        settings.enabledThemes = [
-          "catppuccin.css"
-        ];
-        # TODO: use packaged version
-        themes.catppuccin = ./Catppuccin-Macchiato-BD/src.css;
-      };
-
-      yazi.theme = lib.mkMerge [
-        (import ./yazi/filetype.nix)
-        (import ./yazi/manager.nix)
-        (import ./yazi/theme.nix)
-      ];
-    };
-
-    wayland.windowManager.hyprland.settings.plugin.hyprbars = {
-      bar_color = palette.colors.base.rgb;
-
-      hyprbars-button = lib.mkForce [
-        # close
-        "rgb(ED8796), 15, 󰅖, hyprctl dispatch killactive"
-        # maximize
-        "rgb(C6A0F6), 15, , hyprctl dispatch fullscreen 1"
-      ];
-    };
-
-    xdg.configFile =
-      mkIf (pkgs.stdenv.hostPlatform.isLinux && config.khanelinix.programs.graphical.apps.discord.enable)
-        {
-          # TODO: use packaged version
-          "ArmCord/themes/Catppuccin-Macchiato-BD".source = ./Catppuccin-Macchiato-BD;
-          "BetterDiscord/themes/catppuccin-macchiato.theme.css".source = ./catppuccin-macchiato.theme.css;
+          hyprbars-button = lib.mkForce [
+            # close
+            "rgb(ED8796), 15, 󰅖, hyprctl dispatch killactive"
+            # maximize
+            "rgb(C6A0F6), 15, , hyprctl dispatch fullscreen 1"
+          ];
         };
-  };
+
+        xdg.configFile =
+          mkIf (pkgs.stdenv.hostPlatform.isLinux && config.khanelinix.programs.graphical.apps.discord.enable)
+            {
+              # TODO: use packaged version
+              "ArmCord/themes/Catppuccin-Macchiato-BD".source = ./Catppuccin-Macchiato-BD;
+              "BetterDiscord/themes/catppuccin-macchiato.theme.css".source = ./catppuccin-macchiato.theme.css;
+            };
+      }
+    ]
+  );
 }
