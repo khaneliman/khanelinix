240893482d33f0a2b1d28c02e9b7b48677439c94
Austin Horstman <khaneliman12@gmail.com>
2025-08-05 12:02:58 -0500

    television: init module

---
+++ modules/home/programs/terminal/tools/television/default.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+
+  cfg = config.khanelinix.programs.terminal.tools.television;
+in
+{
+  options.khanelinix.programs.terminal.tools.television = {
+    enable = lib.mkEnableOption "television";
+  };
+
+  config = mkIf cfg.enable {
+    programs.television = {
+      enable = true;
+
+      settings = {
+        ui = {
+          use_nerd_font_icons = true;
+          theme = "catppuccin";
+        };
+        keybindings = {
+          quit = [
+            "esc"
+            "ctrl-c"
+          ];
+        };
+      };
+    };
+  };
+}
