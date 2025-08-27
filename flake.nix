cb3553aa5f6c8111b9ac42758e181e0ccfe01ad8
Austin Horstman <khaneliman12@gmail.com>
2025-08-27 14:55:34 -0500

    flake: reorganize flake inputs with logical groupings
    - Group inputs into core Nix ecosystem, system management, security &
    boot,
        system utilities, desktop environment & theming, and applications
      - Add descriptive section comments for better maintainability
      - Maintain all existing inputs while improving organization

--- flake.nix
+++ flake.nix
@@ -2,28 +2,23 @@
   description = "KhaneliNix";
 
   inputs = {
-    # Core inputs
+    # Core Nix ecosystem
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-master.url = "github:NixOS/nixpkgs";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-master.url = "github:NixOS/nixpkgs";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nix-darwin = {
-      # url = "github:nix-darwin/nix-darwin";
-      url = "github:khaneliman/nix-darwin/copy";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
 
-    # System inputs
+    # System management
     disko = {
       url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     lanzaboote = {
@@ -33,8 +28,14 @@
         flake-compat.follows = "";
       };
     };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
+    nix-darwin = {
+      # url = "github:nix-darwin/nix-darwin";
+      url = "github:khaneliman/nix-darwin/copy";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    nix-rosetta-builder = {
+      # url = "github:cpick/nix-rosetta-builder";
+      url = "github:khaneliman/nix-rosetta-builder/speedfactor";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     nixos-wsl = {
@@ -44,8 +45,12 @@
         flake-compat.follows = "";
       };
     };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
-    # Applications
+    # Applications & packages
     anyrun-nixos-options.url = "github:n3oney/anyrun-nixos-options";
     catppuccin = {
       url = "github:catppuccin/nix";
@@ -70,11 +75,6 @@
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
-    };
-    nix-rosetta-builder = {
-      # url = "github:cpick/nix-rosetta-builder";
-      url = "github:khaneliman/nix-rosetta-builder/speedfactor";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     stylix = {
       url = "github:danth/stylix";
