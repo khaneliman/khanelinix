7bd40aee956be89692e603044506a349a118064d
Austin Horstman <khaneliman12@gmail.com>
2025-09-02 13:20:31 -0500

    swappy: migrate to hm module
    available after https://github.com/nix-community/home-manager/pull/7760

--- modules/home/programs/graphical/addons/swappy/default.nix
+++ modules/home/programs/graphical/addons/swappy/default.nix
@@ -1,13 +1,9 @@
 {
   config,
   lib,
-  pkgs,
-
   ...
 }:
 let
-  inherit (lib) mkIf;
-
   cfg = config.khanelinix.programs.graphical.addons.swappy;
 in
 {
@@ -15,10 +11,26 @@ in
     enable = lib.mkEnableOption "Swappy in the desktop environment";
   };
 
-  config = mkIf cfg.enable {
-    home.packages = with pkgs; [ swappy ];
-
+  config = lib.mkIf cfg.enable {
+    # Placeholder for screenshots folder
     home.file."Pictures/screenshots/.keep".text = "";
-    xdg.configFile."swappy/config".source = ./config;
+
+    programs.swappy = {
+      enable = true;
+
+      settings = {
+        Default = {
+          save_dir = "$HOME/Pictures/screenshots/";
+          save_filename_format = "swappy-%Y%m%d-%H%M%S.png";
+          show_panel = false;
+          line_size = 5;
+          text_size = 20;
+          text_font = "sans-serif";
+          paint_mode = "brush";
+          early_exit = false;
+          fill_shape = false;
+        };
+      };
+    };
   };
 }
