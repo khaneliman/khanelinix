38acd58f10014bc9a8d239cdfabdd5a61cadfc33
Austin Horstman <khaneliman12@gmail.com>
2025-09-02 08:43:28 -0500

    home/suites/development: remove x86 darwin from reviews
    Basically unsupported platform, at this point. Not going to waste
    resources building it.

--- modules/home/suites/development/default.nix
+++ modules/home/suites/development/default.nix
@@ -119,8 +119,8 @@ in
         nra = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "all"'';
         nrap = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "all" --post-result --num-parallel-evals 4'';
         nrapa = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "all" --post-result --num-parallel-evals 4 --approve-pr'';
-        nrd = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "x86_64-darwin aarch64-darwin" --num-parallel-evals 2'';
-        nrdp = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "x86_64-darwin aarch64-darwin" --num-parallel-evals 2 --post-result'';
+        nrd = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "aarch64-darwin" --num-parallel-evals 2'';
+        nrdp = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "aarch64-darwin" --num-parallel-evals 2 --post-result'';
         nrl = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "x86_64-linux aarch64-linux" --num-parallel-evals 2'';
         nrlp = ''${lib.getExe pkgs.nixpkgs-review} pr $1 --systems "x86_64-linux aarch64-linux" --num-parallel-evals 2 --post-result'';
         nup = ''nix-update --commit -u $1'';
