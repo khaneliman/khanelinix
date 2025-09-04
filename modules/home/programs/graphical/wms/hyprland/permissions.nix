9cbf7f498b098c92d4a622a2fcebfb11a0600f3f
Austin Horstman <khaneliman12@gmail.com>
2025-09-03 20:46:42 -0500

    hyprland: permissions use regex instead of exact store path
    See if it helps with issue of booted system vs current system etc and
    sometimes getting a prompt for programs i have explicitly added...

--- modules/home/programs/graphical/wms/hyprland/permissions.nix
+++ modules/home/programs/graphical/wms/hyprland/permissions.nix
@@ -1,28 +1,63 @@
 {
   config,
   lib,
-
   pkgs,
   ...
 }:
 let
-  inherit (lib) mkIf;
   cfg = config.khanelinix.programs.graphical.wms.hyprland;
+
+  mkPackagePermission =
+    pkg: permission: action:
+    let
+      pname = pkg.pname or (builtins.parseDrvName pkg.name).name;
+    in
+    "/nix/store/[a-z0-9]{32}-${pname}-[0-9.]*/bin/${pname}, ${permission}, ${action}";
+
+  mkPackagePermissions =
+    packages: permission: action:
+    map (pkg: mkPackagePermission pkg permission action) packages;
+
+  mkPackagePathPermission =
+    pkg: subPath: permission: action:
+    let
+      pname = pkg.pname or (builtins.parseDrvName pkg.name).name;
+    in
+    "/nix/store/[a-z0-9]{32}-${pname}-[0-9.]*/${subPath}, ${permission}, ${action}";
+
+  mkLibPermission =
+    pkg: libPath: permission: action:
+    let
+      pname = pkg.pname or (builtins.parseDrvName pkg.name).name;
+    in
+    "/nix/store/[a-z0-9]{32}-${pname}-[0-9.]*/${libPath}, ${permission}, ${action}";
 in
 {
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     wayland.windowManager.hyprland = {
       settings = {
-        permission = [
-          "${lib.getExe pkgs.grimblast}, screencopy, allow"
-          "${lib.getExe pkgs.grim}, screencopy, allow"
-          "${lib.getExe pkgs.hyprpicker}, screencopy, allow"
-          "${lib.getExe pkgs.khanelinix.record_screen}, screencopy, allow"
-          "${config.wayland.windowManager.hyprland.portalPackage}/libexec/.xdg-desktop-portal-hyprland-wrapped, screencopy, allow"
-          "${lib.getExe config.programs.hyprlock.package}, screencopy, allow"
-        ]
-        ++ lib.optional (lib.elem pkgs.hyprlandPlugins.hyprbars config.wayland.windowManager.hyprland.plugins) "${lib.getLib pkgs.hyprlandPlugins.hyprbars}/lib/libhyprbars.so, plugin, allow"
-        ++ lib.optional (lib.elem pkgs.hyprlandPlugins.hyprexpo config.wayland.windowManager.hyprland.plugins) "${lib.getLib pkgs.hyprlandPlugins.hyprexpo}/lib/libhyprexpo.so, plugin, allow";
+        permission =
+          mkPackagePermissions [
+            config.programs.hyprlock.package
+            config.programs.hyprshot.package
+            pkgs.grim
+            pkgs.grimblast
+            pkgs.hyprpicker
+            pkgs.khanelinix.record_screen
+          ] "screencopy" "allow"
+          ++ [
+            (mkPackagePathPermission config.wayland.windowManager.hyprland.portalPackage
+              "libexec/.xdg-desktop-portal-hyprland-wrapped"
+              "screencopy"
+              "allow"
+            )
+          ]
+          ++
+            lib.optional (lib.elem pkgs.hyprlandPlugins.hyprbars config.wayland.windowManager.hyprland.plugins)
+              (mkLibPermission pkgs.hyprlandPlugins.hyprbars "lib/libhyprbars.so" "plugin" "allow")
+          ++
+            lib.optional (lib.elem pkgs.hyprlandPlugins.hyprexpo config.wayland.windowManager.hyprland.plugins)
+              (mkLibPermission pkgs.hyprlandPlugins.hyprexpo "lib/libhyprexpo.so" "plugin" "allow");
       };
     };
   };
