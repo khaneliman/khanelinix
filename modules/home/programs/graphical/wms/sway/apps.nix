99a15029c917982875546ba955522882fc7e0f0a
Austin Horstman <khaneliman12@gmail.com>
2025-09-05 17:48:25 -0500

    sway: uwsm wrap startup apps

--- modules/home/programs/graphical/wms/sway/apps.nix
+++ modules/home/programs/graphical/wms/sway/apps.nix
@@ -14,36 +14,42 @@ in
     wayland.windowManager.sway = {
       config = {
         startup =
-          lib.optionals config.programs.firefox.enable [
-            { command = getExe config.programs.firefox.package; }
-          ]
-          ++ lib.optionals config.programs.vesktop.enable [
-            { command = getExe config.programs.vesktop.package; }
-          ]
-          ++ lib.optionals (osConfig.programs.steam.enable or false) [
-            { command = "steam"; }
-          ]
-          ++ lib.optionals config.khanelinix.suites.social.enable [
-            { command = "element-desktop"; }
-          ]
-          ++ lib.optionals config.khanelinix.suites.business.enable [
-            { command = "teams-for-linux"; }
-            { command = "thunderbird"; }
-          ]
-          ++ lib.optionals (osConfig.services.hardware.openrgb.enable or false) [
-            { command = "openrgb -c blue"; }
-          ]
-          ++ lib.optionals (osConfig.programs._1password-gui.enable or false) [
-            { command = "1password --silent"; }
-          ]
-          ++ lib.optionals (osConfig.services.tailscale.enable or false) [
-            { command = "tailscale-systray"; }
-          ]
-          ++ lib.optionals (osConfig.networking.networkmanager.enable or false) [
-            { command = "nm-applet"; }
-          ]
+          let
+            # Helper function to conditionally prefix with uwsm
+            mkStartCommand =
+              cmd: if (osConfig.programs.uwsm.enable or false) then "uwsm app -- ${cmd}" else cmd;
+          in
+          (
+            lib.optionals config.programs.firefox.enable [
+              { command = mkStartCommand (getExe config.programs.firefox.package); }
+            ]
+            ++ lib.optionals config.programs.vesktop.enable [
+              { command = mkStartCommand (getExe config.programs.vesktop.package); }
+            ]
+            ++ lib.optionals (osConfig.programs.steam.enable or false) [
+              { command = mkStartCommand "steam"; }
+            ]
+            ++ lib.optionals config.khanelinix.suites.social.enable [
+              { command = mkStartCommand "element-desktop"; }
+            ]
+            ++ lib.optionals config.khanelinix.suites.business.enable [
+              { command = mkStartCommand "teams-for-linux"; }
+              { command = mkStartCommand "thunderbird"; }
+            ]
+            ++ lib.optionals (osConfig.services.hardware.openrgb.enable or false) [
+              { command = mkStartCommand "openrgb -c blue"; }
+            ]
+            ++ lib.optionals (osConfig.programs._1password-gui.enable or false) [
+              { command = mkStartCommand "1password --silent"; }
+            ]
+            ++ lib.optionals (osConfig.services.tailscale.enable or false) [
+              { command = mkStartCommand "tailscale-systray"; }
+            ]
+            ++ lib.optionals (osConfig.networking.networkmanager.enable or false) [
+              { command = mkStartCommand "nm-applet"; }
+            ]
+          )
           ++ [
-            # Always start these utilities
             { command = "wl-clip-persist --clipboard both"; }
             { command = "$(wayvnc $(tailscale ip --4))"; }
             { command = "notify-send --icon ~/.face -u normal \"Hello $(whoami)\""; }
