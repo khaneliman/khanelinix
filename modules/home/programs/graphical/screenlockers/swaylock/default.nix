b2056fb5133d50082a3d3520022f16db6cf425a6
Austin Horstman <khaneliman12@gmail.com>
2025-09-05 16:17:16 -0500

    swaylock: fix formatting spacing

--- modules/home/programs/graphical/screenlockers/swaylock/default.nix
+++ modules/home/programs/graphical/screenlockers/swaylock/default.nix
@@ -18,8 +18,8 @@ in
   config = mkIf cfg.enable {
     programs.swaylock = {
       enable = true;
-
       package = pkgs.swaylock-effects;
+
       settings = {
         ignore-empty-password = true;
         disable-caps-lock-text = true;
