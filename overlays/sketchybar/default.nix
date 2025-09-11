27bc7c19bce3b6144b8c546636770f3881b161db
Austin Horstman <khaneliman12@gmail.com>
2025-09-11 16:18:30 -0500

    overlays/sketchybar: bump to latest commit

--- overlays/sketchybar/default.nix
+++ overlays/sketchybar/default.nix
@@ -1,12 +1,12 @@
 _: _final: prev: {
   sketchybar = prev.sketchybar.overrideAttrs (oldAttrs: {
     doInstallCheck = false;
-    version = "2.22.1-2025-08-14";
+    version = "2.22.1-2025-09-11";
     src = prev.fetchFromGitHub {
       owner = "FelixKratz";
       repo = "SketchyBar";
-      rev = "6a2f97504b1a7181d519bd300535e7152c2a3cdd";
-      hash = "sha256-fx67l/mQcuxuE8UrWafiSF4n+uIgrioa32Qh3JDPYX8=";
+      rev = "1b751317d6ab4d48d4a58f209425d5560b8ed38b";
+      hash = "sha256-6fVcQPRqMI7nWfj4bPMt/myK5uYNtjZQ3gvO48UP2AI=";
     };
     # Create secondary sketchybar executable for dynamic island
     installPhase = # bash
