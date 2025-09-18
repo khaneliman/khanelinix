ad5f878e0872e90a95a57626f27d80975e954b13
Austin Horstman <khaneliman12@gmail.com>
2025-09-17 21:57:04 -0500

    home/suites/development: re-enable bruno on darwin

--- modules/home/suites/development/default.nix
+++ modules/home/suites/development/default.nix
@@ -56,6 +56,7 @@ in
       packages =
         with pkgs;
         [
+          bruno
           jqp
           onefetch
           tree-sitter
@@ -69,8 +70,6 @@ in
           postman
         ]
         ++ lib.optionals (pkgs.stdenv.hostPlatform.isLinux && !isWSL) [
-          # FIXME: build hangs
-          bruno
           github-desktop
           qtcreator
         ]
