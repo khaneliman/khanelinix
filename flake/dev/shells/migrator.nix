e7d7cdabdf91c1efdf368ab4b84bda517ac2a8c4
Austin Horstman <khaneliman12@gmail.com>
2025-09-18 22:43:08 -0500

    flake/dev/shells: simplify migrator
    Made a proper dev shell for project repo. Only need this for the repos
    being migrated.

--- flake/dev/shells/migrator.nix
+++ flake/dev/shells/migrator.nix
@@ -1,39 +1,20 @@
 {
   mkShell,
   pkgs,
-  lib,
   ...
 }:
 mkShell {
-  packages =
-    with pkgs;
-    [
-      bun
-      nodejs_20
-      eslint_d
-      typescript-language-server
-      typescript
-      claude-code
-      csharpier
-      (csharp-ls.overrideAttrs (_oldAttrs: {
-        # NOTE: csharp-ls requires a very new dotnet 8 sdk. This causes issues with workspace dotnet
-        # collisions because dotnet commands will run off the newest SDK breaking working with lower
-        # version projects.
-        useDotnetFromEnv = false;
-        meta.badPlatforms = [ ];
-      }))
-      (
-        with dotnetCorePackages;
-        combinePackages [
-          dotnet-aspnetcore_8
-          dotnet-runtime_8
-          dotnet-sdk_8
-        ]
-      )
-    ]
-    ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-      react-native-debugger
-    ];
+  packages = with pkgs; [
+    claude-code
+    (
+      with dotnetCorePackages;
+      combinePackages [
+        dotnet-aspnetcore_8
+        dotnet-runtime_8
+        dotnet-sdk_8
+      ]
+    )
+  ];
 
   shellHook = ''
 
