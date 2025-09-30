97b82a9e087c902bc7f84095cb7172fd900811f5
Austin Horstman <khaneliman12@gmail.com>
2025-09-30 15:33:26 -0500

    overlyas/jankyborders: 1.8.3 -> 1.8.4

--- overlays/jankyborders/default.nix
+++ overlays/jankyborders/default.nix
@@ -1,12 +1,12 @@
 _: _final: prev: {
   # TODO: remove after hits channel
   jankyborders = prev.jankyborders.overrideAttrs (_oldAttrs: rec {
-    version = "1.8.3";
+    version = "1.8.4";
     src = prev.fetchFromGitHub {
       owner = "FelixKratz";
       repo = "jankyborders";
       tag = "v${version}";
-      hash = "sha256-lc61PjaRZ8ZOWAFhsf/G3sQkd1oUyePHU43w4pt1AWY=";
+      hash = "sha256-31Er+cUQNJbZnXKC6KvlrBhOvyPAM7nP3BaxunAtvWg=";
     };
   });
 }
