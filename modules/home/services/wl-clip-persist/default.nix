022fa2bb45cc5eb92b12cf521b21bcec172da1c7
Austin Horstman <khaneliman12@gmail.com>
2025-10-16 19:54:56 -0500

    home/services/wl-clip-persist: add module
    replace manual invocation with systemd service from upstream module
    available after https://github.com/nix-community/home-manager/pull/8003

---
+++ modules/home/services/wl-clip-persist/default.nix
@@ -0,0 +1,50 @@
+{
+  config,
+  lib,
+
+  osConfig ? { },
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.khanelinix.services.wl-clip-persist;
+in
+{
+  options.khanelinix.services.wl-clip-persist = {
+    enable = mkEnableOption "wl-clip-persist";
+
+    systemdTargets = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = ''
+        Systemd targets for wl-clip-persist
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services = {
+      wl-clip-persist = {
+        enable = true;
+        clipboardType = "both";
+        systemdTargets =
+          cfg.systemdTargets
+          ++ lib.optionals (
+            (config.wayland.windowManager.hyprland.enable && (osConfig.programs.hyprland.withUWSM or false))
+            || (config.wayland.windowManager.sway.enable && (osConfig.programs.uwsm.enable or false))
+          ) [ "graphical-session.target" ]
+          ++
+            lib.optionals
+              (config.wayland.windowManager.hyprland.enable && !(osConfig.programs.hyprland.withUWSM or false))
+              [
+                "hyprland-session.target"
+              ]
+          ++ lib.optionals (
+            config.wayland.windowManager.sway.enable && !(osConfig.programs.uwsm.enable or false)
+          ) [ "sway-session.target" ];
+
+      };
+    };
+  };
+}
