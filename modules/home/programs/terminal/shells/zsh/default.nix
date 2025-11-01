04b6077808cc4a7da19b5d32d073e869a997f1bc
Austin Horstman <khaneliman12@gmail.com>
2025-11-01 01:20:50 -0500

    zsh: fix syntax highlighting path
    Not sure what changed, stopped working with old path

--- modules/home/programs/terminal/shells/zsh/default.nix
+++ modules/home/programs/terminal/shells/zsh/default.nix
@@ -190,7 +190,7 @@ in
           }
           {
             name = "fast-syntax-highlighting";
-            file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+            file = "share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh";
             src = pkgs.zsh-fast-syntax-highlighting;
           }
           {
