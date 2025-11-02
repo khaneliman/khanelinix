73ad7ecca7c58efea4e1e5ea0b43d3b4b68170a1
Austin Horstman <khaneliman12@gmail.com>
2025-11-02 13:01:33 -0600

    jankyborders: remove unused options
    Stuff was moved to the nix-darwin module

--- modules/home/services/jankyborders/default.nix
+++ modules/home/services/jankyborders/default.nix
@@ -1,28 +1,16 @@
 {
   config,
   lib,
-  osConfig,
-  pkgs,
   ...
 }:
 let
   inherit (lib) mkIf;
-  inherit (lib.khanelinix) mkOpt;
 
   cfg = config.khanelinix.services.jankyborders;
-  inherit (osConfig.khanelinix.services.jankyborders) logPath;
 in
 {
   options.khanelinix.services.jankyborders = {
     enable = lib.khanelinix.mkBoolOpt false "Whether to enable jankyborders in the desktop environment.";
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.jankyborders;
-      defaultText = lib.literalExpression "pkgs.jankyborders";
-      description = "The jankyborders package to use.";
-      example = lib.literalExpression "pkgs.khanelinix.jankyborders";
-    };
-    logFile = mkOpt lib.types.str logPath "Filepath of log output";
   };
 
   config = mkIf cfg.enable {
