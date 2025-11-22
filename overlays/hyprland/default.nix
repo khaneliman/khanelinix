0eafb2721205380621b3ed050071ec28527fbab7
Austin Horstman <khaneliman12@gmail.com>
2025-11-21 22:44:07 -0600

    overlays/hyprland: update patch hash
    saw mismatch on other machine

--- overlays/hyprland/default.nix
+++ overlays/hyprland/default.nix
@@ -3,7 +3,7 @@ _: _final: prev: {
     patches = (oldAttrs.patches or [ ]) ++ [
       (prev.fetchpatch {
         url = "https://patch-diff.githubusercontent.com/raw/hyprwm/Hyprland/pull/12090.patch";
-        hash = "sha256-sYyZ7h5zjQMPUI3plLdorRa4AUB++/SWDY17C7hMDno=";
+        hash = "sha256-pUtFyhiLku4OBXlDI/FgBo8QkG3XbCF1RWdXUXkylLY=";
       })
     ];
   });
