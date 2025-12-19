278a3eedb305eb5ef655ae5dd1211cbe40b47e85
Austin Horstman <khaneliman12@gmail.com>
2025-12-18 22:16:26 -0600

    vscode: re-enable python extension

--- modules/home/programs/graphical/editors/vscode/default.nix
+++ modules/home/programs/graphical/editors/vscode/default.nix
@@ -273,11 +273,7 @@ in
               with pkgs.vscode-extensions;
               commonExtensions
               ++ [
-                # FIXME: incorrect hash
-                # error: hash mismatch in fixed-output derivation '/nix/store/fasa6wib6pa5vviq04a9v2vacap94jiv-ms-python-python.zip.drv':
-                # ┃specified: sha256-8dc1uM/6iUNF+9y4yKH7w4/FsrzgVoOJlIFfQOvY8YM=
-                # ┃got: sha256-uD6NWGD5GyYwd7SeoGsgYEH26NI+hDxCx3f2EhqoOXk=
-                # ms-python.python
+                ms-python.python
                 ms-python.debugpy
                 njpwerner.autodocstring
               ];
