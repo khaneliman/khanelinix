0b5d43d22507f9c4ed8bd99fdd079b5eded04503
Austin Horstman <khaneliman12@gmail.com>
2026-01-08 22:08:48 -0600

    overlays/input-packages: consolidate input overlays

--- overlays/firefox-addons/default.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, ... }:
-final: _prev: {
-  firefox-addons = import inputs.firefox-addons {
-    inherit (final) fetchurl;
-    inherit (final) lib;
-    inherit (final) stdenv;
-  };
-}
---
+++ overlays/input-packages/default.nix
@@ -0,0 +1,75 @@
+{ inputs }:
+final: _prev:
+let
+  citrix = import inputs.nixpkgs-citrix-workspace {
+    inherit (final.stdenv.hostPlatform) system;
+    inherit (final) config;
+  };
+  master = import inputs.nixpkgs-master {
+    inherit (final.stdenv.hostPlatform) system;
+    inherit (final) config;
+  };
+  unstable = import inputs.nixpkgs-unstable {
+    inherit (final.stdenv.hostPlatform) system;
+    inherit (final) config;
+  };
+in
+{
+  #          ╭──────────────────────────────────────────────────────────╮
+  #          │                Citrix last known working                 │
+  #          ╰──────────────────────────────────────────────────────────╯
+  inherit (citrix)
+    citrix_workspace
+    ;
+
+  #          ╭──────────────────────────────────────────────────────────╮
+  #          │                 Firefox Addon repository                 │
+  #          ╰──────────────────────────────────────────────────────────╯
+  firefox-addons = import inputs.firefox-addons {
+    inherit (final) fetchurl;
+    inherit (final) lib;
+    inherit (final) stdenv;
+  };
+
+  #          ╭──────────────────────────────────────────────────────────╮
+  #          │ From nixpkgs-master (fast updating / want latest always) │
+  #          ╰──────────────────────────────────────────────────────────╯
+  inherit (master)
+    antigravity
+    claude-code
+    gemini-cli
+    opencode
+    yaziPlugins
+
+    # TODO: remove after hitting channel
+    normalize
+    vicinae
+    ;
+
+  python3 = _prev.python3.override {
+    packageOverrides = _pyFinal: _pyPrev: {
+      # TODO: remove after hitting channel
+      inherit (master.python3Packages) fastmcp mcp;
+    };
+  };
+
+  python3Packages = final.python3.pkgs;
+
+  #          ╭──────────────────────────────────────────────────────────╮
+  #          │   From nixpkgs-unstable (reasonable update / stability   │
+  #          │                         balance)                         │
+  #          ╰──────────────────────────────────────────────────────────╯
+  inherit (unstable)
+    # Misc
+    _1password-gui
+
+    # Online services to keep up to date
+    element-desktop
+    firefox-devedition
+    firefox-devedition-unwrapped
+    teams-for-linux
+    telegram-desktop
+    thunderbird-unwrapped
+    thunderbird-latest
+    ;
+}
