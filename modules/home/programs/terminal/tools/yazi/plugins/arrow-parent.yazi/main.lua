8c8be992815496c3f3b50e29a608fbd6962c828b
Austin Horstman <khaneliman12@gmail.com>
2026-01-10 16:13:17 -0600

    yazi: fix arrow-parent plugin
    Looks like api change without notice?

--- modules/home/programs/terminal/tools/yazi/plugins/arrow-parent.yazi/main.lua
+++ modules/home/programs/terminal/tools/yazi/plugins/arrow-parent.yazi/main.lua
@@ -7,7 +7,7 @@ local function entry(_, job)
 
 	local target = parent.files[parent.cursor + 1 + job.args[1]]
 	if target and target.cha.is_dir then
-		ya.manager_emit("cd", { target.url })
+		ya.emit("cd", { target.url })
 	end
 end
 
