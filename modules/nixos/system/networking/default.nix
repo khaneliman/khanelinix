cdde3773061996f354ea5e189d55d761df9bd997
Austin Horstman <khaneliman12@gmail.com>
2026-01-14 12:10:07 -0600

    nixos/system/networking: fix tcp optimization grouping
    Make sure the optimizations are properly grouped behind the option
    instead of partially.

--- modules/nixos/system/networking/default.nix
+++ modules/nixos/system/networking/default.nix
@@ -80,18 +80,24 @@ in
         "net.ipv4.icmp_echo_ignore_broadcasts" = true;
         "net.ipv6.conf.default.accept_ra" = 1;
         "net.ipv6.conf.all.accept_ra" = 1;
-        "net.ipv4.tcp_timestamps" = 0;
+        # Timestamps improve performance (PAWS, RTT estimation) - security concern is minimal
+        "net.ipv4.tcp_timestamps" = 1;
 
-        # TCP optimization
-        # TCP Fast Open is a TCP extension that reduces network latency by packing
-        # data in the sender’s initial TCP SYN. Setting 3 = enable TCP Fast Open for
-        # both incoming and outgoing connections:
+        # Conntrack tuning
+        "net.netfilter.nf_conntrack_generic_timeout" = 60;
+        "net.netfilter.nf_conntrack_max" = 1048576;
+        "net.netfilter.nf_conntrack_tcp_timeout_established" = 600;
+        "net.netfilter.nf_conntrack_tcp_timeout_time_wait" = 1;
+      }
+      // lib.optionalAttrs cfg.optimizeTcp {
+        # TCP optimization (enabled via optimizeTcp flag)
+        # TCP Fast Open reduces latency by packing data in initial SYN
         "net.ipv4.tcp_fastopen" = 3;
-        # Bufferbloat mitigations + slight improvement in throughput & latency
+        # BBR congestion control with fq qdisc (designed to work together)
         "net.ipv4.tcp_congestion_control" = "bbr";
-        "net.core.default_qdisc" = "cake";
+        "net.core.default_qdisc" = "fq";
 
-        # Other stuff that I am too lazy to document
+        # Buffer and connection tuning
         "net.core.optmem_max" = 65536;
         "net.core.rmem_default" = 1048576;
         "net.core.rmem_max" = 16777216;
@@ -102,16 +108,12 @@ in
         "net.ipv4.tcp_max_syn_backlog" = 8192;
         "net.ipv4.tcp_max_tw_buckets" = 2000000;
         "net.ipv4.tcp_mtu_probing" = 1;
-        "net.ipv4.tcp_rmem" = "4096 1048576 2097152";
+        "net.ipv4.tcp_rmem" = "4096 1048576 16777216";
         "net.ipv4.tcp_slow_start_after_idle" = 0;
         "net.ipv4.tcp_tw_reuse" = 1;
         "net.ipv4.tcp_wmem" = "4096 65536 16777216";
         "net.ipv4.udp_rmem_min" = 8192;
         "net.ipv4.udp_wmem_min" = 8192;
-        "net.netfilter.nf_conntrack_generic_timeout" = 60;
-        "net.netfilter.nf_conntrack_max" = 1048576;
-        "net.netfilter.nf_conntrack_tcp_timeout_established" = 600;
-        "net.netfilter.nf_conntrack_tcp_timeout_time_wait" = 1;
       };
     };
 
