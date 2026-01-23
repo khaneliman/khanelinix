3b989f71d83a895c8a63cc9e5d3353a7ef0e971b
Austin Horstman <khaneliman12@gmail.com>
2026-01-23 13:24:41 -0600

    chore(nix): add niri cachix

--- modules/common/nix/default.nix
+++ modules/common/nix/default.nix
@@ -271,6 +271,7 @@ in
             "https://khanelinix.cachix.org"
             "https://khanelivim.cachix.org"
             "https://nix-community.cachix.org"
+            "https://niri.cachix.org"
             "https://nixpkgs-unfree.cachix.org"
             "https://numtide.cachix.org"
           ];
@@ -280,6 +281,7 @@ in
             "khanelinix.cachix.org-1:FTmbv7OqlMsmJEOFvAlz7PVkoGtstbwLC2OldAiJZ10="
             "khanelivim.cachix.org-1:Tb0jsMlhXSJDtI2ISiGPBrvL1XIzQrWap80AiJuBGI0="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
             "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
             "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
           ];
