86d4bc44281242d707e43df40824858c0fd11b63
Austin Horstman <khaneliman12@gmail.com>
2026-01-23 13:25:14 -0600

    feat(niri): add home module

---
+++ modules/home/programs/graphical/wms/niri/apps.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+
+  osConfig ? { },
+  ...
+}:
+let
+  inherit (lib) mkAfter mkIf;
+
+  cfg = config.khanelinix.programs.graphical.wms.niri;
+
+  mkStartCommand =
+    cmd: if (osConfig.programs.uwsm.enable or false) then "uwsm app -- ${cmd}" else cmd;
+in
+{
+  config = mkIf cfg.enable {
+    programs.niri.settings.spawn-at-startup = mkAfter (
+      lib.optionals config.programs.firefox.enable [
+        { sh = mkStartCommand (lib.getExe config.programs.firefox.package); }
+      ]
+      ++ lib.optionals (osConfig.programs.steam.enable or false) [
+        { sh = mkStartCommand "steam"; }
+      ]
+      ++ lib.optionals config.khanelinix.suites.social.enable [
+        { sh = mkStartCommand "element-desktop"; }
+      ]
+      ++ lib.optionals config.khanelinix.suites.business.enable [
+        { sh = mkStartCommand "teams-for-linux"; }
+        { sh = mkStartCommand "thunderbird"; }
+      ]
+      ++ lib.optionals (osConfig.services.hardware.openrgb.enable or false) [
+        { sh = mkStartCommand "openrgb -c blue"; }
+      ]
+      ++ lib.optionals (osConfig.programs._1password-gui.enable or false) [
+        { sh = mkStartCommand "1password --silent"; }
+      ]
+      ++ lib.optionals (osConfig.networking.networkmanager.enable or false) [
+        { sh = mkStartCommand "nm-applet"; }
+      ]
+      ++ [
+        { sh = mkStartCommand "wayvnc $(tailscale ip --4)"; }
+      ]
+    );
+  };
+}
---
+++ modules/home/programs/graphical/wms/niri/default.nix
@@ -0,0 +1,74 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf;
+  inherit (lib.khanelinix) disabled enabled;
+
+  cfg = config.khanelinix.programs.graphical.wms.niri;
+in
+{
+  options.khanelinix.programs.graphical.wms.niri = {
+    enable = mkEnableOption "niri";
+    settings = lib.mkOption {
+      type = lib.types.attrs;
+      default = { };
+      description = "Configuration to pass through to the niri module.";
+    };
+  };
+
+  imports = [
+    ./apps.nix
+    ./binds.nix
+    ./variables.nix
+    ./workspace-rules.nix
+    ./window-rules.nix
+  ];
+
+  config = mkIf cfg.enable {
+    home = {
+      packages = with pkgs; [
+        grim
+        light
+        playerctl
+        slurp
+        swaybg
+        wl-clipboard
+      ];
+    };
+
+    khanelinix = {
+      programs = {
+        graphical = {
+          bars = {
+            ashell = lib.mkDefault enabled;
+            waybar = lib.mkDefault disabled;
+          };
+
+          launchers = {
+            anyrun = enabled;
+            vicinae = enabled;
+          };
+
+          screenlockers = {
+            swaylock = enabled;
+          };
+        };
+      };
+
+      suites = {
+        wlroots = enabled;
+      };
+
+      theme = {
+        gtk = enabled;
+        qt = enabled;
+      };
+    };
+
+    programs.niri.settings = cfg.settings;
+  };
+}
