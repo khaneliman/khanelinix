3289c4f3f2342d1b5db63738ba1eb620e88767e5
Austin Horstman <khaneliman12@gmail.com>
2026-01-23 13:24:58 -0600

    feat(niri): add nixos module

---
+++ modules/nixos/programs/graphical/wms/niri/default.nix
@@ -0,0 +1,74 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  inherit (lib)
+    mkBefore
+    mkDefault
+    mkIf
+    types
+    ;
+  inherit (lib.khanelinix) enabled mkOpt;
+
+  cfg = config.khanelinix.programs.graphical.wms.niri;
+in
+{
+  imports = [ inputs.niri.nixosModules.niri ];
+
+  options.khanelinix.programs.graphical.wms.niri = with types; {
+    enable = lib.mkEnableOption "Niri";
+    package = mkOpt package pkgs.niri-stable "Niri package (stable or unstable).";
+  };
+
+  config = mkIf cfg.enable {
+    niri-flake.cache.enable = mkDefault true;
+
+    nixpkgs.overlays = mkBefore [ inputs.niri.overlays.niri ];
+
+    programs.niri = {
+      enable = true;
+      inherit (cfg) package;
+    };
+
+    khanelinix = {
+      display-managers.sddm.enable = true;
+
+      home = {
+        configFile = lib.optionalAttrs config.programs.uwsm.enable {
+          "uwsm/env-niri".text = /* Bash */ ''
+            export XDG_CURRENT_DESKTOP=niri
+            export XDG_SESSION_TYPE=wayland
+            export XDG_SESSION_DESKTOP=niri
+          '';
+        };
+      };
+
+      programs.graphical = {
+        apps = {
+          gnome-disks = enabled;
+          partitionmanager = enabled;
+        };
+
+        file-managers = {
+          nautilus = enabled;
+        };
+      };
+
+      security = {
+        keyring = enabled;
+        polkit = enabled;
+      };
+
+      suites.wlroots = enabled;
+
+      theme = {
+        gtk = enabled;
+        qt = enabled;
+      };
+    };
+  };
+}
