d262ef24f891aff0fbf6d391d35bf35524eb91d8
Austin Horstman <khaneliman12@gmail.com>
2026-01-25 17:11:21 -0600

    fix(niri): guard module on input availability

--- modules/nixos/programs/graphical/wms/niri/default.nix
+++ modules/nixos/programs/graphical/wms/niri/default.nix
@@ -15,60 +15,67 @@ let
   inherit (lib.khanelinix) enabled mkOpt;
 
   cfg = config.khanelinix.programs.graphical.wms.niri;
+  hasNiri = inputs ? niri;
 in
 {
-  imports = [ inputs.niri.nixosModules.niri ];
+  imports = lib.optionals hasNiri [ inputs.niri.nixosModules.niri ];
 
   options.khanelinix.programs.graphical.wms.niri = with types; {
     enable = lib.mkEnableOption "Niri";
     package = mkOpt package pkgs.niri-stable "Niri package (stable or unstable).";
   };
 
-  config = mkIf cfg.enable {
-    niri-flake.cache.enable = mkDefault true;
+  config = mkIf cfg.enable (
+    lib.mkMerge [
+      (lib.optionalAttrs hasNiri {
+        niri-flake.cache.enable = mkDefault true;
 
-    nixpkgs.overlays = mkBefore [ inputs.niri.overlays.niri ];
+        nixpkgs.overlays = mkBefore [ inputs.niri.overlays.niri ];
 
-    programs.niri = {
-      enable = true;
-      inherit (cfg) package;
-    };
-
-    khanelinix = {
-      display-managers.sddm.enable = true;
-
-      home = {
-        configFile = lib.optionalAttrs config.programs.uwsm.enable {
-          "uwsm/env-niri".text = /* Bash */ ''
-            export XDG_CURRENT_DESKTOP=niri
-            export XDG_SESSION_TYPE=wayland
-            export XDG_SESSION_DESKTOP=niri
-          '';
+        programs.niri = {
+          enable = true;
+          inherit (cfg) package;
         };
-      };
+      })
 
-      programs.graphical = {
-        apps = {
-          gnome-disks = enabled;
-          partitionmanager = enabled;
+      {
+        khanelinix = {
+          display-managers.sddm.enable = true;
+
+          home = {
+            configFile = lib.optionalAttrs config.programs.uwsm.enable {
+              "uwsm/env-niri".text = /* Bash */ ''
+                export XDG_CURRENT_DESKTOP=niri
+                export XDG_SESSION_TYPE=wayland
+                export XDG_SESSION_DESKTOP=niri
+              '';
+            };
+          };
+
+          programs.graphical = {
+            apps = {
+              gnome-disks = enabled;
+              partitionmanager = enabled;
+            };
+
+            file-managers = {
+              nautilus = enabled;
+            };
+          };
+
+          security = {
+            keyring = enabled;
+            polkit = enabled;
+          };
+
+          suites.wlroots = enabled;
+
+          theme = {
+            gtk = enabled;
+            qt = enabled;
+          };
         };
-
-        file-managers = {
-          nautilus = enabled;
-        };
-      };
-
-      security = {
-        keyring = enabled;
-        polkit = enabled;
-      };
-
-      suites.wlroots = enabled;
-
-      theme = {
-        gtk = enabled;
-        qt = enabled;
-      };
-    };
-  };
+      }
+    ]
+  );
 }
