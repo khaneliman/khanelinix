65971f49e18a52d1f751d6d92ca7560a444ee26c
Austin Horstman <khaneliman12@gmail.com>
2026-01-29 16:23:34 -0600

    chore(nix)!: remove extra-deprecated-features
    Didn't solve the issue it was intended to, also causes warnings in logs.

--- modules/common/nix/default.nix
+++ modules/common/nix/default.nix
@@ -243,9 +243,6 @@ in
             "pipe-operators"
             "dynamic-derivations"
           ];
-          extra-deprecated-features = lib.mkIf cfg.useLix [
-            "url-literals"
-          ];
           # Prevent builds failing just because we can't contact a substituter
           fallback = true;
           flake-registry = "/etc/nix/registry.json";
