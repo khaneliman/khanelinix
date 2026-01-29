679a389d007fe0a0acf482d316bb6c68b8466722
Austin Horstman <khaneliman12@gmail.com>
2026-01-29 10:08:27 -0600

    refactor(yazi)!: refactor go-to keymaps
    Change up the display and mappings for each platform.

--- modules/home/programs/terminal/tools/yazi/keymap/manager/goto.nix
+++ modules/home/programs/terminal/tools/yazi/keymap/manager/goto.nix
@@ -14,15 +14,14 @@ let
       isCommand ? false,
     }:
     let
-      defaultDesc =
+      runCmd = if isCommand then dirPath else "cd ${dirPath}";
+      description =
         if isCommand then
-          null # Commands must provide their own descriptions
+          desc
+        else if desc != null then
+          "Go to the ${desc} directory"
         else
           "Go to the ${dirPath} directory";
-
-      description = if desc != null then desc else defaultDesc;
-
-      runCmd = if isCommand then dirPath else "cd ${dirPath}";
     in
     {
       on = [
@@ -36,24 +35,10 @@ let
   getDir =
     dir: default: if config.xdg.userDirs.enable then dir else "${config.home.homeDirectory}/${default}";
 
-  # Define all goto locations with minimal required information
-  gotoLocations = [
+  commonLocations = [
     {
       key = "/";
       dirPath = "/";
-    }
-    {
-      key = "h";
-      dirPath = config.home.homeDirectory;
-      desc = "Go to the home directory";
-    }
-    {
-      key = "c";
-      dirPath = config.xdg.configHome;
-    }
-    {
-      key = "t";
-      dirPath = "/tmp";
     }
     {
       key = "<Space>";
@@ -62,20 +47,19 @@ let
       desc = "Go to a directory interactively";
     }
     {
-      key = "D";
-      dirPath = getDir config.xdg.userDirs.download "Downloads";
-    }
-    {
-      key = "G";
-      dirPath = "${getDir config.xdg.userDirs.documents (lib.optionalString pkgs.stdenv.hostPlatform.isLinux "Documents")}/gitlab";
-    }
-    {
-      key = "M";
-      dirPath = "/mnt";
+      key = "c";
+      dirPath = config.xdg.configHome;
+      desc = "~/.config";
     }
     {
       key = "d";
       dirPath = getDir config.xdg.userDirs.documents "Documents";
+      desc = "~/Documents";
+    }
+    {
+      key = "D";
+      dirPath = getDir config.xdg.userDirs.download "Downloads";
+      desc = "~/Downloads";
     }
     {
       key = "e";
@@ -84,31 +68,32 @@ let
     {
       key = "g";
       dirPath = "${getDir config.xdg.userDirs.documents (lib.optionalString pkgs.stdenv.hostPlatform.isLinux "Documents")}/github";
+      desc = "~/Documents/github";
     }
     {
-      key = "i";
-      dirPath = "/run/media/${config.khanelinix.user.name}";
-      desc = "Go to the media directory";
+      key = "G";
+      dirPath = "${getDir config.xdg.userDirs.documents (lib.optionalString pkgs.stdenv.hostPlatform.isLinux "Documents")}/gitlab";
+      desc = "~/Documents/gitlab";
+    }
+    {
+      key = "h";
+      dirPath = config.home.homeDirectory;
+      desc = "~";
+    }
+    {
+      key = "k";
+      dirPath = getDir config.xdg.userDirs.desktop "Desktop";
+      desc = "~/Desktop";
     }
     {
       key = "l";
       dirPath = "${config.home.homeDirectory}/.local/";
-    }
-    {
-      key = "m";
-      dirPath = "/media";
-    }
-    {
-      key = "o";
-      dirPath = "/opt";
+      desc = "~/.local";
     }
     {
       key = "p";
       dirPath = getDir config.xdg.userDirs.pictures "Pictures";
-    }
-    {
-      key = "R";
-      dirPath = "/run";
+      desc = "~/Pictures";
     }
     {
       key = "r";
@@ -117,37 +102,120 @@ let
       desc = "Go to the root of git directory";
     }
     {
-      key = "s";
-      dirPath = "/srv";
+      key = "t";
+      dirPath = "/tmp";
     }
     {
       key = "u";
       dirPath = "/usr";
     }
     {
-      key = "v";
-      dirPath = "/var";
+      key = "V";
+      dirPath = getDir config.xdg.userDirs.videos "Videos";
+      desc = "~/Videos";
     }
     {
       key = "w";
       dirPath = "${config.xdg.dataHome}/wallpapers";
+      desc = "~/.local/share/wallpapers";
+    }
+  ];
+
+  linuxLocations = [
+    {
+      key = "H";
+      dirPath = "/nix/var/nix/profiles/per-user/${config.khanelinix.user.name}/home-manager";
+      desc = "/nix/var/.../home-manager";
+    }
+    {
+      key = "i";
+      dirPath = "/run/media/${config.khanelinix.user.name}";
+      desc = "/run/media/${config.khanelinix.user.name}";
+    }
+    {
+      key = "m";
+      dirPath = "/media";
+    }
+    {
+      key = "M";
+      dirPath = "/mnt";
     }
     {
       key = "n";
       dirPath = "/run/current-system";
-      desc = "Go to the current NixOS system profile";
+    }
+    {
+      key = "o";
+      dirPath = "/opt";
+    }
+    {
+      key = "R";
+      dirPath = "/run";
+    }
+    {
+      key = "s";
+      dirPath = "/srv";
+    }
+    {
+      key = "v";
+      dirPath = "/var";
+    }
+  ];
+
+  darwinLocations = [
+    {
+      key = "a";
+      dirPath = "${config.home.homeDirectory}/Applications";
+      desc = "~/Applications";
+    }
+    {
+      key = "A";
+      dirPath = "/Applications";
+    }
+    {
+      key = "b";
+      dirPath = "${config.home.homeDirectory}/.Trash";
+      desc = "~/.Trash";
+    }
+    {
+      key = "C";
+      dirPath = "${config.home.homeDirectory}/Library/Mobile Documents/com~apple~CloudDocs";
+      desc = "~/Library/.../CloudDocs";
     }
     {
       key = "H";
-      dirPath =
-        if pkgs.stdenv.hostPlatform.isLinux then
-          "/nix/var/nix/profiles/per-user/${config.khanelinix.user.name}/home-manager"
-        else
-          "${config.xdg.stateHome}/home-manager/gcroots";
-      desc = "Go to the home-manager profile";
+      dirPath = "${config.xdg.stateHome}/home-manager/gcroots";
+      desc = "~/.local/state/home-manager/gcroots";
+    }
+    {
+      key = "L";
+      dirPath = "${config.home.homeDirectory}/Library";
+      desc = "~/Library";
+    }
+    {
+      key = "n";
+      dirPath = "/run/current-system";
+    }
+    {
+      key = "P";
+      dirPath = "${config.home.homeDirectory}/Library/CloudStorage/ProtonDrive-${config.khanelinix.user.email}";
+      desc = "~/Library/.../ProtonDrive";
+    }
+    {
+      key = "v";
+      dirPath = "/var";
     }
   ];
+
+  # Available keys:
+  # B, f, F, j, J, q, Q, x, X, y, Y, z, Z, K, N, O, S, U, W, I
+  # Used keys:
+  # /, <Space>, h, c, t, d, D, e, g, G, k, l, p, r, u, V, w, i, M, m, n, o, R, s, v, H, A, a, C, L, P, b
+  gotoLocations =
+    commonLocations
+    ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux linuxLocations
+    ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin darwinLocations;
 in
 {
-  prepend_keymap = map mkGotoKeymap gotoLocations;
+  prepend_keymap = map mkGotoKeymap (builtins.sort (a: b: a.key < b.key) gotoLocations);
 }
