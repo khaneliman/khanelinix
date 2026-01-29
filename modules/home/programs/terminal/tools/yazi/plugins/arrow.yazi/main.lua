de626086ca84b9794e08422a188aa00a7d73e752
Austin Horstman <khaneliman12@gmail.com>
2026-01-29 00:00:14 -0600

    chore(yazi): ya.manager_emit -> ya.emit

--- modules/home/programs/terminal/tools/yazi/plugins/arrow.yazi/main.lua
+++ modules/home/programs/terminal/tools/yazi/plugins/arrow.yazi/main.lua
@@ -3,6 +3,6 @@ return {
 	entry = function(_, job)
 		local current = cx.active.current
 		local new = (current.cursor + job.args[1]) % #current.files
-		ya.manager_emit("arrow", { new - current.cursor })
+		ya.emit("arrow", { new - current.cursor })
 	end,
 }
