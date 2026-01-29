593614c0502ce6c926fabfe7e9f98e1c4c9c6d32
Austin Horstman <khaneliman12@gmail.com>
2026-01-28 23:59:56 -0600

    chore(yazi): remove sixel_fraction option
    Removed upstream

--- modules/home/programs/terminal/tools/yazi/default.nix
+++ modules/home/programs/terminal/tools/yazi/default.nix
@@ -176,7 +176,6 @@ in
             max_height = 900;
             image_filter = "triangle";
             image_quality = 75;
-            sixel_fraction = 15;
             ueberzug_scale = 1;
             ueberzug_offset = [
               0
