81b15aa9449e136b92611d905aa42abfee078f6e
Austin Horstman <khaneliman12@gmail.com>
2026-02-25 00:36:58 -0600

    feat(kitty): bump kitty version and include linux
    Recent wayland improvements in kitty, just use on both platforms with
    latest.

--- overlays/kitty/default.nix
+++ overlays/kitty/default.nix
@@ -1,25 +1,23 @@
 _: _final: prev: {
-  kitty = prev.kitty.overrideAttrs (
-    prev.lib.optionalAttrs prev.stdenv.hostPlatform.isDarwin rec {
-      version = "0.45.0-unstable-2026-01-20-pr9394";
+  kitty = prev.kitty.overrideAttrs rec {
+    version = "0.45.0-unstable-2026-02-25";
 
-      src = prev.fetchFromGitHub {
-        owner = "kovidgoyal";
-        repo = "kitty";
-        rev = "fccb06e4bf9dd9cbc4024f386bf9ea9ac82a850e";
-        hash = "sha256-mUfPaI9FFqd/+3jxzaEcAGiYnhYi9yUwS/mY68VASvE=";
-      };
+    src = prev.fetchFromGitHub {
+      owner = "kovidgoyal";
+      repo = "kitty";
+      rev = "a823f72e0ed21d5eb2d36b86affd79dec5396ad1";
+      hash = "sha256-fz59mrGF45mfYSzbvtT+TSO7u6yCMhz0OHblJZI2e9E=";
+    };
 
-      inherit
-        (
-          (prev.buildGo124Module {
-            pname = "kitty-go-modules";
-            inherit src version;
-            vendorHash = "sha256-Y5EnsTUJYuzGJaMsoBhSWGtCtnJeElkjrhyHnLVM0ZU=";
-          })
-        )
-        goModules
-        ;
-    }
-  );
+    inherit
+      (
+        (prev.buildGo124Module {
+          pname = "kitty-go-modules";
+          inherit src version;
+          vendorHash = "sha256-Z+rLtcxUOit43n7FmMKTCRyTPtv5PGplu5OUEfcvucc=";
+        })
+      )
+      goModules
+      ;
+  };
 }
