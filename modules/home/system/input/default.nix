b6660c01cdd5342209edf557cfbb003ac79cd0d8
Austin Horstman <khaneliman12@gmail.com>
2026-03-07 12:04:40 -0600

    feat(input): support karabiner in home module

--- modules/home/system/input/default.nix
+++ modules/home/system/input/default.nix
@@ -1,13 +1,14 @@
 {
   config,
   lib,
-
+  osConfig ? { },
   ...
 }:
 let
   inherit (lib) mkIf mkEnableOption;
 
   cfg = config.khanelinix.system.input;
+  karabinerEnabled = osConfig.services.karabiner-elements.enable or false;
 in
 {
   options.khanelinix.system.input = {
@@ -15,10 +16,52 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.macos-remap-keys = {
+    services.macos-remap-keys = mkIf (!karabinerEnabled) {
       enable = true;
       keyboard = {
         Capslock = "Escape";
+      };
+    };
+
+    xdg.configFile."karabiner/karabiner.json" = mkIf karabinerEnabled {
+      text = builtins.toJSON {
+        global = {
+          show_in_menu_bar = false;
+        };
+        profiles = [
+          {
+            name = "Default profile";
+            selected = true;
+            virtual_hid_keyboard = {
+              keyboard_type_v2 = "ansi";
+            };
+            complex_modifications.rules = [
+              {
+                description = "Caps Lock to Escape if tapped, Left Control if held";
+                manipulators = [
+                  {
+                    type = "basic";
+                    from = {
+                      key_code = "caps_lock";
+                      modifiers.optional = [ "any" ];
+                    };
+                    to = [
+                      {
+                        key_code = "left_control";
+                        lazy = true;
+                      }
+                    ];
+                    to_if_alone = [
+                      {
+                        key_code = "escape";
+                      }
+                    ];
+                  }
+                ];
+              }
+            ];
+          }
+        ];
       };
     };
   };
