dd9449c0027551b0f49f46d4b549eed971b62b35
Austin Horstman <khaneliman12@gmail.com>
2026-03-14 19:08:09 -0500

    chore(ghostty): organize settings

--- modules/home/programs/terminal/emulators/ghostty/default.nix
+++ modules/home/programs/terminal/emulators/ghostty/default.nix
@@ -40,34 +40,33 @@ in
         in
         {
           adw-toolbar-style = "flat";
-
           background-blur-radius = 20;
           background-opacity = lib.mkDefault 0.8;
-
           clipboard-trim-trailing-spaces = true;
           copy-on-select = "clipboard";
-
           focus-follows-mouse = true;
-
-          font-size = lib.mkDefault 13;
           font-family = lib.mkForce monaspaceNeon;
           font-family-bold = lib.mkForce monaspaceXenon;
-          font-family-italic = lib.mkForce monaspaceRadon;
           font-family-bold-italic = lib.mkForce monaspaceKrypton;
+          font-family-italic = lib.mkForce monaspaceRadon;
           font-feature = "+ss01,+ss02,+ss03,+ss04,+ss05,+ss06,+ss07,+ss08,+ss09,+ss10,+liga,+dlig,+calt";
+          font-size = lib.mkDefault 13;
 
           # NOTE: Different methods of using cgroups for every surface.
           # linux-cgroup = "always";
           gtk-single-instance = false;
 
-          macos-titlebar-style = "tabs";
-          window-colorspace = "srgb";
-          window-theme = "ghostty";
+          macos-auto-secure-input = true;
+          # macos-hidden = "always";
           macos-option-as-alt = true;
+          macos-secure-input-indication = true;
+          macos-titlebar-style = "tabs";
+          macos-icon-frame = lib.mkDefault "plastic";
 
           quit-after-last-window-closed = true;
-
-          # Disables some functionality available through window
+          window-colorspace = "srgb";
+          window-theme = "ghostty";
+          # NOTE: Disables some functionality available through window
           # But, doesn't fit theme of a clean WM with its GTK interface
           window-decoration = lib.mkIf pkgs.stdenv.hostPlatform.isLinux false;
         };
