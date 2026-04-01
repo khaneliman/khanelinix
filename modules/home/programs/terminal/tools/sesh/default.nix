bd1aa6f6c772462e39d6a79dbca42b9704521332
Austin Horstman <khaneliman12@gmail.com>
2026-04-01 17:06:13 -0500

    feat(sesh): create new module
    Move sesh into its own module since it's getting verbose

---
+++ modules/home/programs/terminal/tools/sesh/default.nix
@@ -0,0 +1,106 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.khanelinix.programs.terminal.tools.sesh;
+in
+{
+  options.khanelinix.programs.terminal.tools.sesh = {
+    enable = mkEnableOption "sesh";
+  };
+
+  config = mkIf cfg.enable {
+    home = {
+      packages = [ pkgs.sesh ];
+
+      shellAliases = {
+        sl = "sesh list";
+        tl = "sesh last";
+        tr = ''sesh connect --root "$(pwd)"'';
+        ts = ''sesh connect "$(sesh list | fzf)"'';
+      };
+    };
+
+    xdg.configFile."sesh/sesh.toml".source =
+      let
+        seshFormat = pkgs.formats.toml { };
+        seshWindowNames = [
+          "git"
+          "files"
+          "shell"
+        ];
+        seshWindows = [
+          {
+            name = "git";
+            startup_script = "lazygit";
+          }
+          {
+            name = "files";
+            startup_script = "yazi";
+          }
+          {
+            name = "shell";
+          }
+        ];
+        seshSessions = [
+          {
+            name = "khanelinix";
+            path = "~/khanelinix";
+            startup_command = "nvim";
+            windows = seshWindowNames;
+          }
+        ]
+        ++
+          map
+            (name: {
+              inherit name;
+              path = "~/github/${name}";
+              startup_command = "nvim";
+              windows = seshWindowNames;
+            })
+            [
+              "khanelivim"
+              "nixpkgs"
+              "home-manager"
+              "nixvim"
+              "waybar"
+            ];
+        seshConfig = {
+          blacklist = [ "scratch" ];
+          dir_length = 2;
+          sort_order = [
+            "config"
+            "tmux"
+            "zoxide"
+          ];
+          default_session.preview_command = "eza --all --git --icons --color=always {}";
+          session = seshSessions;
+          wildcard = [
+            {
+              pattern = "~/github/*";
+              preview_command = "eza --all --git --icons --color=always {}";
+            }
+            {
+              pattern = "~/.local/share/worktrees/*";
+              preview_command = "eza --all --git --icons --color=always {}";
+            }
+            {
+              pattern = "~/.local/share/worktrees/*/*";
+              preview_command = "eza --all --git --icons --color=always {}";
+            }
+          ];
+          window = seshWindows;
+        };
+      in
+      pkgs.writeText "sesh.toml" ''
+        #:schema https://github.com/joshmedeski/sesh/raw/main/sesh.schema.json
+
+        ${builtins.readFile (seshFormat.generate "sesh.toml" seshConfig)}
+      '';
+  };
+}
