c981f67c16c719be475cb968d21e64b65761baad
Austin Horstman <khaneliman12@gmail.com>
2026-04-07 20:30:20 -0500

    feat(input): disable two finger right side gesture
    Annoyingly opens notification center while trying to play roblox with
    the kids.

--- modules/home/system/input/default.nix
+++ modules/home/system/input/default.nix
@@ -2,10 +2,16 @@
   config,
   lib,
   osConfig ? { },
+  pkgs,
   ...
 }:
 let
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
 
   cfg = config.khanelinix.system.input;
   karabinerEnabled = osConfig.services.karabiner-elements.enable or false;
@@ -17,6 +23,16 @@ in
 
   options.khanelinix.system.input = {
     enable = mkEnableOption "macOS input";
+    notificationCenterRightEdgeSwipe = mkOption {
+      type = types.nullOr (
+        types.enum [
+          0
+          3
+        ]
+      );
+      default = 0;
+      description = "Two-finger swipe from the right edge on the trackpad: 0 disables it, 3 opens Notification Center.";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -26,5 +42,20 @@ in
         Capslock = "Escape";
       };
     };
+
+    home.activation.notificationCenterRightEdgeSwipe =
+      mkIf (pkgs.stdenv.hostPlatform.isDarwin && cfg.notificationCenterRightEdgeSwipe != null)
+        (
+          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            echo >&2 "Setting Notification Center right-edge swipe gesture..."
+            $DRY_RUN_CMD /usr/bin/defaults write com.apple.AppleMultitouchTrackpad \
+              TrackpadTwoFingerFromRightEdgeSwipeGesture -int ${toString cfg.notificationCenterRightEdgeSwipe}
+            $DRY_RUN_CMD /usr/bin/defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad \
+              TrackpadTwoFingerFromRightEdgeSwipeGesture -int ${toString cfg.notificationCenterRightEdgeSwipe}
+            $DRY_RUN_CMD /usr/bin/defaults -currentHost write NSGlobalDomain \
+              com.apple.trackpad.twoFingerFromRightEdgeSwipeGesture -int ${toString cfg.notificationCenterRightEdgeSwipe}
+            $DRY_RUN_CMD /usr/bin/killall cfprefsd >/dev/null 2>&1 || true
+          ''
+        );
   };
 }
