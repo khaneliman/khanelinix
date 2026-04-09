7df3a00f593b3e7de50cc90c616099d71f7b4347
Austin Horstman <khaneliman12@gmail.com>
2026-04-08 22:07:00 -0500

    refactor(treefmt): remove duplicate hooks
    Use treefmt as the primary entrypoint for shared formatting and linting hooks.
    This removes standalone statix, deadnix, and actionlint pre-commit hooks so they do not run twice with separate configuration paths.

--- flake/dev/checks.nix
+++ flake/dev/checks.nix
@@ -14,16 +14,7 @@
         check.enable = false;
 
         settings.hooks = {
-          # FIXME: broken dependency on darwin
-          actionlint.enable = pkgs.stdenv.hostPlatform.isLinux;
           clang-tidy.enable = true;
-          deadnix = {
-            enable = true;
-
-            settings = {
-              edit = true;
-            };
-          };
           eslint = {
             enable = true;
             package = pkgs.eslint_d;
@@ -37,13 +28,6 @@
             pass_filenames = false;
           };
           pre-commit-hook-ensure-sops.enable = true;
-          statix = {
-            enable = true;
-            # Only staged changes
-            pass_filenames = true;
-            entry = "${lib.getExe pkgs.bash} -c 'for file in \"$@\"; do ${lib.getExe pkgs.statix} check \"$file\"; done' --";
-            language = "system";
-          };
           treefmt.enable = true;
           typos = {
             enable = true;
