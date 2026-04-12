5713cb87d1c4717a2d0bd94fb681f692ed2f6a74
Austin Horstman <khaneliman12@gmail.com>
2026-04-12 16:28:39 -0500

    refactor(treefmt)!: exclude vendored code
    Don't want to mess around with linting/formatting code that comes from a
    external source that was copied in. Keep in-tact.

--- flake/dev/treefmt.nix
+++ flake/dev/treefmt.nix
@@ -80,58 +80,86 @@
           yamlfmt.enable = true;
         };
 
-        settings = {
-          global.excludes = [
-            "*.editorconfig"
-            "*.envrc"
-            "*.gitconfig"
-            "*.git-blame-ignore-revs"
-            "*.gitignore"
-            "*.gitattributes"
-            "*CODEOWNERS"
-            "*LICENSE"
-            "*flake.lock"
-            "*.conf"
-            "*.gif"
-            "*.ico"
-            "*.ini"
-            "*.micro"
-            "*.png"
-            "*.svg"
-            "*.tmux"
-            "*/config"
-            # TODO: formatters?
-            "*.ac"
-            "*.css" # Exclude CSS files from formatting since we use Nix template variables
-            "*.csproj"
-            "*.fsproj"
-            "*.in"
-            "*.kdl"
-            "*.kvconfig"
-            "*.rasi"
-            "*.sln"
-            "*.xml"
-            "*.zsh"
-            "*Makefile"
-            "*makefile"
+        settings =
+          let
+            repoRoot = ../..;
 
-            # Unique files
-            "homes/x86_64-linux/nixos@CORE-PW0D2M1A/git/windows-compat-config"
-            "lib/base64/ascii"
-            "modules/darwin/desktop/wms/yabai/extraConfig"
-            "modules/home/programs/graphical/bars/sketchybar/dynamic-island-sketchybar/helper/islandhelper"
-            "modules/home/programs/graphical/bars/sketchybar/dynamic-island-sketchybar/scripts/islands/music/get_artwork.scpt"
-            "modules/home/programs/graphical/bars/sketchybar/dynamic-island-sketchybar/scripts/islands/volume/data/cache"
-            "modules/home/programs/graphical/bars/sketchybar/dynamic-island-sketchybar/sketchybarrc"
-            "systems/x86_64-linux/khanelinix/hyprlandOutput"
-            "systems/x86_64-linux/khanelinix/swayOutput"
-          ];
+            licenseMarkers = [
+              "LICENSE"
+              "LICENSE.md"
+              "LICENSE.txt"
+            ];
+            rootPrefix = "${toString repoRoot}/";
 
-          formatter.ruff-format.options = [ "--isolated" ];
-          formatter.nixf-diagnose.options = [
-            "--ignore=sema-unused-def-lambda-witharg-formal"
-          ];
-        };
+            findLicensedDirectories =
+              dir:
+              let
+                subdirectories = lib.mapAttrsToList (name: _: dir + "/${name}") (
+                  lib.filterAttrs (_: type: type == "directory") (builtins.readDir dir)
+                );
+                nestedDirectories = lib.concatMap findLicensedDirectories subdirectories;
+                relativeDirectory = lib.removePrefix rootPrefix (toString dir);
+              in
+              lib.optional (builtins.any (
+                marker: builtins.pathExists (dir + "/${marker}")
+              ) licenseMarkers) "${relativeDirectory}/**"
+              ++ nestedDirectories;
+
+            licensedDirectoryExcludes = findLicensedDirectories repoRoot;
+          in
+          {
+            global.excludes = [
+              "*.editorconfig"
+              "*.envrc"
+              "*.gitconfig"
+              "*.git-blame-ignore-revs"
+              "*.gitignore"
+              "*.gitattributes"
+              "*CODEOWNERS"
+              "*LICENSE"
+              "*flake.lock"
+              "*.conf"
+              "*.gif"
+              "*.ico"
+              "*.ini"
+              "*.micro"
+              "*.png"
+              "*.svg"
+              "*.tmux"
+              "*/config"
+              # TODO: formatters?
+              "*.ac"
+              "*.css" # Exclude CSS files from formatting since we use Nix template variables
+              "*.csproj"
+              "*.fsproj"
+              "*.in"
+              "*.kdl"
+              "*.kvconfig"
+              "*.rasi"
+              "*.sln"
+              "*.xml"
+              "*.zsh"
+              "*Makefile"
+              "*makefile"
+
+              # Unique files
+              "homes/x86_64-linux/nixos@CORE-PW0D2M1A/git/windows-compat-config"
+              "lib/base64/ascii"
+              "modules/darwin/desktop/wms/yabai/extraConfig"
+              "modules/home/programs/graphical/bars/sketchybar/dynamic-island-sketchybar/helper/islandhelper"
+              "modules/home/programs/graphical/bars/sketchybar/dynamic-island-sketchybar/scripts/islands/music/get_artwork.scpt"
+              "modules/home/programs/graphical/bars/sketchybar/dynamic-island-sketchybar/scripts/islands/volume/data/cache"
+              "modules/home/programs/graphical/bars/sketchybar/dynamic-island-sketchybar/sketchybarrc"
+              "systems/x86_64-linux/khanelinix/hyprlandOutput"
+              "systems/x86_64-linux/khanelinix/swayOutput"
+            ]
+            ++ licensedDirectoryExcludes;
+
+            formatter.ruff-format.options = [ "--isolated" ];
+            formatter.nixf-diagnose.options = [
+              "--ignore=sema-unused-def-lambda-witharg-formal"
+            ];
+          };
       };
     };
 }
