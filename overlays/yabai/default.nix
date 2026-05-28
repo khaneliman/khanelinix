18a5bb54788d48548cc46e4219fee31c6094d73c
Austin Horstman <khaneliman12@gmail.com>
2026-05-27 22:00:40 -0500

    feat(yabai): test nixpkgs pr overlay
    Replace the local yabai package fork with an overlay that applies
    NixOS/nixpkgs#353182 to nixpkgs-unstable and calls the patched package
    expression directly. This keeps the local config closer to the upstream
    PR while testing the Darwin source-build fix.

--- overlays/yabai/default.nix
+++ overlays/yabai/default.nix
@@ -1,6 +1,19 @@
 { inputs }:
-_final: prev: {
-  inherit (inputs.self.packages.${prev.stdenv.hostPlatform.system})
-    yabai
-    ;
+_final: prev:
+let
+  yabaiPatch = prev.fetchpatch2 {
+    name = "nixpkgs-pr-353182-yabai";
+    url = "https://github.com/NixOS/nixpkgs/pull/353182.patch";
+    hash = "sha256-x1eEc3WFXnSuy7uyPnthU2QbWhDX2NnTjmP526GJNzE=";
+  };
+
+  patchedNixpkgs = prev.applyPatches {
+    name = "nixpkgs-yabai-pr-353182";
+    src = inputs.nixpkgs-unstable;
+    patches = [ yabaiPatch ];
+  };
+in
+{
+  # TODO: remove after https://github.com/NixOS/nixpkgs/pull/353182 lands.
+  yabai = prev.callPackage "${patchedNixpkgs}/pkgs/by-name/ya/yabai/package.nix" { };
 }
