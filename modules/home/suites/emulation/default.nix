709a9e477c3a397516f220b0224e708ef7dd1861
Austin Horstman <khaneliman12@gmail.com>
2026-06-10 08:54:44 -0500

    refactor(emulation): simplify retroarch enable conditional
    Replace if/then with mkDefault on both branches by a single
    lib.mkDefault (!cfg.retroarchFull); identical merge semantics.

--- modules/home/suites/emulation/default.nix
+++ modules/home/suites/emulation/default.nix
@@ -43,7 +43,7 @@ in
       programs = {
         graphical = {
           apps = {
-            retroarch.enable = if cfg.retroarchFull then lib.mkDefault false else lib.mkDefault true;
+            retroarch.enable = lib.mkDefault (!cfg.retroarchFull);
           };
         };
       };
