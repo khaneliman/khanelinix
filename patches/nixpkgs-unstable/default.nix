8b174b75a84024f2817c25f7a9e112882fb02e84
Austin Horstman <khaneliman12@gmail.com>
2026-06-12 18:15:34 -0500

    chore(flake): lock update

--- patches/nixpkgs-unstable/default.nix
+++ patches/nixpkgs-unstable/default.nix
@@ -15,12 +15,4 @@ _: [
   # ./local.patch
   #
   # <patch derivation>
-
-  # vmTools: fix `img` collision with pkgs.img that breaks runInLinuxVM
-  # consumers (nix-rosetta-builder disk image). Remove once nixpkgs-unstable
-  # advances past 68d32ed6cb (merged 2026-06-11).
-  {
-    url = "https://github.com/NixOS/nixpkgs/commit/68d32ed6cbd986a84582b88780168d22adebd6fc.patch";
-    hash = "sha256-rWqOF2m+D5EgYkFSDcfm3aXHIIfOzL3lPS4wy0s9TFk=";
-  }
 ]
