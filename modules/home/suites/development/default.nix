cbbc4585c0d0c90875300773396175b87c810c82
Austin Horstman <khaneliman12@gmail.com>
2026-07-01 09:18:21 -0500

    chore(flake): lock update

--- modules/home/suites/development/default.nix
+++ modules/home/suites/development/default.nix
@@ -95,7 +95,8 @@ in
         ++ lib.optionals (pkgs.stdenv.hostPlatform.isLinux && !isWSL && includes "maximal") [
           qtcreator
         ]
-        ++ lib.optionals cfg.dockerEnable [
+        # FIXME: podman marked broken nixpkgs darwin
+        ++ lib.optionals (pkgs.stdenv.hostPlatform.isLinux && cfg.dockerEnable) [
           podman
           podman-desktop
           podman-tui
@@ -255,7 +256,8 @@ in
     };
 
     services.exo = {
-      enable = mkDefault (cfg.aiEnable && pkgs.stdenv.hostPlatform.isDarwin);
+      # FIXME: broken nixpkgs dep
+      # enable = mkDefault (cfg.aiEnable && pkgs.stdenv.hostPlatform.isDarwin);
       environmentVariables = {
         EXO_LIBP2P_NAMESPACE = "khanelinix";
         EXO_MODELS_READ_ONLY_DIRS = lib.concatStringsSep ":" [
