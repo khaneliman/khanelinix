2c2dfd279f0e7542eb30e9f36b5fa81a75c9ce84
Austin Horstman <khaneliman12@gmail.com>
2026-08-28 15:45:35 -0500

    fix(home-qt): correct shared theme settings
    Let qtct own widget styling and render theme-provided QML colors.

--- modules/home/theme/qt/default.nix
+++ modules/home/theme/qt/default.nix
@@ -21,13 +21,30 @@ let
   cfg = config.khanelinix.theme.qt;
   fontCfg = config.khanelinix.fonts;
   inherit (pkgs.stdenv.hostPlatform) isLinux;
+
+  qtQuickControlsConfig = "${config.xdg.configHome}/qtquickcontrols2.conf";
+  qtQuickControlsFormat = pkgs.formats.ini { };
+  qtQuickControlsPalette = lib.mapAttrs' (
+    role: color: lib.nameValuePair "Palette\\${role}" color
+  ) cfg.qml.palette;
+  qtQuickControlsSettings = lib.genAttrs [
+    "Basic"
+    "Default"
+    "Fusion"
+    "Imagine"
+    "Material"
+    "Universal"
+  ] (_: qtQuickControlsPalette);
+  qtctSettings = lib.mapAttrs (
+    _: section: lib.filterAttrs (_: value: value != null) section
+  ) cfg.settings;
 in
 {
   options.khanelinix.theme.qt = with types; {
-    enable = lib.mkEnableOption "customizing qt and apply themes";
+    enable = lib.mkEnableOption "Qt theming";
 
     theme = {
-      name = mkOpt str "Catppuccin-Macchiato-Blue" "The name of the kvantum theme to apply.";
+      name = mkOpt str "catppuccin-macchiato-blue" "The name of the kvantum theme to apply.";
       package = mkOpt package (
         if isLinux then
           pkgs.catppuccin-kvantum.override {
@@ -39,10 +56,12 @@ in
       ) "The package to use for the theme.";
     };
 
+    qml.palette = mkOpt (attrsOf str) { } "Qt Quick Controls palette";
+
     settings = {
       Appearance = {
         color_scheme_path = mkOpt types.str "" "Color scheme path";
-        custom_palette = mkBoolOpt true "Whether to use custom palette";
+        custom_palette = mkBoolOpt false "Whether to use custom palette";
         icon_theme = mkOpt types.str (
           if isLinux then config.khanelinix.theme.gtk.icon.name else ""
         ) "Icon theme";
@@ -51,8 +70,8 @@ in
       };
 
       Fonts = {
-        fixed = mkOpt types.str "${fontCfg.monaspace.families.krypton} 10" "Fixed font type";
-        general = mkOpt types.str "${fontCfg.monaspace.families.neon} 10" "General font type";
+        fixed = mkOpt types.str ''"${fontCfg.monaspace.families.krypton},12"'' "Fixed font type";
+        general = mkOpt types.str ''"Lexend,12"'' "General font type";
       };
 
       Interface = {
@@ -61,21 +80,20 @@ in
         cursor_flash_time = mkOpt types.int 1000 "Cursor flash time";
         dialog_buttons_have_icons = mkOpt types.int 1 "Whether dialog buttons have icons";
         double_click_interval = mkOpt types.int 400 "Double click interval";
-        gui_effects = mkOpt (types.nullOr types.int) null "Whether to enable effects"; # You might need to adjust this depending on Nix version
+        gui_effects = mkOpt (types.nullOr types.int) null "qtct GUI effects bitmask";
         keyboard_scheme = mkOpt types.int 2 "keyboard_scheme";
         menus_have_icons = mkBoolOpt true "Whether menus have icons";
         show_shortcuts_in_context_menus = mkBoolOpt true "Show shortcuts in context menus";
-        stylesheets = mkOpt (types.nullOr (types.listOf types.str)) null "Stylesheets"; # You might need to adjust this depending on Nix version
-        toolbutton_style = mkOpt types.str "kvantum" "Toolbutton style";
+        stylesheets = mkOpt (types.nullOr (types.listOf types.str)) null "Stylesheets";
+        toolbutton_style = mkOpt types.int 4 "Toolbutton style";
         underline_shortcut = mkOpt types.int 1 "Whether to underline shortcuts";
-        # wheel_scroll_lines = 3;
       };
 
       Troubleshooting = {
-        force_raster_widgets = mkOpt types.int 1 "Whether to force rastering of widgets";
+        force_raster_widgets = mkOpt types.int 1 "qtct raster widget mode";
         ignored_applications = mkOpt (types.nullOr (
           types.listOf types.str
-        )) null "List of applications to ignore"; # You might need to adjust this depending on Nix version
+        )) null "List of applications to ignore";
       };
     };
   };
@@ -83,27 +101,31 @@ in
   config = mkIf (cfg.enable && isLinux) {
     home = {
       packages = with pkgs; [
-        # libraries and programs to ensure that qt applications load without issue
-        # kdePackages = qt6
         kdePackages.breeze-icons
         kdePackages.qqc2-desktop-style
-        kdePackages.qt6ct
-        qt6.qtsvg # needed to load breeze icons
+        libsForQt5.qtstyleplugin-kvantum
+        qt6.qtsvg
         qt6.qtwayland
-        qt6Packages.qt6gtk2
         qt6Packages.qtstyleplugin-kvantum
       ];
 
       sessionVariables = {
-        # scaling - 1 means no scaling
-        QT_AUTO_SCREEN_SCALE_FACTOR = "1";
         # use wayland as the default backend, fallback to xcb if wayland is not available
         QT_QPA_PLATFORM = "wayland;xcb";
         # disable window decorations everywhere
         QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-        # remain backwards compatible with qt5
-        DISABLE_QT5_COMPAT = "0";
+      }
+      // lib.optionalAttrs (cfg.qml.palette != { }) {
+        QT_QUICK_CONTROLS_CONF = qtQuickControlsConfig;
       };
+    };
+
+    systemd.user.sessionVariables.QT_QUICK_CONTROLS_CONF = lib.mkIf (
+      cfg.qml.palette != { }
+    ) qtQuickControlsConfig;
+
+    xdg.configFile."qtquickcontrols2.conf" = lib.mkIf (cfg.qml.palette != { }) {
+      source = qtQuickControlsFormat.generate "qtquickcontrols2.conf" qtQuickControlsSettings;
     };
 
     qt = {
@@ -121,10 +143,8 @@ in
         name = mkDefault "qtct";
       };
 
-      qt5ctSettings = cfg.settings;
-      qt6ctSettings = cfg.settings;
-
-      style = mkDefault { name = "kvantum"; };
+      qt5ctSettings = qtctSettings;
+      qt6ctSettings = qtctSettings;
     };
   };
 }
