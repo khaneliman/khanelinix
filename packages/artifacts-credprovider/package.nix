32fdd38029e8a9dcfc71b42ed22e3e4c79789d73
Austin Horstman <khaneliman12@gmail.com>
2026-09-01 17:59:03 -0500

    chore(artifacts-credprovider): update to 2.0.4
    Update artifacts-credprovider from 1.3.0 to 2.0.4.
    
    nix-update built the package before creating this commit.

--- packages/artifacts-credprovider/package.nix
+++ packages/artifacts-credprovider/package.nix
@@ -2,11 +2,11 @@
 # TODO: upstream
 stdenv.mkDerivation rec {
   name = "artifacts-credprovider";
-  version = "1.3.0";
+  version = "2.0.4";
 
   src = pkgs.fetchurl {
     url = "https://github.com/microsoft/artifacts-credprovider/releases/download/v${version}/Microsoft.Net8.NuGet.CredentialProvider.tar.gz";
-    hash = "sha256-WEssfdOKMjJ3WD/egD4wA69k+JdB9O/ZWM8RstRJGkA=";
+    hash = "sha256-yvDQQIfy/LAzD1rxNhBChJ3l18SGt8oV+LLiF/PDhwY=";
   };
 
   buildPhase = ''
