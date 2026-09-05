fa731b2bb5111d6f92c465deb5a4b99ce8751f46
Austin Horstman <khaneliman12@gmail.com>
2026-09-04 20:06:47 -0500

    feat(yazi): use native JSON and archive previews
    Use upstream JSON formatting and archive tree previews, including
    compressed tarballs. Keep Ouch available for compression without
    replacing the native previewer.

--- modules/home/programs/terminal/tools/yazi/settings/plugin.nix
+++ modules/home/programs/terminal/tools/yazi/settings/plugin.nix
@@ -57,10 +57,6 @@ in
           run = ''piper -- xlsx2csv "$1" | bat -p --color=always --file-name "$1.csv"'';
         }
         {
-          url = "local://*.json";
-          run = ''piper -- bat -p --color=always "$1"'';
-        }
-        {
           url = "local://*.sqlite";
           run = ''piper -- duckdb -c "SELECT * FROM sqlite_scan('$1') LIMIT 50"'';
         }
@@ -69,33 +65,7 @@ in
           run = ''piper -- duckdb -c "SELECT * FROM sqlite_scan('$1') LIMIT 50"'';
         }
       ]
-      ++ lib.optionals (lib.hasAttr "ouch" enabledPlugins) (
-        let
-          mimeTypes = [
-            "application/gzip"
-            "application/x-7z-compressed"
-            "application/x-bzip2"
-            "application/x-compressed-tar"
-            "application/x-gzip"
-            "application/x-rar"
-            "application/x-tar"
-            "application/x-tar+gzip"
-            "application/x-xz"
-            "application/xz"
-            "application/zip"
-          ];
-        in
-        map (mime: {
-          inherit mime;
-          url = "local://*";
-          run = "ouch";
-        }) mimeTypes
-      )
       ++ lib.optionals (lib.hasAttr "piper" enabledPlugins) [
-        {
-          url = "local://*.tar*";
-          run = ''piper --format=url -- tar tf "$1"'';
-        }
         {
           url = "local://*.csv";
           run = ''piper -- bat -p --color=always "$1"'';
